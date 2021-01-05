eb032e08b0953ea79ca9a77df8c7b022201a1da9
Renato Lima <victorespucoc@gmail.com>
2021-01-05 16:43:28 -0300

    conecta

--- principal.php
+++ principal.php
@@ -2127,7 +2127,121 @@ License: You must have a valid license purchased only from themeforest(the above
 										<div class="col-xl-12">
 											<!--begin::List Widget 7-->
 											<div class="card card-custom gutter-b card-stretch">
-												
+												<div class="card-header border-0">
+													<h3 class="card-title font-weight-bolder text-dark">Minhas Mídias</h3>
+													
+												</div>
+												<!--begin::Body-->
+												<div class="card-body pt-0">
+													<!--begin::Item-->
+													<div class="d-flex align-items-center flex-wrap mb-10">
+														<!--begin::Symbol-->
+														<div class="symbol symbol-50 symbol-light mr-5">
+															<span class="symbol-label">
+																<img src="assets/media/svg/misc/006-plurk.svg" class="h-50 align-self-center" alt="" />
+															</span>
+														</div>
+														<!--end::Symbol-->
+														<!--begin::Text-->
+														<div class="d-flex flex-column flex-grow-1 mr-2">
+															<a href="#" class="font-weight-bold text-dark-75 text-hover-primary font-size-lg mb-1">Top Authors</a>
+															<span class="text-muted font-weight-bold">Mark, Rowling, Esther</span>
+														</div>
+														<!--end::Text-->
+														<span class="label label-xl label-light label-inline my-lg-0 my-2 text-dark-50 font-weight-bolder">+82$</span>
+													</div>
+													<!--end::Item-->
+													<!--begin::Item-->
+													<div class="d-flex align-items-center flex-wrap mb-10">
+														<!--begin::Symbol-->
+														<div class="symbol symbol-50 symbol-light mr-5">
+															<span class="symbol-label">
+																<img src="assets/media/svg/misc/015-telegram.svg" class="h-50 align-self-center" alt="" />
+															</span>
+														</div>
+														<!--end::Symbol-->
+														<!--begin::Text-->
+														<div class="d-flex flex-column flex-grow-1 mr-2">
+															<a href="#" class="font-weight-bold text-dark-75 text-hover-primary font-size-lg mb-1">Popular Authors</a>
+															<span class="text-muted font-weight-bold">Randy, Steve, Mike</span>
+														</div>
+														<!--end::Text-->
+														<span class="label label-xl label-light label-inline my-lg-0 my-2 text-dark-50 font-weight-bolder">+280$</span>
+													</div>
+													<!--end::Item-->
+													<!--begin::Item-->
+													<div class="d-flex align-items-center flex-wrap mb-10">
+														<!--begin::Symbol-->
+														<div class="symbol symbol-50 symbol-light mr-5">
+															<span class="symbol-label">
+																<img src="assets/media/svg/misc/003-puzzle.svg" class="h-50 align-self-center" alt="" />
+															</span>
+														</div>
+														<!--end::Symbol-->
+														<!--begin::Text-->
+														<div class="d-flex flex-column flex-grow-1 mr-2">
+															<a href="#" class="font-weight-bold text-dark-75 text-hover-primary font-size-lg mb-1">New Users</a>
+															<span class="text-muted font-weight-bold">John, Pat, Jimmy</span>
+														</div>
+														<!--end::Text-->
+														<span class="label label-xl label-light label-inline my-lg-0 my-2 text-dark-50 font-weight-bolder">+4500$</span>
+													</div>
+													<!--end::Item-->
+													<!--begin::Item-->
+													<div class="d-flex align-items-center flex-wrap mb-10">
+														<!--begin::Symbol-->
+														<div class="symbol symbol-50 symbol-light mr-5">
+															<span class="symbol-label">
+																<img src="assets/media/svg/misc/005-bebo.svg" class="h-50 align-self-center" alt="" />
+															</span>
+														</div>
+														<!--end::Symbol-->
+														<!--begin::Text-->
+														<div class="d-flex flex-column flex-grow-1 mr-2">
+															<a href="#" class="font-weight-bold text-dark-75 text-hover-primary font-size-lg mb-1">Active Customers</a>
+															<span class="text-muted font-weight-bold">Sandra, Tim, Louis</span>
+														</div>
+														<!--end::Text-->
+														<span class="label label-xl label-light label-inline my-lg-0 my-2 text-dark-50 font-weight-bolder">+4500$</span>
+													</div>
+													<!--end::Item-->
+													<!--begin::Item-->
+													<div class="d-flex align-items-center flex-wrap">
+														<!--begin::Symbol-->
+														<div class="symbol symbol-50 symbol-light mr-5">
+															<span class="symbol-label">
+																<img src="assets/media/svg/misc/014-kickstarter.svg" class="h-50 align-self-center" alt="" />
+															</span>
+														</div>
+														<!--end::Symbol-->
+														<!--begin::Text-->
+														<div class="d-flex flex-column flex-grow-1 mr-2">
+															<a href="#" class="font-weight-bold text-dark-75 text-hover-primary font-size-lg mb-1">Bestseller Theme</a>
+															<span class="text-muted font-weight-bold">Disco, Retro, Sports</span>
+														</div>
+														<!--end::Text-->
+														<span class="label label-xl label-light label-inline my-lg-0 my-2 text-dark-50 font-weight-bolder">+4500$</span>
+													</div>
+													<!--end::Item-->
+												</div>
+												<!--end::Body-->
+											</div>
+											<!--end::List Widget 7-->
+										</div>
+									<?php endif; ?>
+									
+								</div>
+								<div class="row">	
+									<?php if($_SESSION['id_perfil'] == 2 || $_SESSION['id_perfil'] == 3 ) :  ?>
+										<div class="col-xl-12">
+											<!--begin::List Widget 7-->
+											<div class="card card-custom gutter-b card-stretch">
+												<!--begin::Header-->
+												<div class="card-header border-0">
+													<h3 class="card-title font-weight-bolder text-dark">Minhas Mídias</h3>
+													
+												</div>
+												<!--end::Header-->
 												<!--begin::Body-->
 												<div class="card-body pt-0">
 													<!--begin::Item-->
@@ -2241,176 +2355,6 @@ License: You must have a valid license purchased only from themeforest(the above
 										</div>
 										<!--end::Stats Widget 27-->
 									</div>
-									<?php endif; ?>
-								</div>
-								<div class="row">	
-									<?php if($_SESSION['id_perfil'] == 2 || $_SESSION['id_perfil'] == 3 ) :  ?>
-										<div class="col-xl-12">
-											<!--begin::List Widget 7-->
-											<div class="card card-custom gutter-b card-stretch">
-												<!--begin::Header-->
-												<div class="card-header border-0">
-													<h3 class="card-title font-weight-bolder text-dark">Minhas Mídias</h3>
-													<div class="card-toolbar">
-														<div class="dropdown dropdown-inline">
-															<a href="#" class="btn btn-clean btn-hover-light-primary btn-sm btn-icon" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-																<i class="ki ki-bold-more-ver"></i>
-															</a>
-															<div class="dropdown-menu dropdown-menu-md dropdown-menu-right">
-																<!--begin::Naviigation-->
-																<ul class="navi">
-																	<li class="navi-header font-weight-bold py-5">
-																		<span class="font-size-lg">Add New:</span>
-																		<i class="flaticon2-information icon-md text-muted" data-toggle="tooltip" data-placement="right" title="Click to learn more..."></i>
-																	</li>
-																	<li class="navi-separator mb-3 opacity-70"></li>
-																	<li class="navi-item">
-																		<a href="#" class="navi-link">
-																			<span class="navi-icon">
-																				<i class="flaticon2-shopping-cart-1"></i>
-																			</span>
-																			<span class="navi-text">Order</span>
-																		</a>
-																	</li>
-																	<li class="navi-item">
-																		<a href="#" class="navi-link">
-																			<span class="navi-icon">
-																				<i class="navi-icon flaticon2-calendar-8"></i>
-																			</span>
-																			<span class="navi-text">Members</span>
-																			<span class="navi-label">
-																				<span class="label label-light-danger label-rounded font-weight-bold">3</span>
-																			</span>
-																		</a>
-																	</li>
-																	<li class="navi-item">
-																		<a href="#" class="navi-link">
-																			<span class="navi-icon">
-																				<i class="navi-icon flaticon2-telegram-logo"></i>
-																			</span>
-																			<span class="navi-text">Project</span>
-																		</a>
-																	</li>
-																	<li class="navi-item">
-																		<a href="#" class="navi-link">
-																			<span class="navi-icon">
-																				<i class="navi-icon flaticon2-new-email"></i>
-																			</span>
-																			<span class="navi-text">Record</span>
-																			<span class="navi-label">
-																				<span class="label label-light-success label-rounded font-weight-bold">5</span>
-																			</span>
-																		</a>
-																	</li>
-																	<li class="navi-separator mt-3 opacity-70"></li>
-																	<li class="navi-footer pt-5 pb-4">
-																		<a class="btn btn-light-primary font-weight-bolder btn-sm" href="#">More options</a>
-																		<a class="btn btn-clean font-weight-bold btn-sm d-none" href="#" data-toggle="tooltip" data-placement="right" title="Click to learn more...">Learn more</a>
-																	</li>
-																</ul>
-																<!--end::Naviigation-->
-															</div>
-														</div>
-													</div>
-												</div>
-												<!--end::Header-->
-												<!--begin::Body-->
-												<div class="card-body pt-0">
-													<!--begin::Item-->
-													<div class="d-flex align-items-center flex-wrap mb-10">
-														<!--begin::Symbol-->
-														<div class="symbol symbol-50 symbol-light mr-5">
-															<span class="symbol-label">
-																<img src="assets/media/svg/misc/006-plurk.svg" class="h-50 align-self-center" alt="" />
-															</span>
-														</div>
-														<!--end::Symbol-->
-														<!--begin::Text-->
-														<div class="d-flex flex-column flex-grow-1 mr-2">
-															<a href="#" class="font-weight-bold text-dark-75 text-hover-primary font-size-lg mb-1">Top Authors</a>
-															<span class="text-muted font-weight-bold">Mark, Rowling, Esther</span>
-														</div>
-														<!--end::Text-->
-														<span class="label label-xl label-light label-inline my-lg-0 my-2 text-dark-50 font-weight-bolder">+82$</span>
-													</div>
-													<!--end::Item-->
-													<!--begin::Item-->
-													<div class="d-flex align-items-center flex-wrap mb-10">
-														<!--begin::Symbol-->
-														<div class="symbol symbol-50 symbol-light mr-5">
-															<span class="symbol-label">
-																<img src="assets/media/svg/misc/015-telegram.svg" class="h-50 align-self-center" alt="" />
-															</span>
-														</div>
-														<!--end::Symbol-->
-														<!--begin::Text-->
-														<div class="d-flex flex-column flex-grow-1 mr-2">
-															<a href="#" class="font-weight-bold text-dark-75 text-hover-primary font-size-lg mb-1">Popular Authors</a>
-															<span class="text-muted font-weight-bold">Randy, Steve, Mike</span>
-														</div>
-														<!--end::Text-->
-														<span class="label label-xl label-light label-inline my-lg-0 my-2 text-dark-50 font-weight-bolder">+280$</span>
-													</div>
-													<!--end::Item-->
-													<!--begin::Item-->
-													<div class="d-flex align-items-center flex-wrap mb-10">
-														<!--begin::Symbol-->
-														<div class="symbol symbol-50 symbol-light mr-5">
-															<span class="symbol-label">
-																<img src="assets/media/svg/misc/003-puzzle.svg" class="h-50 align-self-center" alt="" />
-															</span>
-														</div>
-														<!--end::Symbol-->
-														<!--begin::Text-->
-														<div class="d-flex flex-column flex-grow-1 mr-2">
-															<a href="#" class="font-weight-bold text-dark-75 text-hover-primary font-size-lg mb-1">New Users</a>
-															<span class="text-muted font-weight-bold">John, Pat, Jimmy</span>
-														</div>
-														<!--end::Text-->
-														<span class="label label-xl label-light label-inline my-lg-0 my-2 text-dark-50 font-weight-bolder">+4500$</span>
-													</div>
-													<!--end::Item-->
-													<!--begin::Item-->
-													<div class="d-flex align-items-center flex-wrap mb-10">
-														<!--begin::Symbol-->
-														<div class="symbol symbol-50 symbol-light mr-5">
-															<span class="symbol-label">
-																<img src="assets/media/svg/misc/005-bebo.svg" class="h-50 align-self-center" alt="" />
-															</span>
-														</div>
-														<!--end::Symbol-->
-														<!--begin::Text-->
-														<div class="d-flex flex-column flex-grow-1 mr-2">
-															<a href="#" class="font-weight-bold text-dark-75 text-hover-primary font-size-lg mb-1">Active Customers</a>
-															<span class="text-muted font-weight-bold">Sandra, Tim, Louis</span>
-														</div>
-														<!--end::Text-->
-														<span class="label label-xl label-light label-inline my-lg-0 my-2 text-dark-50 font-weight-bolder">+4500$</span>
-													</div>
-													<!--end::Item-->
-													<!--begin::Item-->
-													<div class="d-flex align-items-center flex-wrap">
-														<!--begin::Symbol-->
-														<div class="symbol symbol-50 symbol-light mr-5">
-															<span class="symbol-label">
-																<img src="assets/media/svg/misc/014-kickstarter.svg" class="h-50 align-self-center" alt="" />
-															</span>
-														</div>
-														<!--end::Symbol-->
-														<!--begin::Text-->
-														<div class="d-flex flex-column flex-grow-1 mr-2">
-															<a href="#" class="font-weight-bold text-dark-75 text-hover-primary font-size-lg mb-1">Bestseller Theme</a>
-															<span class="text-muted font-weight-bold">Disco, Retro, Sports</span>
-														</div>
-														<!--end::Text-->
-														<span class="label label-xl label-light label-inline my-lg-0 my-2 text-dark-50 font-weight-bolder">+4500$</span>
-													</div>
-													<!--end::Item-->
-												</div>
-												<!--end::Body-->
-											</div>
-											<!--end::List Widget 7-->
-										</div>
 									<?php endif; ?>
 									<?php /*?><div class="col-xl-6">
 										<!--begin::Base Table Widget 5-->
