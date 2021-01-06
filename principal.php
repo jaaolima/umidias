74a2f009d5f6fb5bfcb3d9da69bae3add717033a
Renato Lima <victorespucoc@gmail.com>
2021-01-06 17:04:31 -0300

    principal

--- principal.php
+++ principal.php
@@ -2140,109 +2140,148 @@ License: You must have a valid license purchased only from themeforest(the above
 								
 								<div class="row">
 									<?php if($_SESSION['id_perfil'] == 1 || $_SESSION['id_perfil'] == 3 || $_SESSION['id_perfil'] == 4 ) :  ?>
-										<div class="col-xl-12">
-											<!--begin::List Widget 7-->
-											<div class="card card-custom gutter-b card-stretch">
+										<div class="col-xl-6">
+											<!--begin::List Widget 12-->
+											<div class="card card-custom card-stretch gutter-b">
+												<!--begin::Header-->
 												<div class="card-header border-0">
-													<h3 class="card-title font-weight-bolder text-dark">Minhas Mídias</h3>
-													
+													<h3 class="card-title font-weight-bolder text-dark">Latest Media</h3>
+													<div class="card-toolbar">
+														<div class="dropdown dropdown-inline">
+															<a href="#" class="btn btn-clean btn-hover-light-primary btn-sm btn-icon" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+																<i class="ki ki-bold-more-ver"></i>
+															</a>
+															<div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">
+																<!--begin::Navigation-->
+																<ul class="navi navi-hover">
+																	<li class="navi-header pb-1">
+																		<span class="text-primary text-uppercase font-weight-bold font-size-sm">Add new:</span>
+																	</li>
+																	<li class="navi-item">
+																		<a href="#" class="navi-link">
+																			<span class="navi-icon">
+																				<i class="flaticon2-shopping-cart-1"></i>
+																			</span>
+																			<span class="navi-text">Order</span>
+																		</a>
+																	</li>
+																	<li class="navi-item">
+																		<a href="#" class="navi-link">
+																			<span class="navi-icon">
+																				<i class="flaticon2-calendar-8"></i>
+																			</span>
+																			<span class="navi-text">Event</span>
+																		</a>
+																	</li>
+																	<li class="navi-item">
+																		<a href="#" class="navi-link">
+																			<span class="navi-icon">
+																				<i class="flaticon2-graph-1"></i>
+																			</span>
+																			<span class="navi-text">Report</span>
+																		</a>
+																	</li>
+																	<li class="navi-item">
+																		<a href="#" class="navi-link">
+																			<span class="navi-icon">
+																				<i class="flaticon2-rocket-1"></i>
+																			</span>
+																			<span class="navi-text">Post</span>
+																		</a>
+																	</li>
+																	<li class="navi-item">
+																		<a href="#" class="navi-link">
+																			<span class="navi-icon">
+																				<i class="flaticon2-writing"></i>
+																			</span>
+																			<span class="navi-text">File</span>
+																		</a>
+																	</li>
+																</ul>
+																<!--end::Navigation-->
+															</div>
+														</div>
+													</div>
 												</div>
+												<!--end::Header-->
 												<!--begin::Body-->
-												<div class="card-body pt-0">
+												<div class="card-body pt-2">
 													<!--begin::Item-->
-													<div class="d-flex align-items-center flex-wrap mb-10">
+													<div class="d-flex flex-wrap align-items-center mb-10">
 														<!--begin::Symbol-->
-														<div class="symbol symbol-50 symbol-light mr-5">
-															<span class="symbol-label">
-																<img src="assets/media/svg/misc/006-plurk.svg" class="h-50 align-self-center" alt="" />
-															</span>
+														<div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+															<div class="symbol-label" style="background-image: url('assets/media/stock-600x400/img-20.jpg')"></div>
 														</div>
 														<!--end::Symbol-->
-														<!--begin::Text-->
-														<div class="d-flex flex-column flex-grow-1 mr-2">
-															<a href="#" class="font-weight-bold text-dark-75 text-hover-primary font-size-lg mb-1">Top Authors</a>
-															<span class="text-muted font-weight-bold">Mark, Rowling, Esther</span>
+														<!--begin::Title-->
+														<div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+															<a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">Cup &amp; Green</a>
+															<span class="text-muted font-weight-bold">Size: 87KB</span>
 														</div>
-														<!--end::Text-->
-														<span class="label label-xl label-light label-inline my-lg-0 my-2 text-dark-50 font-weight-bolder">+82$</span>
+														<!--end::Title-->
+														<!--begin::btn-->
+														<span class="label label-lg label-light-primary label-inline mt-lg-0 mb-lg-0 my-2 font-weight-bold py-4">Approved</span>
+														<!--end::Btn-->
 													</div>
 													<!--end::Item-->
 													<!--begin::Item-->
-													<div class="d-flex align-items-center flex-wrap mb-10">
+													<div class="d-flex flex-wrap align-items-center mb-10">
 														<!--begin::Symbol-->
-														<div class="symbol symbol-50 symbol-light mr-5">
-															<span class="symbol-label">
-																<img src="assets/media/svg/misc/015-telegram.svg" class="h-50 align-self-center" alt="" />
-															</span>
+														<div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+															<div class="symbol-label" style="background-image: url('assets/media/stock-600x400/img-19.jpg')"></div>
 														</div>
 														<!--end::Symbol-->
-														<!--begin::Text-->
-														<div class="d-flex flex-column flex-grow-1 mr-2">
-															<a href="#" class="font-weight-bold text-dark-75 text-hover-primary font-size-lg mb-1">Popular Authors</a>
-															<span class="text-muted font-weight-bold">Randy, Steve, Mike</span>
+														<!--begin::Title-->
+														<div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+															<a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">Yellow Background</a>
+															<span class="text-muted font-weight-bold">Size: 1.2MB</span>
 														</div>
-														<!--end::Text-->
-														<span class="label label-xl label-light label-inline my-lg-0 my-2 text-dark-50 font-weight-bolder">+280$</span>
+														<!--end::Title-->
+														<!--begin::btn-->
+														<span class="label label-lg label-light-warning label-inline mt-lg-0 mb-lg-0 my-2 font-weight-bold py-4">In Progress</span>
+														<!--end::Btn-->
 													</div>
 													<!--end::Item-->
 													<!--begin::Item-->
-													<div class="d-flex align-items-center flex-wrap mb-10">
+													<div class="d-flex flex-wrap align-items-center mb-10">
 														<!--begin::Symbol-->
-														<div class="symbol symbol-50 symbol-light mr-5">
-															<span class="symbol-label">
-																<img src="assets/media/svg/misc/003-puzzle.svg" class="h-50 align-self-center" alt="" />
-															</span>
+														<div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+															<div class="symbol-label" style="background-image: url('assets/media/stock-600x400/img-25.jpg')"></div>
 														</div>
 														<!--end::Symbol-->
-														<!--begin::Text-->
-														<div class="d-flex flex-column flex-grow-1 mr-2">
-															<a href="#" class="font-weight-bold text-dark-75 text-hover-primary font-size-lg mb-1">New Users</a>
-															<span class="text-muted font-weight-bold">John, Pat, Jimmy</span>
+														<!--begin::Title-->
+														<div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+															<a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">Nike &amp; Blue</a>
+															<span class="text-muted font-weight-bold">Size: 87KB</span>
 														</div>
-														<!--end::Text-->
-														<span class="label label-xl label-light label-inline my-lg-0 my-2 text-dark-50 font-weight-bolder">+4500$</span>
+														<!--end::Title-->
+														<!--begin::btn-->
+														<span class="label label-lg label-light-success label-inline mt-lg-0 mb-lg-0 my-2 font-weight-bold py-4">Success</span>
+														<!--end::btn-->
 													</div>
 													<!--end::Item-->
 													<!--begin::Item-->
-													<div class="d-flex align-items-center flex-wrap mb-10">
+													<div class="d-flex flex-wrap align-items-center">
 														<!--begin::Symbol-->
-														<div class="symbol symbol-50 symbol-light mr-5">
-															<span class="symbol-label">
-																<img src="assets/media/svg/misc/005-bebo.svg" class="h-50 align-self-center" alt="" />
-															</span>
+														<div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+															<div class="symbol-label" style="background-image: url('assets/media/stock-600x400/img-24.jpg')"></div>
 														</div>
 														<!--end::Symbol-->
-														<!--begin::Text-->
-														<div class="d-flex flex-column flex-grow-1 mr-2">
-															<a href="#" class="font-weight-bold text-dark-75 text-hover-primary font-size-lg mb-1">Active Customers</a>
-															<span class="text-muted font-weight-bold">Sandra, Tim, Louis</span>
+														<!--begin::Title-->
+														<div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+															<a href="#" class="text-dark-75 font-weight-bold font-size-lg text-hover-primary mb-1">Red Boots</a>
+															<span class="text-muted font-weight-bold">Size: 345KB</span>
 														</div>
-														<!--end::Text-->
-														<span class="label label-xl label-light label-inline my-lg-0 my-2 text-dark-50 font-weight-bolder">+4500$</span>
-													</div>
-													<!--end::Item-->
-													<!--begin::Item-->
-													<div class="d-flex align-items-center flex-wrap">
-														<!--begin::Symbol-->
-														<div class="symbol symbol-50 symbol-light mr-5">
-															<span class="symbol-label">
-																<img src="assets/media/svg/misc/014-kickstarter.svg" class="h-50 align-self-center" alt="" />
-															</span>
-														</div>
-														<!--end::Symbol-->
-														<!--begin::Text-->
-														<div class="d-flex flex-column flex-grow-1 mr-2">
-															<a href="#" class="font-weight-bold text-dark-75 text-hover-primary font-size-lg mb-1">Bestseller Theme</a>
-															<span class="text-muted font-weight-bold">Disco, Retro, Sports</span>
-														</div>
-														<!--end::Text-->
-														<span class="label label-xl label-light label-inline my-lg-0 my-2 text-dark-50 font-weight-bolder">+4500$</span>
+														<!--end::Title-->
+														<!--begin::btn-->
+														<span class="label label-lg label-light-danger label-inline mt-lg-0 mb-lg-0 my-2 font-weight-bold py-4">Rejected</span>
+														<!--end::btn-->
 													</div>
 													<!--end::Item-->
 												</div>
 												<!--end::Body-->
 											</div>
-											<!--end::List Widget 7-->
+											<!--end::List Widget 12-->
 										</div>
 									<?php endif; ?>
 									
