8f2d0dc3c6f4531218270227182ed734389ec5a3
Renato Lima <renato.lima1983@gmail.com>
2021-01-07 10:47:02 -0300

    marcação

--- appPonto/cadastro.php
+++ appPonto/cadastro.php
@@ -10,10 +10,10 @@ $Categoria = new Categoria();
 $optionscategoria = $Categoria->listaroptionscategoria();
 ?>
 <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-<!--<script
+<script
       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC9HytuCsyClhgU7vNNo8QHYsGtWiVPBuw&callback=initMap&libraries=&v=weekly"
       defer
-    ></script>-->
+    ></script>
     <style>
         #map {
             height: 100%;
@@ -108,6 +108,7 @@ $optionscategoria = $Categoria->listaroptionscategoria();
 
 //let markers = [];
 function initMap() {
+  alert('iniciou');
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 15,
     center: { lat: -15.849511, lng: -48.022440 },
