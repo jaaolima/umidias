fe50e3a9c9b64bbf164ac34849dd601d05c90208
Renato Lima <victorespucoc@gmail.com>
2021-01-07 15:29:18 -0300

    principal

--- principal.php
+++ principal.php
@@ -1461,7 +1461,7 @@ License: You must have a valid license purchased only from themeforest(the above
 															
 															<span class="card-title font-weight-bolder text-dark-75 font-size-h2 mb-0 mt-6 d-block">$50,000</span>
 															<span class="font-weight-bold text-muted font-size-sm d-block">Investido</span>
-															<div class="d-flex position-absolute"> 
+															<div class="d-flex flex-center"> 
 																<input class="btn btn-secondary" type="button" value="Acompanhar"/>
 															</div>
 															
