ed8953f47f56aa7afab1268ddb3ba7f1c000af5a
Renato Lima <renato.lima1983@gmail.com>
2021-01-07 10:43:41 -0300

    marcação

--- appPonto/cadastro.php
+++ appPonto/cadastro.php
@@ -133,13 +133,13 @@ function clearMarkers() {
 
 // Data for the markers consisting of a name, a LatLng and a zIndex for the
 // order in which these markers should display on top of each other.
-const beaches = [
+/*const beaches = [
   ["Bondi Beach", -33.890542, 151.274856, 4],
   ["Coogee Beach", -33.923036, 151.259052, 5],
   ["Cronulla Beach", -34.028249, 151.157507, 3],
   ["Manly Beach", -33.80010128657071, 151.28747820854187, 2],
   ["Maroubra Beach", -33.950198, 151.259302, 1],
-];
+];*/
 
 function setMarkers(map) {
   // Adds markers to the map.
