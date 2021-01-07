d73321b3c89f4539c7c60f31589e7f132ccb36e8
Renato Lima <victorespucoc@gmail.com>
2021-01-07 15:50:25 -0300

    principal

--- appParceiro/appOutdoor/add_midia.php
+++ appParceiro/appOutdoor/add_midia.php
@@ -69,7 +69,7 @@ License: You must have a valid license purchased only from themeforest(the above
 																</svg>
 																<!--end::Svg Icon-->
 															</span>
-															<h3 class="wizard-title">Project Details</h3>
+															<h3 class="wizard-title">Detalhes da Mídia</h3>
 														</div>
 														<span class="svg-icon svg-icon-xl wizard-arrow">
 															<!--begin::Svg Icon | path:assets/media/svg/icons/Navigation/Arrow-right.svg-->
@@ -96,7 +96,7 @@ License: You must have a valid license purchased only from themeforest(the above
 																</svg>
 																<!--end::Svg Icon-->
 															</span>
-															<h3 class="wizard-title">Settings</h3>
+															<h3 class="wizard-title">Especificação</h3>
 														</div>
 														<span class="svg-icon svg-icon-xl wizard-arrow">
 															<!--begin::Svg Icon | path:assets/media/svg/icons/Navigation/Arrow-right.svg-->
@@ -110,33 +110,7 @@ License: You must have a valid license purchased only from themeforest(the above
 															<!--end::Svg Icon-->
 														</span>
 													</div>
-													<div class="wizard-step" data-wizard-type="step">
-														<div class="wizard-label">
-															<span class="svg-icon svg-icon-4x wizard-icon">
-																<!--begin::Svg Icon | path:assets/media/svg/icons/Home/Globe.svg-->
-																<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-																	<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-																		<rect x="0" y="0" width="24" height="24" />
-																		<path d="M13,18.9450712 L13,20 L14,20 C15.1045695,20 16,20.8954305 16,22 L8,22 C8,20.8954305 8.8954305,20 10,20 L11,20 L11,18.9448245 C9.02872877,18.7261967 7.20827378,17.866394 5.79372555,16.5182701 L4.73856106,17.6741866 C4.36621808,18.0820826 3.73370941,18.110904 3.32581341,17.7385611 C2.9179174,17.3662181 2.88909597,16.7337094 3.26143894,16.3258134 L5.04940685,14.367122 C5.46150313,13.9156769 6.17860937,13.9363085 6.56406875,14.4106998 C7.88623094,16.037907 9.86320756,17 12,17 C15.8659932,17 19,13.8659932 19,10 C19,7.73468744 17.9175842,5.65198725 16.1214335,4.34123851 C15.6753081,4.01567657 15.5775721,3.39010038 15.903134,2.94397499 C16.228696,2.49784959 16.8542722,2.4001136 17.3003976,2.72567554 C19.6071362,4.40902808 21,7.08906798 21,10 C21,14.6325537 17.4999505,18.4476269 13,18.9450712 Z" fill="#000000" fill-rule="nonzero" />
-																		<circle fill="#000000" opacity="0.3" cx="12" cy="10" r="6" />
-																	</g>
-																</svg>
-																<!--end::Svg Icon-->
-															</span>
-															<h3 class="wizard-title">Delivery Details</h3>
-														</div>
-														<span class="svg-icon svg-icon-xl wizard-arrow">
-															<!--begin::Svg Icon | path:assets/media/svg/icons/Navigation/Arrow-right.svg-->
-															<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-																<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-																	<polygon points="0 0 24 0 24 24 0 24" />
-																	<rect fill="#000000" opacity="0.3" transform="translate(12.000000, 12.000000) rotate(-90.000000) translate(-12.000000, -12.000000)" x="11" y="5" width="2" height="14" rx="1" />
-																	<path d="M9.70710318,15.7071045 C9.31657888,16.0976288 8.68341391,16.0976288 8.29288961,15.7071045 C7.90236532,15.3165802 7.90236532,14.6834152 8.29288961,14.2928909 L14.2928896,8.29289093 C14.6714686,7.914312 15.281055,7.90106637 15.675721,8.26284357 L21.675721,13.7628436 C22.08284,14.136036 22.1103429,14.7686034 21.7371505,15.1757223 C21.3639581,15.5828413 20.7313908,15.6103443 20.3242718,15.2371519 L15.0300721,10.3841355 L9.70710318,15.7071045 Z" fill="#000000" fill-rule="nonzero" transform="translate(14.999999, 11.999997) scale(1, -1) rotate(90.000000) translate(-14.999999, -11.999997)" />
-																</g>
-															</svg>
-															<!--end::Svg Icon-->
-														</span>
-													</div>
+													
 													<div class="wizard-step" data-wizard-type="step">
 														<div class="wizard-label">
 															<span class="svg-icon svg-icon-4x wizard-icon">
@@ -150,7 +124,7 @@ License: You must have a valid license purchased only from themeforest(the above
 																</svg>
 																<!--end::Svg Icon-->
 															</span>
-															<h3 class="wizard-title">Review and Submit</h3>
+															<h3 class="wizard-title">Revisão</h3>
 														</div>
 														<span class="svg-icon svg-icon-xl wizard-arrow last">
 															<!--begin::Svg Icon | path:assets/media/svg/icons/Navigation/Arrow-right.svg-->
