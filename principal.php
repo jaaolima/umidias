f0d905d9e1a3edf590a33d99dac6729cb32a7318
Renato Lima <victorespucoc@gmail.com>
2021-01-08 13:59:22 -0300

    principal

--- principal.php
+++ principal.php
@@ -918,13 +918,13 @@ License: You must have a valid license purchased only from themeforest(the above
 										<!--begin::Nav-->
 										<div class="navi navi-spacer-x-0 pt-5">
 											<!--begin::Item-->
-											<a href="custom/apps/user/profile-1/personal-information.html" class="navi-item px-8">
+											<a href="perfil.php" class="navi-item px-8">
 												<div class="navi-link">
 													<div class="navi-icon mr-2">
 														<i class="flaticon2-calendar-3 text-success"></i>
 													</div>
 													<div class="navi-text">
-														<a href="perfil.php"class="font-weight-bold">Meu perfil</a>
+														<div href="perfil.php"class="font-weight-bold">Meu perfil</div>
 														<div class="text-muted">Account settings and more
 														<span class="label label-light-danger label-inline font-weight-bold">update</span></div>
 													</div>
