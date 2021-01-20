acbe64fc67353a5ca30c11464cf9955188ef2d1a
Renato Lima <victorespucoc@gmail.com>
2021-01-20 14:17:34 -0300

    container

--- principal.php
+++ principal.php
@@ -181,7 +181,7 @@ License: You must have a valid license purchased only from themeforest(the above
 						<div class="main d-flex flex-column flex-row-fluid">
 							<div class="content flex-column-fluid" id="conteudo">
 								<div class="mb-8 ">
-									<h2 class="font-weight-bolder">Dashboard</h2>
+									<h1 class="font-weight-bolder">Dashboard</h2>
 								</div>
 								<!--begin::Row-->	
 								<div class="row">
