f2b923f2bcdb067866b14fcd703779a4d14e418b
Renato Lima <victorespucoc@gmail.com>
2021-01-20 14:19:48 -0300

    container

--- principal.php
+++ principal.php
@@ -150,9 +150,7 @@ License: You must have a valid license purchased only from themeforest(the above
 									</a>
 								</li>
 							</ul>
-							<!--end::Menu Nav-->
-						<div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; height: 825px; right: 4px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 300px;"></div></div></div>
-						<!--end::Menu Container-->
+						</div>
 					</div>
 					<!--end::Aside Menu-->
 				</div>
