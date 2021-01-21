cbc52fd45bf309e9c7970fccec71b14233e70a51
Renato Lima <victorespucoc@gmail.com>
2021-01-21 11:01:07 -0300

    container

--- principal.php
+++ principal.php
@@ -325,64 +325,24 @@ License: You must have a valid license purchased only from themeforest(the above
 												<!--begin::Header-->
 												<div class="card-header border-0">
 													<h3 class="card-title font-weight-bolder text-dark">Latest Media</h3>
-													<div class="card-toolbar">
-														<div class="dropdown dropdown-inline">
-															<a href="#" class="btn btn-clean btn-hover-light-primary btn-sm btn-icon" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-																<i class="ki ki-bold-more-ver"></i>
-															</a>
-															<div class="dropdown-menu dropdown-menu-md dropdown-menu-right">
-																<!--begin::Navigation-->
-																<ul class="navi navi-hover">
-																	<li class="navi-header font-weight-bold py-4">
-																		<span class="font-size-lg">Choose Label:</span>
-																		<i class="flaticon2-information icon-md text-muted" data-toggle="tooltip" data-placement="right" title="Click to learn more..."></i>
-																	</li>
-																	<li class="navi-separator mb-3 opacity-70"></li>
-																	<li class="navi-item">
-																		<a href="#" class="navi-link">
-																			<span class="navi-text">
-																				<span class="label label-xl label-inline label-light-success">Customer</span>
-																			</span>
-																		</a>
-																	</li>
-																	<li class="navi-item">
-																		<a href="#" class="navi-link">
-																			<span class="navi-text">
-																				<span class="label label-xl label-inline label-light-danger">Partner</span>
-																			</span>
-																		</a>
-																	</li>
-																	<li class="navi-item">
-																		<a href="#" class="navi-link">
-																			<span class="navi-text">
-																				<span class="label label-xl label-inline label-light-warning">Suplier</span>
-																			</span>
-																		</a>
-																	</li>
-																	<li class="navi-item">
-																		<a href="#" class="navi-link">
-																			<span class="navi-text">
-																				<span class="label label-xl label-inline label-light-primary">Member</span>
-																			</span>
-																		</a>
-																	</li>
-																	<li class="navi-item">
-																		<a href="#" class="navi-link">
-																			<span class="navi-text">
-																				<span class="label label-xl label-inline label-light-dark">Staff</span>
-																			</span>
-																		</a>
-																	</li>
-																	<li class="navi-separator mt-3 opacity-70"></li>
-																	<li class="navi-footer py-4">
-																		<a class="btn btn-clean font-weight-bold btn-sm" href="#">
-																		<i class="ki ki-plus icon-sm"></i>Add new</a>
-																	</li>
-																</ul>
-																<!--end::Navigation-->
-															</div>
-														</div>
-													</div>
+													<table class="table-hover">
+														<thead>
+															<tr>
+																<th>Mídias contratadas</th>
+																<th>Data inicial</th>
+																<th>Data final</th>
+																<th>Valor contratado</th>
+															</tr>
+														</thead>
+														<tbody>
+															<tr>
+																<td>Universo Painéis</td>
+																<td>21/01/2021</td>
+																<td>21/01/2021</td>
+																<td>00,00 RS</td>
+															</tr>
+														</tbody>
+													</table>
 												</div>
 												<!--end::Header-->
 												<!--begin::Body-->
