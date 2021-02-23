6e51bfcd3f1ff0c94de905efcea827e044fe9706
João victor <victorespucoc@gmail.com>
2021-02-23 17:00:55 -0300

    monitorizaçao

--- principal.php
+++ principal.php
@@ -52,7 +52,7 @@ License: You must have a valid license purchased only from themeforest(the above
 		<!--begin::Main-->
 		<div class="d-flex flex-column flex-root">
 			<!--begin::Page-->
-			<div class="d-flex flex-row flex-column-fluid page">
+			<div class="d-flex flex-row flex-column-fluid page bg-grey">
 				<!--incio do sidebar-->
 				<div class="aside aside-left aside-fixed d-flex flex-column flex-row-auto menu-overlay" id="kt_aside">
 					<!--begin::Brand-->
