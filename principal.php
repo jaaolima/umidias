545b5de7a275899d063facbbbe933b19495788b3
João victor <victorespucoc@gmail.com>
2021-02-24 17:30:38 -0300

    principal

--- principal.php
+++ principal.php
@@ -64,17 +64,13 @@ License: You must have a valid license purchased only from themeforest(the above
 						<!--end::Logo-->
 						<!--begin::Toggle-->
 						<button class="brand-toggle btn btn-sm px-0 ml-25 menu-close" id="kt_aside_toggle">
-							<span class="svg-icon svg-icon svg-icon-xl">
-								<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Navigation/Angle-double-left.svg-->
-								<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-									<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-										<polygon points="0 0 24 0 24 24 0 24"></polygon>
-										<path d="M5.29288961,6.70710318 C4.90236532,6.31657888 4.90236532,5.68341391 5.29288961,5.29288961 C5.68341391,4.90236532 6.31657888,4.90236532 6.70710318,5.29288961 L12.7071032,11.2928896 C13.0856821,11.6714686 13.0989277,12.281055 12.7371505,12.675721 L7.23715054,18.675721 C6.86395813,19.08284 6.23139076,19.1103429 5.82427177,18.7371505 C5.41715278,18.3639581 5.38964985,17.7313908 5.76284226,17.3242718 L10.6158586,12.0300721 L5.29288961,6.70710318 Z" fill="#000000" fill-rule="nonzero" transform="translate(8.999997, 11.999999) scale(-1, 1) translate(-8.999997, -11.999999)"></path>
-										<path d="M10.7071009,15.7071068 C10.3165766,16.0976311 9.68341162,16.0976311 9.29288733,15.7071068 C8.90236304,15.3165825 8.90236304,14.6834175 9.29288733,14.2928932 L15.2928873,8.29289322 C15.6714663,7.91431428 16.2810527,7.90106866 16.6757187,8.26284586 L22.6757187,13.7628459 C23.0828377,14.1360383 23.1103407,14.7686056 22.7371482,15.1757246 C22.3639558,15.5828436 21.7313885,15.6103465 21.3242695,15.2371541 L16.0300699,10.3841378 L10.7071009,15.7071068 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" transform="translate(15.999997, 11.999999) scale(-1, 1) rotate(-270.000000) translate(-15.999997, -11.999999)"></path>
-									</g>
-								</svg>
-								<!--end::Svg Icon-->
-							</span>
+							<span class = "svg-icon svg-icon-primary svg-icon-2x"> <! - begin :: Svg Icon | caminho: /var/www/preview.keenthemes.com/metronic/releases/2021-02-01-052524/theme/html/demo8/dist /../ src / media / svg / icons / Code / Right-circle. svg -> <svg xmlns = "http://www.w3.org/2000/svg" xmlns: xlink = "http://www.w3.org/1999/xlink" width = "24px" height = " 24px "viewBox =" 0 0 24 24 "version =" 1.1 ">
+								<g stroke = "none" stroke-width = "1" fill = "none" fill-rule = "evenodd">
+									<rect x = "0" y = "0" largura = "24" altura = "24" />
+									<circle fill = "# 000000" opacity = "0.3" cx = "12" cy = "12" r = "10" />
+									<path d = "M7.96323356,15.1775211 C7.62849853,15.5122561 7.08578582,15.5122561 6.75105079,15.1775211 C6.41631576,14.842786 6.41631576,14.3000733 6.75105079,13.9485819.15.5122561 6.75105079,15.1775211 C6.41631576,14.842786 6.41631576,14.3000733 6.75105079,13.94819.579.847.279.847.887. 847. 867. 847. 847. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 867. 2220465,13.5110121 C18.5710056,13.8308912 18.5945795,14.3730917 18.2747004,14.7220508 C17.9548212,15.0710098 17.4126207,15.0945838 17.0636617,14.14.3730917 18.2747004,14.7220508 C17.9548212,15.0710098 17.4126207,15.0945838 17.0636617,14.7747046 L12.525700700 "fill573" non-6-7000 "fill573" non-6167000 "fill5.606" non-6176167000 "fill573. transform = "translate (12.500001, 12.000001) rotate (-270.000000) translate (-12.500001, -12.000001)" />
+								</g>
+							</svg> <! - end :: Svg Icon -> </span>
 						</button>
 						<!--end::Toolbar-->
 					</div>
