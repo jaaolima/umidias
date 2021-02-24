3f5caa311efdd429f4c74856594ce63cae84052b
João victor <victorespucoc@gmail.com>
2021-02-24 14:33:52 -0300

    principal

--- principal.php
+++ principal.php
@@ -158,21 +158,6 @@ License: You must have a valid license purchased only from themeforest(the above
 				<div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
 					<!--begin::Container-->
 					<div class="d-flex flex-row flex-column-fluid ">
-						<div class="container-fluid d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap m-0">
-							<a href="#" class="btn btn-sm btn-light font-weight-bold mr-2" id="kt_dashboard_daterangepicker" data-toggle="tooltip" title="" data-placement="left" data-original-title="Select dashboard daterange">
-								<span class="text-muted font-size-base font-weight-bold mr-2" id="kt_dashboard_daterangepicker_title"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Hoje: </font></font></span>
-								<span class="text-primary font-size-base font-weight-bolder" id="kt_dashboard_daterangepicker_date"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">19 de janeiro</font></font></span>
-							</a>
-						</div>
-						<div class="topbar-item">
-							<div class="btn btn-icon btn-icon-mobile w-auto btn-clean d-flex align-items-center btn-lg px-2" id="kt_quick_user_toggle">
-								<span class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Oi,</font></font></span>
-								<span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Sean</font></font></span>
-								<span class="symbol symbol-lg-35 symbol-25 symbol-light-success">
-									<span class="symbol-label font-size-h5 font-weight-bold"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">S</font></font></span>
-								</span>
-							</div>
-						</div>
 						<!--begin::Content Wrapper-->
 						<div class="main d-flex flex-column flex-row-fluid">
 							<div class="content flex-column-fluid" id="conteudo">
