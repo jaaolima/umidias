9adeb33d0e54d99e133aa9ff6574150f42b02469
João victor <victorespucoc@gmail.com>
2021-02-24 14:59:22 -0300

    principal

--- principal.php
+++ principal.php
@@ -85,8 +85,8 @@ License: You must have a valid license purchased only from themeforest(the above
 						<div id="kt_aside_menu" class="aside-menu my-4 scroll ps ps--active-y" data-menu-vertical="1" data-menu-scroll="1" data-menu-dropdown-timeout="500" style="height: 825px; overflow: hidden;">
 							<!--begin::Menu Nav-->
 							<ul class="menu-nav nav-pills">
-								<li class="menu-item my-4 botao-menu" aria-haspopup="true">
-									<a href="principal.php" class="btn rounded ml-2 mr-10 text-left">
+								<li class="menu-item my-4 botao-menu mr-30 btn" aria-haspopup="true">
+									<a href="principal.php" class="btn rounded ml-2 text-left">
 										<span class="svg-icon menu-icon">
 											<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Design/Layers.svg-->
 											<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
@@ -101,8 +101,8 @@ License: You must have a valid license purchased only from themeforest(the above
 										<span class="menu-text"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Dashboard</font></font></span>
 									</a>
 								</li>
-								<li class="menu-item my-4 botao-menu" aria-haspopup="true">
-									<a href="appCliente/listar_tipo.php" class="btn rounded ml-2 mr-30 text-left">
+								<li class="menu-item my-4 botao-menu mr-30 btn" aria-haspopup="true">
+									<a href="appCliente/listar_tipo.php" class="btn rounded ml-2 text-left">
 										<span class="svg-icon menu-icon">
 											<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Design/Layers.svg-->
 											<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
@@ -117,8 +117,8 @@ License: You must have a valid license purchased only from themeforest(the above
 										<span class="menu-text"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Alugar mídia</font></font></span>
 									</a>
 								</li>
-								<li class="menu-item my-4 botao-menu" aria-haspopup="true">
-									<a href="appCliente/listar_minhas_midias.php" class="btn rounded ml-2 mr-20 text-left">
+								<li class="menu-item my-4 botao-menu mr-30 btn" aria-haspopup="true">
+									<a href="appCliente/listar_minhas_midias.php" class="btn rounded ml-2 text-left">
 										<span class="svg-icon menu-icon">
 											<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Design/Layers.svg-->
 											<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
@@ -133,8 +133,8 @@ License: You must have a valid license purchased only from themeforest(the above
 										<span class="menu-text"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Mídias alugadas</font></font></span>
 									</a>
 								</li>
-								<li class="menu-item my-4 botao-menu" aria-haspopup="true">
-									<a href="/metronic/demo1/index.html" class="btn rounded ml-2 mr-30 text-left">
+								<li class="menu-item my-4 botao-menu mr-30 btn" aria-haspopup="true">
+									<a href="/metronic/demo1/index.html" class="btn rounded ml-2 text-left">
 										<span class="svg-icon menu-icon">
 											<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Design/Layers.svg-->
 											<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
