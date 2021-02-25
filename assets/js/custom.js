a00a8b56a47b76fa744f8ce1ada3a10eb1f3e530
João victor <victorespucoc@gmail.com>
2021-02-25 09:33:06 -0300

    principal

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -103,16 +103,8 @@ $(document).on('click', function(e) { //Hide menu when clicked outside menu area
 	}
 });
 
-$(document).ready(function(){
-  var get = document.URL;
-  if(get.match(/principal/i)){
-      $('.dashboard-botao').addClass('botao-menu-active');
-      $('.dashboard-texto').addClass('texto-menu-active');
-  }
-  if(get.match(/cargo/i)){
-      $('.aw-menu li:eq(1)').addClass('is-selected');
-  }
-  if(get.match(/setor/i)){
-      $('.aw-menu li:eq(2)').addClass('is-selected');
-  }
+$('.dashboard-botao').click(function (e)
+{
+  $('.dashboard-botao').removeClass('botao-menu');
+  $(this).addClass('botao-menu-active');
 });
