a987de7e8fdf7f0805b80a17167aa9493c1c77fb
João victor <victorespucoc@gmail.com>
2021-02-25 17:04:58 -0300

    principal

--- principal.php
+++ principal.php
@@ -115,16 +115,14 @@ License: You must have a valid license purchased only from themeforest(the above
 					<div class="d-flex flex-row flex-column-fluid ">
 						<!--begin::Content Wrapper-->
 						<div class="main d-flex flex-column flex-row-fluid">
-							<div class="container-fluid d-flex align-items-stretch justify-content-between">
-								<div class="topbar">
-									<div class="topbar-item">
-										<div class="btn btn-icon btn-icon-mobile w-auto btn-clean d-flex align-items-center btn-lg px-2" id="kt_quick_user_toggle">
-											<span class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1">Hi,</span>
-											<span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">Sean</span>
-											<span class="symbol symbol-lg-35 symbol-25 symbol-light-success">
-												<span class="symbol-label font-size-h5 font-weight-bold">S</span>
-											</span>
-										</div>
+							<div class="topbar">
+								<div class="topbar-item">
+									<div class="btn btn-icon btn-icon-mobile w-auto btn-clean d-flex align-items-center btn-lg px-2" id="kt_quick_user_toggle">
+										<span class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1">Hi,</span>
+										<span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">Sean</span>
+										<span class="symbol symbol-lg-35 symbol-25 symbol-light-success">
+											<span class="symbol-label font-size-h5 font-weight-bold">S</span>
+										</span>
 									</div>
 								</div>
 							</div>
