e2b4c1699649e71de721e35d5db350fecfcd2cfb
João victor <victorespucoc@gmail.com>
2021-02-25 09:55:44 -0300

    principal

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -108,5 +108,5 @@ $('.botao-menu').click(function (e)
 		$('.botao-menu').removeClass('botao-menu-active');
 		$(this).addClass('botao-menu-active');
     $('.texto-menu').removeClass('texto-menu-active');
-		$('.texto-menu').addClass('texto-menu-active');
+		$(this).addClass('texto-menu-active');
 	});
