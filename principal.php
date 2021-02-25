46cbb36685b6598bb6b6b45e1cb24d2b17004096
João victor <victorespucoc@gmail.com>
2021-02-25 16:58:59 -0300

    principal

--- principal.php
+++ principal.php
@@ -116,13 +116,15 @@ License: You must have a valid license purchased only from themeforest(the above
 						<!--begin::Content Wrapper-->
 						<div class="main d-flex flex-column flex-row-fluid">
 							<div>
-								<div class="topbar-item">
-									<div class="btn btn-icon btn-icon-mobile w-auto btn-clean d-flex align-items-center btn-lg px-2" id="kt_quick_user_toggle">
-										<span class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Oi,</font></font></span>
-										<span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Sean</font></font></span>
-										<span class="symbol symbol-lg-35 symbol-25 symbol-light-success">
-											<span class="symbol-label font-size-h5 font-weight-bold"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">S</font></font></span>
-										</span>
+								<div class="topbar">
+									<div class="topbar-item">
+										<div class="btn btn-icon btn-icon-mobile w-auto btn-clean d-flex align-items-center btn-lg px-2" id="kt_quick_user_toggle">
+											<span class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Oi,</font></font></span>
+											<span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Sean</font></font></span>
+											<span class="symbol symbol-lg-35 symbol-25 symbol-light-success">
+												<span class="symbol-label font-size-h5 font-weight-bold"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">S</font></font></span>
+											</span>
+										</div>
 									</div>
 								</div>
 							</div>
