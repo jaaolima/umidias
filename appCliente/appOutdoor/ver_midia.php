de440567289cab0b5225cb3fc7c0b2fa39527c73
João victor <victorespucoc@gmail.com>
2021-03-02 17:22:27 -0300

    principal

--- appCliente/appOutdoor/ver_midia.php
+++ appCliente/appOutdoor/ver_midia.php
@@ -77,15 +77,15 @@ License: You must have a valid license purchased only from themeforest(the above
 												<span>St.Central - Taguatinga, Brasilia - DF, 40297-400</span>
 											</div>
 											<div class="my-10">
-												<span class="texto-negrito">Descrição </span>
+												<h3 class="texto-negrito">Descrição </h3>
 												<span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quibusdam adipisci vitae provident quo deserunt ipsum quidem fugit mollitia, repudiandae delectus! Blanditiis animi sunt possimus officiis ipsam unde quo obcaecati?</span>
 											</div>
 											<div class="my-10">
-												<span class="texto-negrito">Tamanho</span>
+												<h3 class="texto-negrito">Tamanho</h3>
 												<span>6,50 x 10,2</span>
 											</div>
 											<div class="my-10">
-												<span class="texto-negrito">Latitude e Longitude</span>
+												<h3 class="texto-negrito">Latitude e Longitude</h3>
 												<span>00000000000 000000000</span>
 											</div>
 											
