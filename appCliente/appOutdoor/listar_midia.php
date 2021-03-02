212ae70553c18d0a0788b956f203c49fdd0be4b5
João victor <victorespucoc@gmail.com>
2021-03-02 16:30:27 -0300

    principal

--- appCliente/appOutdoor/listar_midia.php
+++ appCliente/appOutdoor/listar_midia.php
@@ -102,7 +102,7 @@ License: You must have a valid license purchased only from themeforest(the above
 													<span class="text-dark font-weight-bold text-dark font-size-h4">Alcance</span><br>
 													<span class=" texto-fraco font-size-h6">15.456 pessoas</span>
 												</div>
-												<div class="my-4 mx-4 text-end">
+												<div class="my-4 mx-4 text-right">
 													<a href="appCliente/appOutdoor/ver_midia.php" class="text-primary">Detalhes</a>
 													<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 														<path d="M5 12H19" stroke="#B721FF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
