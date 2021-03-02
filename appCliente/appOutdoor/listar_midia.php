36c7f0c810ab35f6fb14f0ad63005a2a02930d23
João victor <victorespucoc@gmail.com>
2021-03-02 16:22:49 -0300

    principal

--- appCliente/appOutdoor/listar_midia.php
+++ appCliente/appOutdoor/listar_midia.php
@@ -84,7 +84,7 @@ License: You must have a valid license purchased only from themeforest(the above
 												</div>
 												<!--end::User-->
 												<!--begin::Name-->
-												<div class="my-4 mx-15">
+												<div class="my-4 mx-15 text-left">
 													<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 														<path d="M19.7273 10.3636C19.7273 16.0909 12.3636 21 12.3636 21C12.3636 21 5 16.0909 5 10.3636C5 8.41068 5.77581 6.53771 7.15676 5.15676C8.53771 3.77581 10.4107 3 12.3636 3C14.3166 3 16.1896 3.77581 17.5705 5.15676C18.9515 6.53771 19.7273 8.41068 19.7273 10.3636Z" stroke="#57616A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 														<path d="M12.3636 12.8183C13.7192 12.8183 14.8181 11.7193 14.8181 10.3637C14.8181 9.00812 13.7192 7.90918 12.3636 7.90918C11.008 7.90918 9.90906 9.00812 9.90906 10.3637C9.90906 11.7193 11.008 12.8183 12.3636 12.8183Z" stroke="#57616A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -92,15 +92,15 @@ License: You must have a valid license purchased only from themeforest(the above
 													<a href="appCliente/appOutdoor/ver_midia.php"class="text-dark font-weight-bold text-dark font-size-h4">St.Central - Taguatinga, Brasilia - DF, 40297-400</a>
 													<p class="texto-fraco font-size-h6">Nexa - Next generation SAAS</p>
 												</div>
-												<div class="my-4 mx-15">
+												<div class="my-4 mx-15 text-left">
 													<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 														<path d="M17 21V19C17 17.9391 16.5786 16.9217 15.8284 16.1716C15.0783 15.4214 14.0609 15 13 15H5C3.93913 15 2.92172 15.4214 2.17157 16.1716C1.42143 16.9217 1 17.9391 1 19V21" stroke="#57616A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 														<path d="M9 11C11.2091 11 13 9.20914 13 7C13 4.79086 11.2091 3 9 3C6.79086 3 5 4.79086 5 7C5 9.20914 6.79086 11 9 11Z" stroke="#57616A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 														<path d="M23 20.9999V18.9999C22.9993 18.1136 22.7044 17.2527 22.1614 16.5522C21.6184 15.8517 20.8581 15.3515 20 15.1299" stroke="#57616A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 														<path d="M16 3.12988C16.8604 3.35018 17.623 3.85058 18.1676 4.55219C18.7122 5.2538 19.0078 6.11671 19.0078 7.00488C19.0078 7.89305 18.7122 8.75596 18.1676 9.45757C17.623 10.1592 16.8604 10.6596 16 10.8799" stroke="#57616A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 													</svg>
-													<p class="text-dark font-weight-bold text-dark font-size-h4">Alcance</p>
-													<p class=" texto-fraco font-size-h6">15.456 pessoas</p>
+													<span class="text-dark font-weight-bold text-dark font-size-h4">Alcance</span>
+													<span class=" texto-fraco font-size-h6">15.456 pessoas</span>
 												</div>
 												<!--end::Name-->
 											</div>
