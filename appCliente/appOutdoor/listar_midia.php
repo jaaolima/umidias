fac0f7b60baf9ac98e01a4538c06ef95f697a2b4
João victor <victorespucoc@gmail.com>
2021-03-02 16:37:50 -0300

    principal

--- appCliente/appOutdoor/listar_midia.php
+++ appCliente/appOutdoor/listar_midia.php
@@ -81,32 +81,35 @@ License: You must have a valid license purchased only from themeforest(the above
 												<!--begin::User-->
 												<div class="" >
 													<img class="img-midia rounded-top"src="assets/media/project-logos/3.png" alt="image" />
-													<svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-														<g filter="url(#filter0_d)">
-															<circle cx="32" cy="28" r="28" fill="white"/>
-														</g>
-														<g clip-path="url(#clip0)">
-															<path d="M43.9316 30.7356C43.5651 25.9631 41.3431 22.9724 39.3828 20.3333C37.5677 17.8901 36 15.7802 36 12.6678C36 12.4178 35.86 12.1893 35.638 12.0747C35.4153 11.9595 35.1477 11.9784 34.9453 12.1255C32.0013 14.2321 29.5449 17.7827 28.6869 21.1704C28.0912 23.529 28.0124 26.1805 28.0013 27.9317C25.2826 27.351 24.6667 23.2842 24.6602 23.2399C24.6296 23.029 24.5007 22.8454 24.3132 22.7452C24.1238 22.6462 23.9011 22.6391 23.709 22.7341C23.5664 22.8031 20.2096 24.5087 20.0143 31.3188C20.0006 31.5454 20 31.7726 20 31.9998C20 38.6159 25.3834 43.999 32 43.999C32.0091 43.9996 32.0189 44.0009 32.0267 43.999C32.0293 43.999 32.0319 43.999 32.0351 43.999C38.6354 43.9801 44 38.6042 44 31.9998C44 31.6671 43.9316 30.7356 43.9316 30.7356ZM32 42.6657C29.7943 42.6657 28 40.7544 28 38.405C28 38.3249 27.9994 38.2442 28.0052 38.1452C28.0319 37.1544 28.2201 36.4781 28.4264 36.0282C28.8132 36.8589 29.5046 37.6225 30.6276 37.6225C30.9961 37.6225 31.2943 37.3243 31.2943 36.9559C31.2943 36.0067 31.3139 34.9117 31.5502 33.9235C31.7605 33.0472 32.2631 32.115 32.8998 31.3677C33.183 32.3377 33.7351 33.1227 34.2742 33.889C35.0457 34.9852 35.8432 36.1187 35.9832 38.0515C35.9916 38.1661 36.0001 38.2813 36.0001 38.405C36 40.7544 34.2057 42.6657 32 42.6657Z" fill="url(#paint0_linear)"/>
-														</g>
-														<defs>
-															<filter id="filter0_d" x="0" y="0" width="64" height="64" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-																<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-																<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
-																<feOffset dy="4"/>
-																<feGaussianBlur stdDeviation="2"/>
-																<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12 0"/>
-																<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
-																<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
-															</filter>
-															<linearGradient id="paint0_linear" x1="20" y1="28.1154" x2="43.9951" y2="28.1154" gradientUnits="userSpaceOnUse">
-																<stop stop-color="#FF5C00"/>
-																<stop offset="1" stop-color="#FFD600"/>
-															</linearGradient>
-															<clipPath id="clip0">
-																<rect width="32" height="32" fill="white" transform="translate(16 12)"/>
-															</clipPath>
-														</defs>
-													</svg>
+													<div>
+														<svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+															<g filter="url(#filter0_d)">
+																<circle cx="32" cy="28" r="28" fill="white"/>
+															</g>
+															<g clip-path="url(#clip0)">
+																<path d="M43.9316 30.7356C43.5651 25.9631 41.3431 22.9724 39.3828 20.3333C37.5677 17.8901 36 15.7802 36 12.6678C36 12.4178 35.86 12.1893 35.638 12.0747C35.4153 11.9595 35.1477 11.9784 34.9453 12.1255C32.0013 14.2321 29.5449 17.7827 28.6869 21.1704C28.0912 23.529 28.0124 26.1805 28.0013 27.9317C25.2826 27.351 24.6667 23.2842 24.6602 23.2399C24.6296 23.029 24.5007 22.8454 24.3132 22.7452C24.1238 22.6462 23.9011 22.6391 23.709 22.7341C23.5664 22.8031 20.2096 24.5087 20.0143 31.3188C20.0006 31.5454 20 31.7726 20 31.9998C20 38.6159 25.3834 43.999 32 43.999C32.0091 43.9996 32.0189 44.0009 32.0267 43.999C32.0293 43.999 32.0319 43.999 32.0351 43.999C38.6354 43.9801 44 38.6042 44 31.9998C44 31.6671 43.9316 30.7356 43.9316 30.7356ZM32 42.6657C29.7943 42.6657 28 40.7544 28 38.405C28 38.3249 27.9994 38.2442 28.0052 38.1452C28.0319 37.1544 28.2201 36.4781 28.4264 36.0282C28.8132 36.8589 29.5046 37.6225 30.6276 37.6225C30.9961 37.6225 31.2943 37.3243 31.2943 36.9559C31.2943 36.0067 31.3139 34.9117 31.5502 33.9235C31.7605 33.0472 32.2631 32.115 32.8998 31.3677C33.183 32.3377 33.7351 33.1227 34.2742 33.889C35.0457 34.9852 35.8432 36.1187 35.9832 38.0515C35.9916 38.1661 36.0001 38.2813 36.0001 38.405C36 40.7544 34.2057 42.6657 32 42.6657Z" fill="url(#paint0_linear)"/>
+															</g>
+															<defs>
+																<filter id="filter0_d" x="0" y="0" width="64" height="64" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+																	<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+																	<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+																	<feOffset dy="4"/>
+																	<feGaussianBlur stdDeviation="2"/>
+																	<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12 0"/>
+																	<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+																	<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+																</filter>
+																<linearGradient id="paint0_linear" x1="20" y1="28.1154" x2="43.9951" y2="28.1154" gradientUnits="userSpaceOnUse">
+																	<stop stop-color="#FF5C00"/>
+																	<stop offset="1" stop-color="#FFD600"/>
+																</linearGradient>
+																<clipPath id="clip0">
+																	<rect width="32" height="32" fill="white" transform="translate(16 12)"/>
+																</clipPath>
+															</defs>
+														</svg>
+														<span>Alugadas 345 vezes </span>
+													</div>
 												</div>
 												<!--end::User-->
 												<!--begin::Name-->
