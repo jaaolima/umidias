5afdfbec60361efa75d57aba16c9ce87f5c93cc2
João victor <victorespucoc@gmail.com>
2021-03-04 15:59:48 -0300

    principal

--- appCliente/appOutdoor/ver_midia.php
+++ appCliente/appOutdoor/ver_midia.php
@@ -118,7 +118,7 @@ License: You must have a valid license purchased only from themeforest(the above
 											<div class="separator separator-solid"></div>
 											<div class="my-6 mx-6">
 												<div class="d-flex">
-													<div class="ml-n4 col-8">
+													<div class="ml-n4 col-10">
 														<span>Valor bissemanal da mídia</span>
 														<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 															<path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#BDBDBD" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -131,7 +131,7 @@ License: You must have a valid license purchased only from themeforest(the above
 													</div>
 												</div>												
 												<div class="d-flex">
-													<div class="ml-n4 col-8">
+													<div class="ml-n4 col-10">
 														<span>Taxa de serviço</span>
 														<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 															<path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#BDBDBD" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
