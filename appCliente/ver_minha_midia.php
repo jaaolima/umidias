bb6c7a03fe0e88de2d02df6d6adf26dc876e3c44
João victor <victorespucoc@gmail.com>
2021-03-04 16:40:09 -0300

    principal

--- appCliente/ver_minha_midia.php
+++ appCliente/ver_minha_midia.php
@@ -94,15 +94,14 @@ License: You must have a valid license purchased only from themeforest(the above
                                             <h3 class="texto-negrito">Contrato</h3>
                                             <span class="">Valor: 10.000,00</span><br>
                                             <span class="">Forma de pagamento: 10.000,00 em 2x</span><br>
-                                            <div class="mt-4">
+                                            <div class="mt-8">
                                                 <a class="text-primary ">Baixar segunda via da nota fiscal</a>
-                                            </div>                                            
-                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15" stroke="#B721FF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                                <path d="M7 10L12 15L17 10" stroke="#B721FF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                                <path d="M12 15V3" stroke="#B721FF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                            </svg>
-
+                                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15" stroke="#B721FF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    <path d="M7 10L12 15L17 10" stroke="#B721FF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    <path d="M12 15V3" stroke="#B721FF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </svg>
+                                            </div>                                                                                        
                                         </div>	
                                         <div class="my-12">
                                             <h3 class="texto-negrito">Arte</h3>
