afef99f8c97abd162eae20523b9ef7edc6db03b4
João victor <victorespucoc@gmail.com>
2021-03-05 17:11:40 -0300

    index

--- appCliente/perfil.php
+++ appCliente/perfil.php
@@ -55,9 +55,9 @@ License: You must have a valid license purchased only from themeforest(the above
                                             <div class="my-6">
                                                 <h4 class="font-weight-bolder">Perfil</h4>
                                             </div>
-                                            <div class="my-6 overlay">
-                                                <span class="symbol symbol-circle symbol-80 symbol-light-success overlay-wrapper">
-													<span class="symbol-label font-size-h5 font-weight-bold">S</span>                                               
+                                            <div class="my-6 ">
+                                                <span class="symbol symbol-circle symbol-80 symbol-light-success overlay ">
+													<span class="symbol-label font-size-h5 font-weight-bold overlay-wrapper">S</span>                                               
                                                 </span>
 												<button class="overlay-layer">
 
