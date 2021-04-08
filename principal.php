125833f3d1bb87d1b54d593939035c4a491c77bd
Joao victor <victorespucoc@gmail.com>
2021-04-08 16:02:07 -0300

    Tipo de midia

--- principal.php
+++ principal.php
@@ -164,8 +164,8 @@ License: You must have a valid license purchased only from themeforest(the above
 									<div class="topbar-item">
 										<div class=" w-auto btn-clean d-flex align-items-center px-2" id="kt_quick_user_toggle">
 											<div class="text-right">
-												<span class="text-dark font-weight-bolder font-size-base d-none d-md-inline mr-3">Olá, <?php $dadosUsuario['ds_nome'] ?></span><br>
-												<span class="text-dark font-weight-bold font-size-base d-flex d-md-inline mr-1"><?php $dadosUsuario['ds_email'] ?></span>
+												<span class="text-dark font-weight-bolder font-size-base d-none d-md-inline mr-3">Olá, <?php echo $dadosUsuario['ds_nome'] ?></span><br>
+												<span class="text-dark font-weight-bold font-size-base d-flex d-md-inline mr-1"><?php echo $dadosUsuario['ds_email'] ?></span>
 											</div>
 											<span class="symbol symbol-lg-35 symbol-circle symbol-25 symbol-light-success">
 												<img src="assets/media/maria.jpg" class="symbol-label"alt="">
