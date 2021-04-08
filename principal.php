5e394135cdb5e32bbd3d5a689e942adb27287ad4
Joao victor <victorespucoc@gmail.com>
2021-04-08 15:39:03 -0300

    Tipo de midia

--- principal.php
+++ principal.php
@@ -11,7 +11,7 @@
    }
    
 	$midia = new Midia();
-	$dados = $midia->listarTipoMidia($_POST);
+	$retorno = $midia->listarTipoMidia($_POST);
 ?>
 <!DOCTYPE html>
 <!--
@@ -266,6 +266,24 @@ License: You must have a valid license purchased only from themeforest(the above
 																	<h3  class="titulo-div">Alugue sua mídia</h3>
 																</div>
 																<div class="row m-0 col-12" >
+																	<?php 
+																		while($dados = $retorno->fetch())
+																		{
+																			echo '<div class="col-3 bg-white pl-20 py-6 mb-10 mt-7 border-right pr-20" style="text-align: center;">
+																						<span class="svg-icon svg-icon-3x svg-icon-white d-block my-2">
+																							<svg width="50" height="46" viewBox="0 0 50 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+																								<path d="M44.2 1H5.8C3.14903 1 1 3.14903 1 5.8V29.8C1 32.451 3.14903 34.6 5.8 34.6H44.2C46.851 34.6 49 32.451 49 29.8V5.8C49 3.14903 46.851 1 44.2 1Z" stroke="#333333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+																								<path d="M20.2 44.2H29.8" stroke="#333333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+																								<path d="M25 34.6001V44.2001" stroke="#333333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+																							</svg>
+																						</span>
+																						<p  class="titulo-midia d-block ">'$dados["ds_nome"]'</p>
+																						<a href="appCliente/appOutdoor/listar_midia.php" class="btn btn-primary" type="button" >Alugar mídia</a>
+																					</div>';
+
+																		}
+																	
+																	?>
 																	<div class="col-3 bg-white pl-20 py-6 mb-10 mt-7 border-right pr-20" style="text-align: center;">
 																		<span class="svg-icon svg-icon-3x svg-icon-white d-block my-2">
 																			<svg width="50" height="46" viewBox="0 0 50 46" fill="none" xmlns="http://www.w3.org/2000/svg">
