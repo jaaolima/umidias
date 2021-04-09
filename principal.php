bd271ad4000cae7a26d7e001dadac8b4778573b4
Joao victor <victorespucoc@gmail.com>
2021-04-09 17:26:03 -0300

    Tipo de midia

--- principal.php
+++ principal.php
@@ -258,9 +258,9 @@ License: You must have a valid license purchased only from themeforest(the above
 																		while($dados = $retorno->fetch())
 																		{ 
 																			$tamanho = 100/ count($dados);
-																			$end = end($dados);
+																			/*$end = end($dados);
 																			var_dump(current($dados));
-																			/*$borda = if()){"border-right"};*/
+																			$borda = if()){"border-right"};*/
 																			echo '<div class="bg-white pl-20 py-6 mb-10 mt-7  pr-20 " style="text-align: center; width:'.$tamanho.'% ;">
 																						<span class="svg-icon svg-icon-3x svg-icon-white d-block my-2">
 																							<svg width="50" height="46" viewBox="0 0 50 46" fill="none" xmlns="http://www.w3.org/2000/svg">
