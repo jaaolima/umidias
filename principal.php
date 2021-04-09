dfa27f1ede87010f9793c7591f97b7dccb567653
Joao victor <victorespucoc@gmail.com>
2021-04-09 16:40:47 -0300

    Tipo de midia

--- principal.php
+++ principal.php
@@ -254,8 +254,8 @@ License: You must have a valid license purchased only from themeforest(the above
 																</div>
 																<div class="row m-0 col-12 justify-content-center" >
 																	<?php 
-																		$tamanho = 100/ count($dados);
-																		while($dados = $retorno->fetch())
+																		
+																		while($dados = $retorno->fetch(), $tamanho = 100/ count($dados))
 																		{ 
 																			echo '<div class="bg-white pl-20 py-6 mb-10 mt-7 border-right pr-20" style="text-align: center; width:'.$tamanho.'% ;">
 																						<span class="svg-icon svg-icon-3x svg-icon-white d-block my-2">
