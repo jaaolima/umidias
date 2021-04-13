ec4e79cbeabf56701c68c22ebf8fe58b32dcd091
Joao victor <victorespucoc@gmail.com>
2021-04-13 14:36:30 -0300

    principal

--- principal.php
+++ principal.php
@@ -251,13 +251,13 @@ License: You must have a valid license purchased only from themeforest(the above
 																</div>
 																<div class="row m-0 col-12 justify-content-center" >
 																	<?php 
-																		$dados = $retorno->fetch();
+																		
+																		
+																		while($dados = $retorno->fetch())
+																		{ 
 																		$tamanho = 100/ count($dados);
 																		/*$end = end($dados);
 																		$borda = if($end){"border-right"};*/
-																		while($dados)
-																		{ 
-																			
 																			echo '<div class="bg-white pl-20 py-6 mb-10 mt-7  pr-20 " style="text-align: center; width:'.$tamanho.'% ;">
 																						<span class="svg-icon svg-icon-3x svg-icon-white d-block my-2">
 																							<svg width="50" height="46" viewBox="0 0 50 46" fill="none" xmlns="http://www.w3.org/2000/svg">
