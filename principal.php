1c0f6dae3c07d684bbab5e8ed0f3286edaec3282
Joao victor <victorespucoc@gmail.com>
2021-04-14 13:38:09 -0300

    menu

--- principal.php
+++ principal.php
@@ -254,7 +254,7 @@ License: You must have a valid license purchased only from themeforest(the above
 																		while($dados = $retorno->fetch())
 																		{ 
 																		$valor = count($dados) -1;
-																		echo end($dados);
+																		/*echo end($dados);*/
 																		$tamanho = 100/ $valor);
 																		
 																			echo '<div class="bg-white pl-20 py-6 mb-10 mt-7  pr-20" style="text-align: center; width:'.$tamanho.'% ;">
