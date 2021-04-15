115c024c54c830003c6d2df845c1c36553b28615
João Victor <victorespucoc@gmail.com>
2021-04-15 17:25:27 -0300

    menu

--- appCliente/perfil.php
+++ appCliente/perfil.php
@@ -37,13 +37,11 @@ License: You must have a valid license purchased only from themeforest(the above
 		<!--begin::Global Theme Styles(used by all pages)-->
 		<link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
 		<link href="assets/plugins/custom/prismjs/prismjs.bundle.css" rel="stylesheet" type="text/css" />
-		<link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+		<!--<link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />-->
 		<!--end::Global Theme Styles-->
 		<!--begin::Layout Themes(used by all pages)-->
 		<!--end::Layout Themes-->
 		<link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 	</head>
 	<!--end::Head-->
 	<!--begin::Body-->
