19cc88fa8b3264c77f90440d44c35fc558475d04
João Victor <victorespucoc@gmail.com>
2021-04-15 17:22:20 -0300

    menu

--- appCliente/perfil.php
+++ appCliente/perfil.php
@@ -44,7 +44,6 @@ License: You must have a valid license purchased only from themeforest(the above
 		<link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-		<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 	</head>
 	<!--end::Head-->
 	<!--begin::Body-->
