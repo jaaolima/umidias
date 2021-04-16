7d6e0d0e529f8430f0f873f292b8894e3a6346d0
João Victor <victorespucoc@gmail.com>
2021-04-16 17:05:58 -0300

    dropdown

--- appCliente/perfil.php
+++ appCliente/perfil.php
@@ -89,8 +89,15 @@ License: You must have a valid license purchased only from themeforest(the above
 															<div class="">
 																<h3 class="texto-negrito">Trocar imagem do perfil</h3>
 															</div>
-															<div>
-
+															<div class="dropzone text-center" >
+																<div>
+																	<svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+																		<path d="M40 80C62.0914 80 80 62.0914 80 40C80 17.9086 62.0914 0 40 0C17.9086 0 0 17.9086 0 40C0 62.0914 17.9086 80 40 80Z" fill="#B721FF"/>
+																	</svg>
+																</div>
+																<div>
+																	<p>Arraste e solte a sua imagem aqui</p>
+																</div>
 															</div>
 															<div>
 																<p class="texto-fraco">ou</p>
