540956988e3be6cd9bdc1dbaec6142762ddb5a3b
João Victor <victorespucoc@gmail.com>
2021-04-16 16:08:26 -0300

    dropdown

--- principal.php
+++ principal.php
@@ -255,8 +255,8 @@ License: You must have a valid license purchased only from themeforest(the above
 																		{ 
 																		$valor = count($dados) -1;
 																		$tamanho = 100/ $valor;
-																			var_dump(end($dados));
-																			if($dados == 2)){ 
+																		var_dump(end($dados));
+																			if($dados["id_midia"] == 2)){ 
 																				echo '<div class="bg-white pl-20 py-6 mb-10 mt-7  pr-20" style="text-align: center; width:'.$tamanho.'%  ;">
 																							<span class="svg-icon svg-icon-3x svg-icon-white d-block my-2">
 																								<svg width="50" height="46" viewBox="0 0 50 46" fill="none" xmlns="http://www.w3.org/2000/svg">
