9137cb03a417308f2c8e07e9f21e6f2e101c111d
João Victor <victorespucoc@gmail.com>
2021-04-20 12:16:27 -0300

    parte de alcance

--- appCliente/perfil.php
+++ appCliente/perfil.php
@@ -189,7 +189,7 @@ License: You must have a valid license purchased only from themeforest(the above
 															<div class="my-4">
 																<h3 class="texto-negrito">Trocar imagem do perfil</h3>
 															</div>
-															<form action="/file-upload" id="my-awesome-dropzone" class="my-4 dropzone dropzone-default text-center" >
+															<form  id="my-awesome-dropzone" class="my-4 dropzone dropzone-default text-center" >
 																<div class="my-4">
 																	<svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
 																		<path d="M40 80C62.0914 80 80 62.0914 80 40C80 17.9086 62.0914 0 40 0C17.9086 0 0 17.9086 0 40C0 62.0914 17.9086 80 40 80Z" fill="#B721FF"/>
