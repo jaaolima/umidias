74eb601dac5013cc32e304e996af0542aeca1edd
João Victor <victorespucoc@gmail.com>
2021-04-20 09:30:12 -0300

    parte de alcance

--- appCliente/perfil.php
+++ appCliente/perfil.php
@@ -235,7 +235,7 @@ License: You must have a valid license purchased only from themeforest(the above
                                             
                                         </div>
                                     </div>
-									<div class="justify-content-end">
+									<div class="text-end py-8">
 										<a href="">Cancelar</a>
 										<button class="btn btn-primary">Salvar alterações</button>
 									</div>
