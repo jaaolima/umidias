ea118b4c76ad042e34722ad0dd6a11477f5e42c1
João Victor <victorespucoc@gmail.com>
2021-04-24 09:12:27 -0300

    menu

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -149,7 +149,7 @@ $('.botao-menu').click(function (e)
 
   $('#nav_toggle').click(function(e){
     $(this).css({"margin-left": "102px"})
-    $('.texto-menu').css({"display": "none !important"})
+    $('.texto-menu').css({"display": "none "})
     $('#navbar').css({"width": "120px"})
     $("#kt_wrapper").css({"margin-left" : "120px"})
 
