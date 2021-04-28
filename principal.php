beed7686db5fe15f61664325a039d009a48a545d
João Victor <victorespucoc@gmail.com>
2021-04-28 15:51:21 -0300

    midia

--- principal.php
+++ principal.php
@@ -155,6 +155,18 @@ License: You must have a valid license purchased only from themeforest(the above
 										<span class="menu-text texto-menu texto-menu-financeiro"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Financeiro</font></font></span>
 									</a>
 								</li>
+								<?php if($id_usuario == 3) : ?>
+								<li class="menu-item my-4 botao-menu botao-menu-financeiro btn ml-5" aria-haspopup="true" style="width:max-content;" data-menu-toggle="hover">
+									<a href="/appMidia/listar_midia.php" class="btn rounded mx-2 text-left ">
+										<svg  width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M11.9091 3V21" stroke="#57616A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="svg-hover svg-financeiro"/>
+											<path d="M16 6.27271H9.86364C9.10415 6.27271 8.37578 6.57441 7.83874 7.11144C7.3017 7.64848 7 8.37686 7 9.13634C7 9.89582 7.3017 10.6242 7.83874 11.1612C8.37578 11.6983 9.10415 12 9.86364 12H13.9545C14.714 12 15.4424 12.3017 15.9794 12.8387C16.5165 13.3758 16.8182 14.1041 16.8182 14.8636C16.8182 15.6231 16.5165 16.3515 15.9794 16.8885C15.4424 17.4255 14.714 17.7273 13.9545 17.7273H7" class="svg-hover svg-financeiro" stroke="#57616A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+										</svg>
+ 
+										<span class="menu-text texto-menu texto-menu-financeiro"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Gerenc. Mídia</font></font></span>
+									</a>
+								</li>
+								<?php endif ;?>
 							</ul>
 							<div class="position-fixed bottom-0 ml-8 mb-6">
 								<a  href="principal.php" class="btn ml-2 text-left d-flex">
