528c487500a7b59d8d3a2784041b18e8e289d7b8
João Victor <victorespucoc@gmail.com>
2021-05-04 16:31:47 -0300

    alugar midia

--- principal.php
+++ principal.php
@@ -247,7 +247,7 @@ License: You must have a valid license purchased only from themeforest(the above
 																								</svg>
 																							</span>
 																							<p  class="titulo-midia d-block ">'.$dados["ds_nome"].'</p>
-																							<a href="appParceiro/appOutdoor/add_midia.php" class="btn btn-primary" type="button" >Alugar mídia</a>
+																							<a href="appParceiro/appOutdoor/add_midia.php" class="btn btn-primary" type="button" >Adicionar mídia</a>
 																						</div>';
 																			}
 																			else{
@@ -260,7 +260,7 @@ License: You must have a valid license purchased only from themeforest(the above
 																								</svg>
 																							</span>
 																							<p  class="titulo-midia d-block ">'.$dados["ds_nome"].'</p>
-																							<a href="appParceiro/appOutdoor/add_midia.php" class="btn btn-primary" type="button" >Alugar mídia</a>
+																							<a href="appParceiro/appOutdoor/add_midia.php" class="btn btn-primary" type="button" >Adicionar mídia</a>
 																						</div>';
 																			}
 
