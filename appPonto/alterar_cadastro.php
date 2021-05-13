cb56bbc3fa1f404361065a913a20cc3a5996df29
João Victor <victorespucoc@gmail.com>
2021-05-13 11:53:54 -0300

    gerenciamento

--- appPonto/alterar_cadastro.php
+++ appPonto/alterar_cadastro.php
@@ -121,8 +121,8 @@ jQuery(document).ready(function() {
 var demo3 = function() {
 	var map = new GMaps({
 		div: '#map',
-		lat: <? echo $dados["ds_latitude"] ?>,
-		lng: <? echo $dados["ds_longitude"] ?>,
+		lat: <?php echo $dados["ds_latitude"] ?>,
+		lng: <?php echo $dados["ds_longitude"] ?>,
 		dblclick: function(e) {
 			map.removeMarkers();
 			map.addMarker({
