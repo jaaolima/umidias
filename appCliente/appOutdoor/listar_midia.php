13d8f106c4d0f99feed0a30809268a7f1c7824eb
João Victor <victorespucoc@gmail.com>
2021-05-13 12:51:08 -0300

    gerenciamento

--- appCliente/appOutdoor/listar_midia.php
+++ appCliente/appOutdoor/listar_midia.php
@@ -4,7 +4,7 @@
 	ini_set('display_startup_erros',1);
 	error_reporting(E_ALL);
 	
-	require_once("../Classes/Ponto.php");
+	require_once("../../Classes/Ponto.php");
 	$ponto = new Ponto();
 	$retorno = $ponto->listarPonto($_POST);
 ?>
