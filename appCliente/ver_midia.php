48bf7a0b1a02b191ec4f79e8049132b86f1fa3c4
João Victor <victorespucoc@gmail.com>
2021-05-13 13:28:41 -0300

    gerenciamento

--- appCliente/ver_midia.php
+++ appCliente/ver_midia.php
@@ -85,7 +85,7 @@ License: You must have a valid license purchased only from themeforest(the above
 									<div class="col-7">
 										<div class="mb-8">
 											<div class="mb-12">
-												<h1 class="h1-titulo"><?php echo $dados["ds_local"]; ?></h1>
+												<h1 class="h1-titulo"><?php /*echo $dados["ds_local"];*/ ?></h1>
 												<span>St.Central - Taguatinga, Brasilia - DF, 40297-400</span>
 											</div>
 											<div class="my-12">
@@ -98,7 +98,7 @@ License: You must have a valid license purchased only from themeforest(the above
 											</div>
 											<div class="my-12">
 												<h4 class="texto-negrito">Latitude e Longitude</h4>
-												<span><?php echo $dados["ds_latitude"] " " $dados["ds_longitude"];?></span>
+												<span><?php/* echo $dados["ds_latitude"] " " $dados["ds_longitude"];*/?></span>
 											</div>
 											
 										</div>
@@ -164,7 +164,7 @@ License: You must have a valid license purchased only from themeforest(the above
 														</svg>
 													</div>													
 													<div class="text-right w-100">
-														<span class="font-weight-bolder text-right"><?php echo $dados["nu_valor"]; ?></span>
+														<span class="font-weight-bolder text-right"><?php /*echo $dados["nu_valor"];*/ ?></span>
 													</div>
 												</div>												
 												<div class="d-flex">
