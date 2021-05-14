0fcedbb885ff55c7a4ed2923a078ad8f98906453
Renato Lima <renato.lima1983@gmail.com>
2021-05-14 11:58:53 -0300

    favicon

--- index.php
+++ index.php
@@ -103,7 +103,7 @@ License: You must have a valid license purchased only from themeforest(the above
 									<div>
 										<form class="form" novalidate="novalidate" id="kt_login_signin_form">
 											<div class="form-group" >
-												<input class="form-control rectangle_1 my-4" type="text"  name="ds_usuario" id="ds_usuario" autocomplete="off" placeholder="E-mail"/>
+												<input class="form-control rectangle_1 my-4" type="text"  name="ds_usuario" id="ds_usuario" autocomplete="off" placeholder="Usuário ou E-mail"/>
 												<input class="form-control rectangle_1 my-4" type="password"  name="ds_senha" id="ds_senha"  placeholder="Senha"/>
 											</div>
 											<div class="form-group d-flex flex-wrap justify-content-end align-items-center">
