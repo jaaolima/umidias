e64f0b70f372ef8d110eb3d11991946c0eeccd89
João Victor <victorespucoc@gmail.com>
2021-05-14 07:48:25 -0300

    cadastro ponto

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -14,14 +14,14 @@ $("body").on('click', 'a', function(event){
     return false;
   }
 
-  if ($(this).attr('href').startsWith('paciente.php'))
+  /*if ($(this).attr('href').startsWith('paciente.php'))
   {
     var url = new URL($(this).attr('href').val());
     var id_paciente = url.searchParams.get("id_paciente");
     var ds_nome = url.searchParams.get("id_paciente");
     location.href='paciente.php?id_paciente='+id_paciente+'&ds_nome='+ds_nome;
     return false;
-  }
+  }*/
 
   if ($(this).attr('href').startsWith('#kt_portlet_base_demo') )
   {
