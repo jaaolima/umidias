cf74a6a47f38ea9c3f75eddfbb6f328eeda0a199
João Victor <victorespucoc@gmail.com>
2021-05-14 09:48:47 -0300

    ver midia

--- Classes/Ponto.php
+++ Classes/Ponto.php
@@ -93,10 +93,10 @@
 				$select = "SELECT id_ponto, nu_valor, t.id_midia, ds_local, t.ds_nome
 							FROM tb_ponto p
 							inner join tb_tipo_midia t on p.id_midia=t.id_midia
-							where id_ponto=:id_ponto";
+							where id_usuario=:id_usuario";
 				
 				$stmt = $con->prepare($select); 
-				$params = array(':id_ponto' => $id_ponto);
+				$params = array(':id_usuario' => $id_usuario);
 				
 				$stmt->execute($params);
 
