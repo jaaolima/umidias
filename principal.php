4706697ebed0feb6a9b590c1e12f7fcb1b412d14
Renato Lima <renato.lima1983@gmail.com>
2021-05-14 10:23:36 -0300

    página principal

--- principal.php
+++ principal.php
@@ -3,6 +3,7 @@
 	ini_set('display_startup_erros',1);
 	error_reporting(E_ALL);
 	session_start();
+	setlocale(LC_ALL, 'pt_BR');  
 
 	require_once("Classes/Midia.php");
 	require_once("Classes/Usuario.php");
@@ -23,18 +24,7 @@
 	$meusPontos = $ponto->listarMeusPontos($id_usuario);
 ?>
 <!DOCTYPE html>
-<!--
-Template Name: Metronic - Bootstrap 4 HTML, React, Angular 10 & VueJS Admin Dashboard Theme
-Author: KeenThemes
-Website: http://www.keenthemes.com/
-Contact: support@keenthemes.com
-Follow: www.twitter.com/keenthemes
-Dribbble: www.dribbble.com/keenthemes
-Like: www.facebook.com/keenthemes
-Purchase: https://1.envato.market/EA4JP
-Renew Support: https://1.envato.market/EA4JP
-License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
--->
+
 <html lang="en">
 	<!--begin::Head-->
 	<head><base href=""> 
@@ -129,7 +119,7 @@ License: You must have a valid license purchased only from themeforest(the above
 											<path d="M4 9.3L12.1 3L20.2 9.3V19.2C20.2 19.6774 20.0104 20.1352 19.6728 20.4728C19.3352 20.8104 18.8774 21 18.4 21H5.8C5.32261 21 4.86477 20.8104 4.52721 20.4728C4.18964 20.1352 4 19.6774 4 19.2V9.3Z" fill="none" stroke="#57616A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="svg-hover svg-dashboard svg-active"/>
 										</svg>
 
-										<span class="menu-text texto-menu-active texto-menu texto-menu-dashboard"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Dashboard</font></font></span>
+										<span class="menu-text texto-menu-active texto-menu texto-menu-dashboard"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Painel de Controle</font></font></span>
 									</a>
 								</li>
 								<?php if($_SESSION['id_perfil'] == 1 ) :   ?>
