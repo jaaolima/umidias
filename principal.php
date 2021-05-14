54436e5c831e22c4cdc920119e12adcd6fa19983
João Victor <victorespucoc@gmail.com>
2021-05-14 09:51:20 -0300

    ver midia

--- principal.php
+++ principal.php
@@ -409,40 +409,6 @@ License: You must have a valid license purchased only from themeforest(the above
 																	}
 																	
 																?>
-																	<tr>
-																		<td>
-																			<div class="d-flex">
-																				<span class="symbol symbol-lg-50 symbol-circle symbol-40 symbol-light-success">
-																					<span class="symbol-label font-size-h5 font-weight-bold">P</span>
-																				</span>
-																				<div class="ml-3 mt-2">
-																					<span class="texto-negrito">Outdoor</span><br>																				
-																					<svg class="mr-1" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-																						<g clip-path="url(#clip0)">
-																						<path d="M14 6.66675C14 11.3334 8 15.3334 8 15.3334C8 15.3334 2 11.3334 2 6.66675C2 5.07545 2.63214 3.54933 3.75736 2.42411C4.88258 1.29889 6.4087 0.666748 8 0.666748C9.5913 0.666748 11.1174 1.29889 12.2426 2.42411C13.3679 3.54933 14 5.07545 14 6.66675Z" stroke="#57616A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-																						<path d="M8 8.66675C9.10457 8.66675 10 7.77132 10 6.66675C10 5.56218 9.10457 4.66675 8 4.66675C6.89543 4.66675 6 5.56218 6 6.66675C6 7.77132 6.89543 8.66675 8 8.66675Z" stroke="#57616A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-																						</g>
-																						<defs>
-																						<clipPath id="clip0">
-																						<rect width="16" height="16" fill="white"/>
-																						</clipPath>
-																						</defs>
-																					</svg>
-																					<span>Taguatinga Centro</span>	
-																				</div>
-																			</div>
-																		</td>
-																		<td class="py-8">21/01/2021</td>
-																		<td class="py-8">21/01/2021</td>
-																		<td class="py-8">00,00 RS</td>
-																		<td class="py-8"><a href="appCliente/ver_minha_midia.php">
-																			<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-																				<path d="M5 12H19" stroke="#B721FF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-																				<path d="M12 5L19 12L12 19" stroke="#B721FF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-																			</svg>
-																		</a></td>
-																	</tr>
-																	
 																</tbody>
 															</table>
 															<div class="w-100 mt-6">
