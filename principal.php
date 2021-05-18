1f3668204ede944316edc557ddc9e9b854f8d8cc
João Victor <victorespucoc@gmail.com>
2021-05-18 14:33:14 -0300

    principal

--- principal.php
+++ principal.php
@@ -423,11 +423,11 @@
 												<div class="card-body d-flex">
 													<div class="d-flex py-5 flex-column align-items-start flex-grow-1">
 														<div class="flex-grow-1" style="display: inherit;">
-															<h3 class="card-title titulo-div">Total de Pontos</h3>
+															<h3 class="card-title titulo-div">Total de Pontos:</h3>
 														</div>
 														<?php
-															/*$dadosTotal = $dadosTodasMidias->fetch()
-															echo count($dadosTotal);*/
+															$dadosTotal = $dadosTodasMidias->fetch();
+															echo count($dadosTotal["id_ponto"]);
 														?>
 														<h5</h5>
 													</div>
