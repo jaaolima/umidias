8598a7a44011d9932ab4b7ad2cda3019fd8dd0d7
João Victor <victorespucoc@gmail.com>
2021-05-19 14:33:04 -0300

    bisemana

--- assets/js/appParceiro/cadastro.js
+++ assets/js/appParceiro/cadastro.js
@@ -48,36 +48,12 @@ $(document).ready(function() {
         if($(this).val() === "CPF"){
             $('#cpf').show();
 			$('#cnpj').hide();
-			if($("#nu_cpf").val() == "")
-			{
-				$("#nu_cpf").focus();
-				swal.fire("Erro", "Preencha o CPF", "error");
-				$("#nu_cpf").addClass("is-invalid");
-				return false;	
-			}
-			else
-			{
-				$("#nu_cpf").removeClass("is-invalid");	
-				$("#nu_cpf").addClass("is-valid");
-			}
             return;
         }
 		else{
 			$('#cnpj').show();
 			$('#cpf').hide();
 			$('[name="nu_cpf"]').prop('checked', false);
-			if($("#nu_cnpj").val() == "")
-			{
-				$("#nu_cnpj").focus();
-				swal.fire("Erro", "Preencha o CNPJ", "error");
-				$("#nu_cnpj").addClass("is-invalid");
-				return false;	
-			}
-			else
-			{
-				$("#nu_cnpj").removeClass("is-invalid");	
-				$("#nu_cnpj").addClass("is-valid");
-			}
 		}
 
         
@@ -121,7 +97,35 @@ function validar()
 		$("#ds_nomeempresa").removeClass("is-invalid");	
 		$("#ds_nomeempresa").addClass("is-valid");
 	}
+	if($("#id_regime").val() === "CPF"){
+		if($("#nu_cpf").val() == "")
+		{
+			$("#nu_cpf").focus();
+			swal.fire("Erro", "Preencha o CPF", "error");
+			$("#nu_cpf").addClass("is-invalid");
+			return false;	
+		}
+		else
+		{
+			$("#nu_cpf").removeClass("is-invalid");	
+			$("#nu_cpf").addClass("is-valid");
+		}
 
+	}
+	else{
+		if($("#nu_cnpj").val() == "")
+		{
+			$("#nu_cnpj").focus();
+			swal.fire("Erro", "Preencha o CNPJ", "error");
+			$("#nu_cnpj").addClass("is-invalid");
+			return false;	
+		}
+		else
+		{
+			$("#nu_cnpj").removeClass("is-invalid");	
+			$("#nu_cnpj").addClass("is-valid");
+		}
+	}
 	
 
 	if($("#ds_logradouro").val() == "")
