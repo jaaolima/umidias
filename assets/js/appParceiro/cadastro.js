19f8f71739b20f14eb977bda5760da90e21cab6b
João Victor <victorespucoc@gmail.com>
2021-05-19 17:37:11 -0300

    cnpj

--- assets/js/appParceiro/cadastro.js
+++ assets/js/appParceiro/cadastro.js
@@ -8,7 +8,8 @@ $(document).ready(function() {
 
 	$("#salvar").on("click", function(e){ 
 		var cnpj = $("#nu_cnpj").val();
-		if(validar() && validarCNPJ(cnpj))
+		var cnpj = $("#nu_cpf").val();
+		if(validar() && validarCNPJ(cnpj) && validarCPF(cpf))
 		{ 
 			$.ajax({
 		        url: 'appParceiro/gravar_parceiro.php'
@@ -267,14 +268,6 @@ function validarCNPJ(cnpj) {
  
     cnpj = cnpj.replace(/[^\d]+/g,'');
  
-    if(cnpj == '')
-	{
-		$("#nu_cnpj").focus();
-		swal.fire("Erro", "CNPJ inválido", "error");
-		$("#nu_cnpj").addClass("is-invalid");
-		return false;		
-	}
-     
     if (cnpj.length != 14)
 	{
 		$("#nu_cnpj").focus();
@@ -343,4 +336,54 @@ function validarCNPJ(cnpj) {
            
     return true;
     
+}
+function validarCPF(cpf) {	
+	cpf = cpf.replace(/[^\d]+/g,'');	
+	// Elimina CPFs invalidos conhecidos	
+	if (cpf.length != 11 || 
+		cpf == "00000000000" || 
+		cpf == "11111111111" || 
+		cpf == "22222222222" || 
+		cpf == "33333333333" || 
+		cpf == "44444444444" || 
+		cpf == "55555555555" || 
+		cpf == "66666666666" || 
+		cpf == "77777777777" || 
+		cpf == "88888888888" || 
+		cpf == "99999999999")
+	{
+		$("#nu_cpf").focus();
+		swal.fire("Erro", "CPF inválido", "error");
+		$("#nu_cpf").addClass("is-invalid");
+		return false;		
+	}	
+	// Valida 1o digito	
+	add = 0;	
+	for (i=0; i < 9; i ++)		
+		add += parseInt(cpf.charAt(i)) * (10 - i);	
+		rev = 11 - (add % 11);	
+		if (rev == 10 || rev == 11)		
+			rev = 0;	
+		if (rev != parseInt(cpf.charAt(9)))		
+		{
+			$("#nu_cpf").focus();
+			swal.fire("Erro", "CPF inválido", "error");
+			$("#nu_cpf").addClass("is-invalid");
+			return false;		
+		}			
+	// Valida 2o digito	
+	add = 0;	
+	for (i = 0; i < 10; i ++)		
+		add += parseInt(cpf.charAt(i)) * (11 - i);	
+	rev = 11 - (add % 11);	
+	if (rev == 10 || rev == 11)	
+		rev = 0;	
+	if (rev != parseInt(cpf.charAt(10)))
+	{
+		$("#nu_cpf").focus();
+		swal.fire("Erro", "CPF inválido", "error");
+		$("#nu_cpf").addClass("is-invalid");
+		return false;		
+	}			
+	return true;   
 }
