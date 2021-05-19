743d8d2890558496d4fdb108390412e9f6dbf5e6
João Victor <victorespucoc@gmail.com>
2021-05-19 14:05:54 -0300

    bisemana

--- principal.php
+++ principal.php
@@ -196,13 +196,13 @@
 								</li>
 								<li class="menu-item my-4 botao-menu botao-menu-material btn ml-5" aria-haspopup="true" style="width:max-content;" data-menu-toggle="hover">
 									<a href="/appMaterial/listar_material.php" class="btn rounded mx-2 text-left ">
-										<i class="flaticon-users icon-xl svg-hover svg-material"></i>
+										<i class="flaticon-folder icon-xl svg-hover svg-material"></i>
 										<span class="menu-text texto-menu texto-menu-material"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Materiais</font></font></span>
 									</a>
 								</li>
 								<li class="menu-item my-4 botao-menu botao-menu-bisemana btn ml-5" aria-haspopup="true" style="width:max-content;" data-menu-toggle="hover">
 									<a href="/appBisemana/listar_bisemana.php" class="btn rounded mx-2 text-left ">
-										<i class="flaticon-users icon-xl svg-hover svg-bisemana"></i>
+										<i class="flaticon-calendar-1 icon-xl svg-hover svg-bisemana"></i>
 										<span class="menu-text texto-menu texto-menu-bisemana"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Bisemana</font></font></span>
 									</a>
 								</li>
