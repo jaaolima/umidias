7dcc2c3e32aa1146c8f67a6b67ca67de9abec671
João Victor <victorespucoc@gmail.com>
2021-05-20 10:47:48 -0300

    cnpj

--- principal.php
+++ principal.php
@@ -453,7 +453,7 @@
 												<div class="card-body d-flex">
 													<div class="d-flex py-5 flex-column align-items-start flex-grow-1">
 														<div class="flex-grow-1" style="display: inherit;">
-															<h3 class="card-title titulo-div">Total de Parceiros: <?php echo $dadosTotalParceiros["id_usuario"]; ?></h3>
+															<h3 class="card-title titulo-div">Total de Parceiros: <?php echo $dadosTotalParceiros["id_parceiro"]; ?></h3>
 														</div>
 													</div>
 												</div>
