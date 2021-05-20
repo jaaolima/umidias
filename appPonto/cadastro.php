bf9623815e9e9fe94794ba06ef943827759c1670
João Victor <victorespucoc@gmail.com>
2021-05-20 11:32:12 -0300

    cnpj

--- appPonto/cadastro.php
+++ appPonto/cadastro.php
@@ -67,7 +67,7 @@ $optionsparceiro = $Parceiro->listaroptionsparceiro(null);
             <div class="form-group row">
                 <div class="form-group col-4">
                     <label class="">Multiple File Upload</label>
-                    <div class="dropzone dropzone-default dropzone-primary" id="kt_dropzone_2"></div>
+                    <div class="dropzone dropzone-default dropzone-primary" id="kt_dropzone_2" style="height: 70%;"></div>
                     <div class="dropzone-msg dz-message needsclick">
                         <h3 class="dropzone-msg-title">Solte os arquivos aqui ou clique para fazer o upload.</h3>
                         <span class="dropzone-msg-desc">Faça upload de até 5 arquivos</span>
