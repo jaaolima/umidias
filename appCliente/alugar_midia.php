7172c9233489d980e96c6389370355308a31e9df
João Victor <victorespucoc@gmail.com>
2021-05-21 11:44:52 -0300

    cnpj

--- appCliente/alugar_midia.php
+++ appCliente/alugar_midia.php
@@ -8,6 +8,9 @@
 	$material = new material();
     $optionsMaterial = $material->listarOptionsMaterial();
     $id_midia = $_POST["id_midia"];
+    $id_ponto = $_POST["id_ponto"];
+    $dt_inicial = $_POST["dt_inicial"];
+    $mes = $_POST["mes"];
 
 ?>
 <div class="card card-custom card-stretch gutter-b box-shadow" id="alugar_midia">
@@ -15,9 +18,9 @@
         <h3 class="font-weight-bolder">Detalhes adicionais:</h3>
     </div>
     <div class="separator separator-solid"></div>
-    <div class="my-6 mx-6">
-        <?php if($id_midia == 1) : ?>
+    <form class="my-6 mx-6" id="form_alugar">
         <div class="form-group row"> 
+            <?php if($id_midia == 1) : ?>
             <div class="col-6">
                 <label>Tipo de material</label>
                 <select name="id_material" id="id_material" class="form-control">
@@ -27,27 +30,19 @@
                     ?>
                 </select>
             </div>
-        </div>
-        <div class="form-group row"> 
+            <?php endif ;?>
             <div class="col-6">
                 <label>Adicione sua Arte</label>
-                <input type="file" class="form-control">
+                <input type="file" class="form-control" name="ds_arte" id="ds_arte">
                 <span class="texto-chumbo">A arte deve ter escala de 9,0 x 3,60</span>
             </div>
         </div>
-        <?php endif ;?>
-        <?php if($id_midia == 2) : ?>
-        <div class="form-group row"> 
-            <div class="col-6">
-                <label>Adicione sua Arte</label>
-                <input type="file" class="form-control">
-                <span class="texto-chumbo">A arte deve ter escala de 9,0 x 3,60</span>
-            </div>
-        </div>
-        <?php endif ;?>
-    </div>
+        <input type="hidden" name="id_midia" id="id_midia" value="<?php echo $id_midia; ?>">
+        <input type="hidden" name="id_ponto" id="id_ponto" value="<?php echo $id_ponto; ?>">
+    </form>
     <div class="separator separator-solid"></div>
-    <div class="my-6 mx-6">
-        <button class="btn btn-primary w-100">Ir para método de Pagamento</button>
+    <div class="my-6 mx-6 d-flex">
+        <button class="btn btn-primary w-100" id="voltar">Voltar</button>
+        <button class="btn btn-primary w-100" id="pagamento">Ir para método de Pagamento</button>
     </div>
 </div>
---
+++ assets/js/appCliente/alugar_midia.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+	    
+
+	$("#pagamento").on("click", function(e){ 
+		if(validar())
+		{ 
+			$.ajax({
+		        url: 'appCliente/gravar_alterar_cliente.php'
+				, data: $("#form_usuario").serialize()
+		        , type: 'post'
+		    });		
+		}	
+	});
+    $("#voltar").on("click", function(){
+		$('#form_alugar').trigger("reset");
+        var id_ponto = $("#id_ponto").val();
+        redirectTo(`appCliente/ver_midia?id_ponto=`+id_ponto+`.php`);
+    }); 
+});
+
+function validar()
+{
+	if($("#ds_arte").val() == "")
+	{
+		$("#ds_arte").focus();
+		swal.fire("Erro", "Adicione uma arte", "error");
+		$("#ds_arte").addClass("is-invalid");
+		return false;	
+	}
+	else
+	{
+		$("#ds_arte").removeClass("is-invalid");	
+		$("#ds_arte").addClass("is-valid");
+	}
+
+	
+	
+	
+
+	return true;
+}
