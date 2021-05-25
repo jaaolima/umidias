941c989274ea025b6eeada93cca98f6c16487bb4
João Victor <victorespucoc@gmail.com>
2021-05-25 17:01:01 -0300

    ponto

--- appPonto/alterar_cadastro.php
+++ appPonto/alterar_cadastro.php
@@ -88,7 +88,7 @@ $optionsparceiro = $Parceiro->listaroptionsparceiro($id_parceiro);
                     <div class="carousel-inner">
                         <?php
                             while($fotos = $dadosFoto->fetch()){
-                                if($dadosFoto['ds_foto'][0] == $dadosFoto["ds_foto"]){
+                                if($dadosFoto['ds_foto'](0) == $dadosFoto["ds_foto"]){
                                     echo "<div class='carousel-item active'>
                                             <img class='d-block w-100' style='max-height:300px;'  src='".$fotos["ds_foto"]."' >
                                         </div>";
