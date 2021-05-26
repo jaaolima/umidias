aac0a1d5d3d255f32eb7b9de1fb63912c2766a8e
jaaolima <victorespucoc@gmail.com>
2021-05-26 13:41:48 -0300

    alterar parceiro

--- appParceiro/alterar_cadastro.php
+++ appParceiro/alterar_cadastro.php
@@ -52,7 +52,7 @@ $optionsCidade = $parceiro->listarOptionsCidade($dados['id_estado'], $dados['id_
                     <input type="text" class="form-control" id="ds_nomeempresa" name="ds_nomeempresa" value="<?php echo $dados['ds_nomeempresa']?>"/>
                 </div>
 
-                <?php if($dados["id_regime"] == "CPF") : ?>
+
                 <div class="form-group col-md-3 d-none" id="cnpj">
                     <label >CNPJ <span class="text-danger">*</span></label>
                     <input type="text" class="form-control" id="nu_cnpj" name="nu_cnpj" value="<?php echo $dados['nu_cnpj']?>"/>
@@ -61,9 +61,8 @@ $optionsCidade = $parceiro->listarOptionsCidade($dados['id_estado'], $dados['id_
                     <label >CPF<span class="text-danger">*</span></label>
                     <input type="text" class="form-control" id="nu_cpf" name="nu_cpf" value="<?php echo $dados['nu_cpf']?>"/>
                 </div>
-                <?php endif; ?>
 
-                <?php if($dados["id_regime"] != "CPF") : ?>
+                <?php /* if($dados["id_regime"] != "CPF") : ?>
                 <div class="form-group col-md-3 d-none" id="cpf">
                     <label >CPF<span class="text-danger">*</span></label>
                     <input type="text" class="form-control" id="nu_cpf" name="nu_cpf" value="<?php echo $dados['nu_cpf']?>"/>
@@ -72,7 +71,7 @@ $optionsCidade = $parceiro->listarOptionsCidade($dados['id_estado'], $dados['id_
                     <label >CNPJ <span class="text-danger">*</span></label>
                     <input type="text" class="form-control" id="nu_cnpj" name="nu_cnpj" value="<?php echo $dados['nu_cnpj']?>"/>
                 </div>
-                <?php endif; ?>
+                <?php endif; */?>
                 
             </div>
             <div class="form-group row"> 
