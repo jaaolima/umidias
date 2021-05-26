c76de93a74111b8bbfab06c52a759ea3355dace4
jaaolima <victorespucoc@gmail.com>
2021-05-26 16:43:38 -0300

    parceiro

--- appPonto/excluir_foto_ponto.php
+++ appPonto/excluir_foto_ponto.php
@@ -2,8 +2,7 @@
 	ini_set('display_errors',1);
 	ini_set('display_startup_erros',1);
 	error_reporting(E_ALL);
-    echo $_POST["id_ponto_foto"];
-	/*require_once("../Classes/Ponto.php");
+	require_once("../Classes/Ponto.php");
 	$ponto = new Ponto();
-	$ponto->excluirFotoPonto($_POST);*/
+	$ponto->excluirFotoPonto($_POST);
 ?> 
