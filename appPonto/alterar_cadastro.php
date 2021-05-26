036ddb4d50e4f30d2b67cdd027f93ad773e196d5
jaaolima <victorespucoc@gmail.com>
2021-05-26 16:21:43 -0300

    parceiro

--- appPonto/alterar_cadastro.php
+++ appPonto/alterar_cadastro.php
@@ -94,14 +94,14 @@ $optionsparceiro = $Parceiro->listaroptionsparceiro($id_parceiro);
                                 $total .= 1;
                                 if($total == 1){
                                     echo "<div class='carousel-item active'>
-                                            <button class='btn btn-outline-primary bg-white  position-absolute' style='top: 10px;right: 40px;' id='excluirFoto".$fotos["id_ponto_foto"]."'>Excluir</button>
+                                            <button class='btn btn-outline-primary bg-white  position-absolute' style='top: 10px;right: 40px;' type='button' id='excluirFoto".$fotos["id_ponto_foto"]."'>Excluir</button>
                                             <img class='d-block w-100 img-fluid' style='max-height:300px;'  src='".$fotos["ds_foto"]."' >
                                         </div>";
                                         
                                 }
                                 else{
                                     echo "<div class='carousel-item'>
-                                        <button class='btn btn-outline-primary bg-white  position-absolute' style='top: 10px;right: 40px;' id='excluirFoto".$fotos["id_ponto_foto"]."'>Excluir</button>
+                                        <button class='btn btn-outline-primary bg-white  position-absolute' style='top: 10px;right: 40px;' type='button' id='excluirFoto".$fotos["id_ponto_foto"]."'>Excluir</button>
                                         <img class='d-block w-100 img-fluid' style='max-height:300px;'  src='".$fotos["ds_foto"]."' >
                                     </div>";
                                 }
