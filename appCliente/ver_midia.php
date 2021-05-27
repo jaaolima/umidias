6bf22e0e3f0404f58e7af10961221629f66e63f2
jaaolima <victorespucoc@gmail.com>
2021-05-27 18:22:41 -0300

    filtro

--- appCliente/ver_midia.php
+++ appCliente/ver_midia.php
@@ -384,7 +384,9 @@ License: You must have a valid license purchased only from themeforest(the above
 
 		//evento dispara quando retira o foco do campo texto
 		mes.onblur = function(){
-			valor.innerHTML = "<h2>"+ parseInt((<?php echo $dados["nu_valor"] ?> * mes.value), 10) + parseInt(((<?php echo $dados["nu_valor"] ?> * mes.value) * 0.2), 10)  +"</h2>";
+			taxa = <?php echo $dados["nu_valor"] ?> * mes.value) * 0.2;
+			Total = <?php echo $dados["nu_valor"] ?> * mes.value
+			valor.innerHTML = "<h2>"+ parseInt(taxa) + parseInt(total)  +"</h2>";
 		}
 		var demo3 = function() {
 			var map = new GMaps({
