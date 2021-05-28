bb4f3dbd1504d36f8f19826c7702150c4a3cf0f7
jaaolima <victorespucoc@gmail.com>
2021-05-28 16:57:30 -0300

    filtro

--- appCliente/pagamento.php
+++ appCliente/pagamento.php
@@ -116,8 +116,8 @@ License: You must have a valid license purchased only from themeforest(the above
                                     <div class="my-12">
                                         <h4 class="texto-negrito">Período</h4>
                                         <?php 
-                                            for ($i=0; $i < count($bisemana) ; $i++) { 
-                                                echo "<span>".$bisemana[$i]."</span>"
+                                            for($i=0; $i < count($bisemana) ; $i++) { 
+                                                echo "<span>".$bisemana[$i]."</span>";
                                             }
                                         ?> 
                                     </div>
