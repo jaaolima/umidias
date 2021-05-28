b5084ead3933c13d5fc38623e312117b4e0e9012
jaaolima <victorespucoc@gmail.com>
2021-05-28 11:23:22 -0300

    filtro

--- principal.php
+++ principal.php
@@ -70,8 +70,8 @@
 				</a>
 				<!--end::Logo-->
 				<div class=" w-auto btn-clean d-flex align-items-center px-2" id="kt_quick_user_toggle">
-					<a href="#">
-						<i class="flaticon-shopping-basket icon-xl svg-active"></i>
+					<a href="#" class="mr-4">
+						<i class="flaticon-shopping-basket icon-xl svg-shop"></i>
 					</a>
 					<div><p class="mb-0 mt-1 ml-2 texto-chumbo">Olá, <?php echo $dadosUsuario['ds_nome'];?>!</p></div>
 					<div class="dropdown">
