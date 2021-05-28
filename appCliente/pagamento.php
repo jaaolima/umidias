6f033c10512585128a05fd73bdf4606a451e612c
jaaolima <victorespucoc@gmail.com>
2021-05-28 14:42:20 -0300

    filtro

--- appCliente/pagamento.php
+++ appCliente/pagamento.php
@@ -136,7 +136,7 @@ License: You must have a valid license purchased only from themeforest(the above
                                     <?php endif; ?>
                                     <div class="my-12">
                                         <h4 class="texto-negrito">Valor Total </h4>
-                                        <h4 style="color: green;"><?php echo ($dados["nu_valor"] * $mes) + ($dados["nu_valor"] * $mes * 0.2);?></h4>
+                                        <h4 style="color: green;"><?php echo ($dados["nu_valor"] * $mes) + (($dados["nu_valor"] * $mes) * 0.2);?></h4>
                                     </div>
                                 </div>
 
