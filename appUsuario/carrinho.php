4e3ef05613d89291b42611076b48ca21cf0ca006
jaaolima <victorespucoc@gmail.com>
2021-05-28 12:23:38 -0300

    filtro

--- appUsuario/carrinho.php
+++ appUsuario/carrinho.php
@@ -47,7 +47,7 @@ License: You must have a valid license purchased only from themeforest(the above
                                     <h3>Carrinho:</h3>
                                 </div>
                                 <div class="separator separator-solid"></div>
-                                <div class="my-6 mx-6 d-flex">
+                                <div class="my-6 mx-6 d-flex row">
                                     <?php
                                         while($dados = $retorno->fetch()){
                                             echo "<div class='col-4' >
