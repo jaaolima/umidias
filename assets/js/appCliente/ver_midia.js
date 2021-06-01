bb77c20be5ac75aa887c0eae2ead8b511c601e32
jaaolima <victorespucoc@gmail.com>
2021-06-01 17:19:31 -0300

    outdoor

--- assets/js/appCliente/ver_midia.js
+++ assets/js/appCliente/ver_midia.js
@@ -173,15 +173,9 @@ $(document).ready(function() {
 				{
 					bisemana.push( $(this).val());
 				});
-				swal.fire({
-                    position: 'top-right',
-                    type: 'success',
-                    title: bisemana,
-                    showConfirmButton: true
-                });
 				var id_material = $("#id_material").val();
 				var ds_arte = $('input[type=file]').val();
-				//redirectTo("appCliente/pagamento.php?id_ponto="+id_ponto+"&bisemana="+bisemana+"&ds_arte="+ds_arte+"&id_material="+id_material);
+				redirectTo("appCliente/pagamento.php?id_ponto="+id_ponto+"&bisemana="+bisemana+"&ds_arte="+ds_arte+"&id_material="+id_material);
 			}
 			
 		}	
