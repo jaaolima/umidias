18f9dea21459fa2e7ba1c88a71c0885ae176c6c8
jaaolima <victorespucoc@gmail.com>
2021-06-01 15:45:33 -0300

    admin

--- Classes/Ponto.php
+++ Classes/Ponto.php
@@ -293,7 +293,8 @@
 							right join rl_ponto_foto f on a.id_ponto=f.id_ponto
 							right join tb_ponto p on a.id_ponto=p.id_ponto
 							inner join tb_tipo_midia t on p.id_midia=t.id_midia
-							where id_usuario=:id_usuario";
+							where id_usuario=:id_usuario
+							and f.ds_foto = (select min(ds_foto) from rl_ponto_foto pf where p.id_ponto = pf.id_ponto)";
 				
 				$stmt = $con->prepare($select); 
 				$params = array(':id_usuario' => $id_usuario);
