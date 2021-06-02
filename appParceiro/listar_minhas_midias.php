9817ff90d73aa7e99341051f8cad11eaf73b2866
jaaolima <victorespucoc@gmail.com>
2021-06-02 16:51:17 -0300

    ver midia

--- appParceiro/listar_minhas_midias.php
+++ appParceiro/listar_minhas_midias.php
@@ -63,8 +63,8 @@ License: You must have a valid license purchased only from themeforest(the above
 								<h1 class="h1-titulo">Minhas mídias</h1>
 							</div>
 							<div class="card card-custom bgi-no-repeat bgi-size-cover gutter-b bg-white"  >
-								<div class="card-body d-flex">											
-									<table class="table table-hover w-100" id="table_ponto">
+								<div class="card-body">											
+									<table class="table table-hover" id="table_ponto">
 										<thead>
 											<tr>
 												<th>Id Ponto</th>
