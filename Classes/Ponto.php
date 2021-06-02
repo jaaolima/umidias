5401a98834dddf34d35a0904a06517c17560bc10
jaaolima <victorespucoc@gmail.com>
2021-06-02 15:59:00 -0300

    ver midia

--- Classes/Ponto.php
+++ Classes/Ponto.php
@@ -311,6 +311,33 @@
     			print "ERRO:".$e->getMessage();		
 			}
 		}
+		public function listarMeusPontos($id_usuario)
+		{
+			try{
+				$con = Conecta::criarConexao();
+				
+				$select = "SELECT p.id_ponto, nu_valor, t.id_midia, ds_local, t.ds_tipo, f.ds_foto
+							FROM tb_ponto p
+							right join rl_ponto_foto f on p.id_ponto=f.id_ponto
+							inner join tb_tipo_midia t on p.id_midia=t.id_midia
+							where id_parceiro=:id_usuario
+							and f.ds_foto = (select min(ds_foto) from rl_ponto_foto pf where p.id_ponto = pf.id_ponto)";
+				
+				$stmt = $con->prepare($select); 
+				$params = array(':id_usuario' => $id_usuario);
+				
+				$stmt->execute($params);
+
+				return $stmt;
+				
+					
+			}
+			catch(exception $e)
+			{
+				header('HTTP/1.1 500 Internal Server Error');
+    			print "ERRO:".$e->getMessage();		
+			}
+		}
 		public function listarTodasMidias()
 		{
 			try{
