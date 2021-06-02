94935d6556fd1b19b99342adb9509509e02bf804
jaaolima <victorespucoc@gmail.com>
2021-06-02 14:28:26 -0300

    ver midia

--- appCliente/ver_midia.php
+++ appCliente/ver_midia.php
@@ -386,7 +386,7 @@ License: You must have a valid license purchased only from themeforest(the above
 		var mes = document.getElementById("mes");
 		var valor1 = document.getElementById("valor1");
 
-		mes.onblur = function(){
+		mes.on("keyup", function(){
 			let taxa = 0.2 * (<?php echo $dados["nu_valor"] ?> * mes.value);
 			let valor = <?php echo $dados["nu_valor"] ?> * mes.value;
 			let total = parseInt(taxa, 10) + parseInt(valor, 10);
@@ -398,7 +398,7 @@ License: You must have a valid license purchased only from themeforest(the above
 		var valor2 = document.getElementById("valor2");
 		valor2.innerHTML = "<h2>teste</h2>";
 
-		$(':checkbox').click(function(){
+		$(':checkbox').on("click", function(){
 			let taxa = 0.2 * (<?php echo $dados["nu_valor"] ?> * totalBisemana);
 			let valor = <?php echo $dados["nu_valor"] ?> * totalBisemana;
 			let total = parseInt(taxa, 10) + parseInt(valor, 10);
