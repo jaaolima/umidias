d90ca8e39e3069e27ebb7be19634e6b3019046af
jaaolima <victorespucoc@gmail.com>
2021-06-02 16:47:23 -0300

    ver midia

--- appParceiro/listar_minhas_midias.php
+++ appParceiro/listar_minhas_midias.php
@@ -65,64 +65,60 @@ License: You must have a valid license purchased only from themeforest(the above
 							<div class="row">
 								<div class="col-xl-12">
 									<div class="card card-custom bgi-no-repeat bgi-size-cover gutter-b bg-white"  >
-										<div class="card-body d-flex">
-											<div class="d-flex py-5 flex-column align-items-start flex-grow-1">
-												<div class="row m-0 col-12" >											
-													<table class="table table-hover" id="table_ponto">
-														<thead>
-															<tr>
-																<th>Id Ponto</th>
-																<th>Minhas Mídias</th>
-																<th>Descrição</th>
-																<th>Valor</th>
-																<th>Tipo de Mídia</th>
-																<th>Observações</th>
-																<th>Ações</th> 
-															</tr>
-														</thead> 
-														<tbody>
-															<?php
-																
-																while($dados = $meusPontos->fetch()){
-																	echo "<tr>
-																				<td>".$dados['id_ponto']."</td>
-																				<td>
-																					<div class='d-flex'>
-																						<div class='d-flex'>
-																							<span class='symbol symbol-lg-50 symbol-circle symbol-40 symbol-light-success'>
-																								<img class='symbol-label img-fluid' src='".$dados["ds_foto"]."'>
-																							</span>
-																							<div class='ml-3 mt-2'>
-																								<span class='texto-negrito'>".$dados["ds_tipo"]."</span><br>																				
-																								<svg class='mr-1' width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
-																									<g clip-path='url(#clip0)'>
-																									<path d='M14 6.66675C14 11.3334 8 15.3334 8 15.3334C8 15.3334 2 11.3334 2 6.66675C2 5.07545 2.63214 3.54933 3.75736 2.42411C4.88258 1.29889 6.4087 0.666748 8 0.666748C9.5913 0.666748 11.1174 1.29889 12.2426 2.42411C13.3679 3.54933 14 5.07545 14 6.66675Z' stroke='#57616A' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/>
-																									<path d='M8 8.66675C9.10457 8.66675 10 7.77132 10 6.66675C10 5.56218 9.10457 4.66675 8 4.66675C6.89543 4.66675 6 5.56218 6 6.66675C6 7.77132 6.89543 8.66675 8 8.66675Z' stroke='#57616A' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/>
-																									</g>
-																									<defs>
-																									<clipPath id='clip0'>
-																									<rect width='16' height='16' fill='white'/>
-																									</clipPath>
-																									</defs>
-																								</svg>
-																								<span>".$dados["ds_local"]."</span>	
-																							</div>
-																						</div>
+										<div class="card-body d-flex">											
+											<table class="table table-hover" id="table_ponto">
+												<thead>
+													<tr>
+														<th>Id Ponto</th>
+														<th>Minhas Mídias</th>
+														<th>Descrição</th>
+														<th>Valor</th>
+														<th>Tipo de Mídia</th>
+														<th>Observações</th>
+														<th>Ações</th> 
+													</tr>
+												</thead> 
+												<tbody>
+													<?php
+														
+														while($dados = $meusPontos->fetch()){
+															echo "<tr>
+																		<td>".$dados['id_ponto']."</td>
+																		<td>
+																			<div class='d-flex'>
+																				<div class='d-flex'>
+																					<span class='symbol symbol-lg-50 symbol-circle symbol-40 symbol-light-success'>
+																						<img class='symbol-label img-fluid' src='".$dados["ds_foto"]."'>
+																					</span>
+																					<div class='ml-3 mt-2'>
+																						<span class='texto-negrito'>".$dados["ds_tipo"]."</span><br>																				
+																						<svg class='mr-1' width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+																							<g clip-path='url(#clip0)'>
+																							<path d='M14 6.66675C14 11.3334 8 15.3334 8 15.3334C8 15.3334 2 11.3334 2 6.66675C2 5.07545 2.63214 3.54933 3.75736 2.42411C4.88258 1.29889 6.4087 0.666748 8 0.666748C9.5913 0.666748 11.1174 1.29889 12.2426 2.42411C13.3679 3.54933 14 5.07545 14 6.66675Z' stroke='#57616A' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/>
+																							<path d='M8 8.66675C9.10457 8.66675 10 7.77132 10 6.66675C10 5.56218 9.10457 4.66675 8 4.66675C6.89543 4.66675 6 5.56218 6 6.66675C6 7.77132 6.89543 8.66675 8 8.66675Z' stroke='#57616A' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/>
+																							</g>
+																							<defs>
+																							<clipPath id='clip0'>
+																							<rect width='16' height='16' fill='white'/>
+																							</clipPath>
+																							</defs>
+																						</svg>
+																						<span>".$dados["ds_local"]."</span>	
 																					</div>
-																				</td>
-																				<td>".$dados['ds_descricao']."</td>
-																				<td>".$dados['nu_valor']."</td>
-																				<td>".$dados['ds_tipo']."</td>
-																				<td>".$dados['ds_observacao']."</td>
-																				<td nowrap></td>
-																			</tr>";
-																}
-																
-															?>
-														</tbody>
-													</table>
-												</div>
-											</div>
+																				</div>
+																			</div>
+																		</td>
+																		<td>".$dados['ds_descricao']."</td>
+																		<td>".$dados['nu_valor']."</td>
+																		<td>".$dados['ds_tipo']."</td>
+																		<td>".$dados['ds_observacao']."</td>
+																		<td nowrap></td>
+																	</tr>";
+														}
+														
+													?>
+												</tbody>
+											</table>
 										</div>
 									</div>
 								</div>
