1958686a0a6affe955b2452b879d4a1dde03e17f
jaaolima <victorespucoc@gmail.com>
2021-06-04 16:04:09 -0300

    carrinho

--- Classes/Ponto.php
+++ Classes/Ponto.php
@@ -567,7 +567,7 @@
 				$stmt->execute($params);
 
 				var_dump($ds_foto);
-				if(!isset($ds_foto)){
+				if(isset($ds_foto)){
 					foreach($ds_foto as $key => $foto)
 					{
 						$tamanho = 20000000;
