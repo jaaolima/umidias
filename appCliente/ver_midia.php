d8b2d2302420184efa7596741ad97666c21b3f07
jaaolima <victorespucoc@gmail.com>
2021-06-04 16:57:56 -0300

    carrinho

--- appCliente/ver_midia.php
+++ appCliente/ver_midia.php
@@ -390,7 +390,7 @@ License: You must have a valid license purchased only from themeforest(the above
 				let taxa = 0.2 * (<?php echo $dados["nu_valor"] ?> * totalBisemana); 
 				let valor = <?php echo $dados["nu_valor"] ?> * totalBisemana;
 				let total = parseInt(taxa, 10) + parseInt(valor, 10);
-				valor2.innerHTML = "<h2>"+ total +"</h2>";
+				valor2.innerHTML = "<h2>"+ totalBisemana +"</h2>";
 				
 			});
 
