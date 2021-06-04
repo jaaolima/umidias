54dbf17193e31c73754ea75ff10a21869f3fa2dc
jaaolima <victorespucoc@gmail.com>
2021-06-04 16:48:18 -0300

    carrinho

--- appCliente/ver_midia.php
+++ appCliente/ver_midia.php
@@ -381,8 +381,13 @@ License: You must have a valid license purchased only from themeforest(the above
 		jQuery(document).ready(function() {
 			demo3();
 		});
+
+		
 		$(document).ready(function() {
-			
+			//calculo outdoor
+			var totalBisemana = $("input[name='bisemana[]']:checked").length;
+			var valor2 = document.getElementById("valor2");
+
 			$("input[name='bisemana[]']").on("click", function(){
 				let taxa = 0.2 * (<?php echo $dados["nu_valor"] ?> * totalBisemana); 
 				let valor = <?php echo $dados["nu_valor"] ?> * totalBisemana;
@@ -404,9 +409,7 @@ License: You must have a valid license purchased only from themeforest(the above
 			valor1.innerHTML = "<h2>"+ total +"</h2>";
 		}
 
-		//calculo outdoor
-		var totalBisemana = $("input[name='bisemana[]']:checked").length;
-		var valor2 = document.getElementById("valor2");
+		
 		
 
 		
