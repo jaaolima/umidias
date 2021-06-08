2862b0aba071e861e2f0707a58ef1aa08c8dc2b7
jaaolima <victorespucoc@gmail.com>
2021-06-08 11:49:25 -0300

    admin

--- principal.php
+++ principal.php
@@ -475,9 +475,12 @@
 															<h3 class="card-title titulo-div">Total de Midias: <?php echo $dadosTotalMidias["id_ponto"]; ?></h3>
 														</div>
 														<div id="chart_2"></div>
-														<div class="d-flex"><h5>Contratadas: </h5><?php echo $dadosTotalContratadas["id_ponto"]; ?></div>
-														<div class="d-flex"><h5>Pendentes: </h5><?php echo $dadosTotalPendentes["id_ponto"]; ?></div>
-														<div class="d-flex"><h5>Reservadas: </h5><?php echo $dadosTotalReservadas["id_ponto"]; ?></div>
+														<div class="d-flex">
+															<div class="d-flex"><h5>Contratadas: </h5><p style="color: green ;"><?php echo $dadosTotalContratadas["id_ponto"]; ?></p></div>
+															<div class="d-flex"><h5>Pendentes: </h5><p style="color:yellow;"><?php echo $dadosTotalPendentes["id_ponto"]; ?></p></div>
+															<div class="d-flex"><h5>Reservadas: </h5><p style="color: red;"><?php echo $dadosTotalReservadas["id_ponto"]; ?></p></div>
+														</div>
+														
 													</div>
 												</div>
 											</div>
