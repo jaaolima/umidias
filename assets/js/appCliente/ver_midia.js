4bc6fcd6a11a12e9125b1a11b92c29e0da07fbc8
jaaolima <victorespucoc@gmail.com>
2021-06-17 17:42:16 -0300

    principal

--- assets/js/appCliente/ver_midia.js
+++ assets/js/appCliente/ver_midia.js
@@ -169,7 +169,7 @@ $(document).ready(function() {
 					bisemana.push( $(this).val());
 				});
 				var id_material = $("#id_material").val();
-				var ds_arte = $('input[type=file]');
+				var ds_arte = $('input[type=file]').files[0];
 				redirectTo("appCliente/pagamento.php?id_ponto="+id_ponto+"&bisemana="+bisemana+"&ds_arte="+ds_arte+"&id_material="+id_material);
 			}
 			
