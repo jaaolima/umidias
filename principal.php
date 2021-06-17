a3e9bd99e33a89e6a4f2f760c470367db604f387
jaaolima <victorespucoc@gmail.com>
2021-06-17 15:25:59 -0300

    principal

--- principal.php
+++ principal.php
@@ -618,7 +618,6 @@
 		<script src="assets/js/custom.js"></script>
 		<script src="//maps.google.com/maps/api/js?key=AIzaSyBTGnKT7dt597vo9QgeQ7BFhvSRP4eiMSM&callback=initialize"></script>
 		<script src="assets/plugins/custom/gmaps/gmaps.js"></script>
-		<script src="assets/js/pages/crud/file-upload/dropzonejs.js"></script>
 		<!-- <script src="assets/js/scripts.bundle2.min.js"></script> -->
 		<script>
 			const primary = '#6993FF';
