aaff66f3c7b0dc79d834f576410709d2951eb52c
jaaolima <victorespucoc@gmail.com>
2021-06-17 16:35:33 -0300

    principal

--- appCliente/carrinho.php
+++ appCliente/carrinho.php
@@ -84,12 +84,11 @@ License: You must have a valid license purchased only from themeforest(the above
                                                         <div class='my-8 mx-15 text-left'>
                                                             <div class='d-flex ml-n8'>
                                                                 <div class='mt-1'>
-                                                                    <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                                                                        <path d='M17 21V19C17 17.9391 16.5786 16.9217 15.8284 16.1716C15.0783 15.4214 14.0609 15 13 15H5C3.93913 15 2.92172 15.4214 2.17157 16.1716C1.42143 16.9217 1 17.9391 1 19V21' stroke='#57616A' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/>
-                                                                        <path d='M9 11C11.2091 11 13 9.20914 13 7C13 4.79086 11.2091 3 9 3C6.79086 3 5 4.79086 5 7C5 9.20914 6.79086 11 9 11Z' stroke='#57616A' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/>
-                                                                        <path d='M23 20.9999V18.9999C22.9993 18.1136 22.7044 17.2527 22.1614 16.5522C21.6184 15.8517 20.8581 15.3515 20 15.1299' stroke='#57616A' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/>
-                                                                        <path d='M16 3.12988C16.8604 3.35018 17.623 3.85058 18.1676 4.55219C18.7122 5.2538 19.0078 6.11671 19.0078 7.00488C19.0078 7.89305 18.7122 8.75596 18.1676 9.45757C17.623 10.1592 16.8604 10.6596 16 10.8799' stroke='#57616A' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/>
-                                                                    </svg>
+                                                                <svg width='50' height='46' viewBox='0 0 50 46' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                                                                    <path d='M44.2 1H5.8C3.14903 1 1 3.14903 1 5.8V29.8C1 32.451 3.14903 34.6 5.8 34.6H44.2C46.851 34.6 49 32.451 49 29.8V5.8C49 3.14903 46.851 1 44.2 1Z' stroke='#333333' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/>
+                                                                    <path d='M20.2 44.2H29.8' stroke='#333333' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/>
+                                                                    <path d='M25 34.6001V44.2001' stroke='#333333' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/>
+                                                                </svg>
                                                                 </div>
                                                                 <div class='ml-2'>
                                                                     <span class='text-dark font-weight-bold text-dark font-size-h4'>Tipo:</span><br>
