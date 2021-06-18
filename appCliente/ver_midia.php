5264d39d739fed01f5827e1e64661ecdb561c3c3
jaaolima <victorespucoc@gmail.com>
2021-06-18 16:13:09 -0300

    principal

--- appCliente/ver_midia.php
+++ appCliente/ver_midia.php
@@ -217,9 +217,11 @@ License: You must have a valid license purchased only from themeforest(the above
 													</table>												
 												</div>
 												<div class="separator separator-solid"></div>
+												<div class="my-6 mx-6">
+													<h3 class="font-weight-bolder">Material</h3>
+												</div>
 												<div class=" my-6 mx-6">
 													<div class="form-group">
-														<label>Tipo de material</label>
 														<select name="id_material" id="id_material" class="form-control">
 															<option value="lona">Lona</option>
 														</select>
@@ -253,9 +255,11 @@ License: You must have a valid license purchased only from themeforest(the above
 											
 												</div>
 												<div class="separator separator-solid"></div>
+												<div class="my-6 mx-6">
+													<h3 class="font-weight-bolder">Material</h3>
+												</div>
 												<div class="d-flex my-6 mx-6" >
 													<div class="form-group">
-														<label>Tipo de material</label>
 														<select name="id_material" id="id_material" class="form-control">
 															<option value="">Selecione...</option>
 															<?php 
