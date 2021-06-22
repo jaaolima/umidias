c7a9548f3cda8906e80d562e554f0251b10aaa96
jaaolima <victorespucoc@gmail.com>
2021-06-22 17:18:50 -0300

    parceiro

--- Classes/Material.php
+++ Classes/Material.php
@@ -125,11 +125,11 @@
 				{
 					$valores = explode(",", $id_material);
 					if(in_array($dados['id_material'], $valores)){
-						$options.= "<input type='checkbox' id='".$dados['id_material']."' name='id_material[]' value='".$dados['id_material']." ' checked>
+						$options.= "<input type='checkbox' id='".$dados['id_material']."' name='id_material[]' value='".$dados['id_material']."' checked>
 							<label for='".$dados['id_material']."'>".$dados['ds_material']."</label></br>"; 
 					}
 					else{
-						$options.= "<input type='checkbox' id='".$dados['id_material']."' name='id_material[]' value='".$dados['id_material']." '>
+						$options.= "<input type='checkbox' id='".$dados['id_material']."' name='id_material[]' value='".$dados['id_material']."'>
 							<label for='".$dados['id_material']."'>".$dados['ds_material']."</label></br>"; 
 					} 
 					
