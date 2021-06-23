fe8e8ad378fe1f95f838976de8f28dc406c8ba03
jaaolima <victorespucoc@gmail.com>
2021-06-23 15:34:47 -0300

    parceiro

--- appCliente/ver_midia.php
+++ appCliente/ver_midia.php
@@ -477,6 +477,8 @@ License: You must have a valid license purchased only from themeforest(the above
 			let valor = $(this).find(':selected').attr('valor')
 			let total =  parseInt(valor, 10);
 			valormidia.innerHTML = total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+			var valorTotal = parseInt($('#valor2').val());
+			valorMidia.innerHTML = "<h2>"+ (total + valorTotal).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) +"</h2>"
 		}
 
 		
