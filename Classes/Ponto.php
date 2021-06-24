5352589eca6a3565e1f61bac42b8754cf105e461
jaaolima <victorespucoc@gmail.com>
2021-06-24 17:02:04 -0300

    parceiro

--- Classes/Ponto.php
+++ Classes/Ponto.php
@@ -1012,7 +1012,7 @@
 				$con = Conecta::criarConexao();
 				
 				
-				$select = "SELECT a.id_alugado, a.id_ponto, ds_descricao, ds_latitude, ds_longitude, nu_valor, ds_tipo, ds_observacao, ds_local, ds_tamanho,  p.id_midia, a.id_material, id_periodo, id_parceiro, dt_inicial, dt_final, ds_nome
+				$select = "SELECT a.id_alugado, a.id_ponto, ds_descricao, ds_latitude, ds_longitude, nu_valor, ds_tipo, ds_observacao, ds_local, ds_tamanho,  p.id_midia, a.id_material, id_periodo, p.id_parceiro, dt_inicial, dt_final, ds_nome
 						FROM rl_alugado a
 						right join tb_ponto p on a.id_ponto=p.id_ponto
 						inner join tb_tipo_midia t on p.id_midia=t.id_midia
