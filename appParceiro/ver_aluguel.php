1bbd0cfc159ff15752a171d9aee37b9fbcb452cc
jaaolima <victorespucoc@gmail.com>
2021-06-25 14:38:22 -0300

    parceiro

--- appParceiro/ver_aluguel.php
+++ appParceiro/ver_aluguel.php
@@ -1,5 +1,10 @@
 <?php
+ini_set('display_errors',1);
+ini_set('display_startup_erros',1);
+error_reporting(E_ALL);
 
+require_once("../Classes/Ponto.php");
+$ponto = new Ponto();
 ?>
 <div class="timeline timeline-6 mt-3">
     <!--begin::Item-->
