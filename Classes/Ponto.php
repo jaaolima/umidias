cd02b0fd6a4de428690e72497dd31b089372de13
jaaolima <victorespucoc@gmail.com>
2021-06-30 16:01:19 -0300

    gerenciar aluguel

--- Classes/Ponto.php
+++ Classes/Ponto.php
@@ -1045,7 +1045,7 @@
 				
 				
 				$select = "SELECT 
-							a.id_ponto, ds_descricao, ds_latitude, ds_longitude, p.nu_valor, ds_tipo, ds_observacao, ds_local, ds_tamanho,  p.id_midia, a.ds_material, id_periodo, id_parceiro, dt_inicial, dt_final, nu_valor_alugado
+							a.id_ponto, ds_descricao, ds_latitude, ds_longitude, p.nu_valor, ds_tipo, ds_observacao, ds_local, ds_tamanho,  p.id_midia, m.ds_material, id_periodo, id_parceiro, dt_inicial, dt_final, nu_valor_alugado
 						FROM rl_alugado a
 						right join tb_ponto p on a.id_ponto=p.id_ponto
 						inner join tb_tipo_midia t on p.id_midia=t.id_midia
