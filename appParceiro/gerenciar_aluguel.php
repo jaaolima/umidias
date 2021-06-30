4b108d84378fe433865e9737c3ce073acb6d195e
jaaolima <victorespucoc@gmail.com>
2021-06-30 15:30:30 -0300

    gerenciar aluguel

--- appParceiro/gerenciar_aluguel.php
+++ appParceiro/gerenciar_aluguel.php
@@ -108,7 +108,7 @@ License: You must have a valid license purchased only from themeforest(the above
                                     </div>
                                 </div>
                                 <div class="col-4">
-                                    <div id="timeline"></div>
+                                    <div id="timeline "></div>
                                 </div>
                             </div>		
 							<!--end::Content-->
