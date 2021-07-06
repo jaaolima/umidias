8d2676667bd4d73d41ef75202726806400e84027
jaaolima <victorespucoc@gmail.com>
2021-07-06 14:52:41 -0300

    logos

--- index.php
+++ index.php
@@ -50,41 +50,11 @@ License: You must have a valid license purchased only from themeforest(the above
 		<div class="d-flex flex-column flex-root">
 			<!--begin::Login-->
 			<div class="login login-1 login-signin-on d-flex flex-column flex-lg-row flex-column-fluid main" id="kt_login">
-				<!--begin::Aside-->
-				<div class="login-aside flex-row-auto bgi-size-cover bgi-no-repeat max-w-lg-550px " >
-					<!--begin: Aside Container-->
-					<?php /*?><div class="d-flex flex-row-fluid flex-column justify-content-between"> 
-						<!--begin: Aside header-->
-						<a href="principal.php" class="flex-column-auto mt-5 pb-lg-0 pb-10">
-                            <img alt="Logo" src="assets/media/logo.png" class="logo-default max-h-55px" />
-						</a>
-						<!--end: Aside header-->
-						<!--begin: Aside content-->
-						<div class="flex-column-fluid d-flex flex-column justify-content-center">
-							<h3 class="font-size-h1 mb-5 text-black">Bem vindo a Unimídias</h3>
-							<!--<p class="font-weight-lighter text-black opacity-80">The ultimate Bootstrap, Angular 8, React &amp; VueJS admin theme framework for next generation web apps.</p>-->
-						</div>
-						<!--end: Aside content-->
-						<!--begin: Aside footer for desktop-->
-						<div class="d-none flex-column-auto d-lg-flex justify-content-between mt-10">
-							<div class="opacity-70 font-weight-bold text-black">© 2020 Unimidias</div>
-							<div class="d-flex">
-								<a href="#" class="text-black">Sobre</a>
-								<a href="#" class="text-black ml-10">Parceiros</a>
-								<a href="#" class="text-black ml-10">Contato</a>
-							</div>
-						</div>
-
-						<!--end: Aside footer for desktop-->
-					</div><?php */?>
-					<!--end: Aside Container-->
-				</div>
-				<!--begin::Aside-->
 				<!--begin::Content-->
 				<div class="d-flex flex-column flex-row-fluid position-relative p-7 overflow-hidden">
 					<!--begin::Content body-->
 					<div class="d-flex flex-column-fluid flex-center mt-30 mt-lg-0">
-						<div class="card card-custom bgi-no-repeat bgi-size-cover gutter-b bg-white w-100 box-shadow"  style="max-width: 400px;">
+						<div class="card card-custom bgi-no-repeat bgi-size-cover gutter-b bg-white w-100 box-shadow"  style="max-width: 400px;left:300px;">
 							<!--begin::Signin-->
 							<div class=" login-signin">
 								<div class="mx-8 mt-8">
