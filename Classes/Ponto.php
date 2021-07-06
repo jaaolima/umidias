f2dbd93baea01c3ecc7a7f210b28876f8d306172
jaaolima <victorespucoc@gmail.com>
2021-07-06 16:09:29 -0300

    logos

--- Classes/Ponto.php
+++ Classes/Ponto.php
@@ -515,12 +515,13 @@
 			try{
 				$con = Conecta::criarConexao();
 				
-				$select = "SELECT p.id_ponto, nu_valor, t.id_midia, ds_local, t.ds_tipo, f.ds_foto, ds_descricao,  ds_observacao, u.ds_nome, a.dt_inicial, a.dt_final
+				$select = "SELECT p.id_ponto, nu_valor, t.id_midia, ds_local, t.ds_tipo, f.ds_foto, ds_descricao,  ds_observacao, u.ds_nome, a.dt_inicial, a.dt_final, id_status_mida, s.ds_status
 							FROM rl_alugado a
 							right join tb_ponto p on a.id_ponto=p.id_ponto
 							right join rl_ponto_foto f on p.id_ponto=f.id_ponto
 							inner join tb_tipo_midia t on p.id_midia=t.id_midia
 							inner join tb_usuario u on a.id_usuario=u.id_usuario
+							left join tb_status_midia s on a.id_status_midia=s.id_status
 							where p.id_parceiro=:id_parceiro
 							and f.ds_foto = (select min(ds_foto) from rl_ponto_foto pf where p.id_ponto = pf.id_ponto)
 							order by a.dt_inicial
