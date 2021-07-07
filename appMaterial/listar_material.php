65957f180bd9b24f61eec2e168f0247ffc80b3c3
jaaolima <victorespucoc@gmail.com>
2021-07-07 09:47:10 -0300

    bisemana

--- appMaterial/listar_material.php
+++ appMaterial/listar_material.php
@@ -11,7 +11,7 @@
 <div class="card card-custom gutter-b">
 	<div class="card-header flex-wrap py-3">
 		<div class="card-title">
-		<i class="flaticon-folder icon-xl svg-hover svg-material"></i>
+		<i class="flaticon-folder icon-xl mr-2"></i>
 			<h3 class="card-label">
 				Materiais Cadastrados
 			</h3>
