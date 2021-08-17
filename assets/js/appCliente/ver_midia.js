9a47186d075f46f5a12b1ea4224bf43a2c1ef9a9
jaaolima <victorespucoc@gmail.com>
2021-08-17 16:03:23 -0300

    carrinho

--- assets/js/appCliente/ver_midia.js
+++ assets/js/appCliente/ver_midia.js
@@ -106,7 +106,7 @@ $(document).ready(function() {
 		{
 			var id_usuario = $("#id_usuario").val();
 			var id_ponto = $("#id_ponto").val();
-			var ds_arte = $('[name="arte[]"]').val();
+			var ds_arte = $('[name="arte[]"]').files();
 			var nu_valor_alugado = $("#valor_alugado").text();
 			var id_material = $("#id_material").val();
 
