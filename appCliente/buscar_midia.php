de8fe5145402dc702d4cfc8966ae24e891b70f3a
jaaolima <victorespucoc@gmail.com>
2021-08-19 15:46:58 -0300

    carrinho

--- appCliente/buscar_midia.php
+++ appCliente/buscar_midia.php
@@ -212,7 +212,7 @@ License: You must have a valid license purchased only from themeforest(the above
 				div: '#map',
 				lat: position.coords.latitude,
 				lng: position.coords.longitude,
-				zoom: 30
+				zoom: 8
 			});
 
 			/*google.maps.event.addListener(map, 'dblclick', function(event) {
