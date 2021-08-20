e180b5f90fef21dbe8cb2a7d2c8dbbcccbb6e694
jaaolima <victorespucoc@gmail.com>
2021-08-20 14:24:38 -0300

    carrinho

--- Classes/Bisemana.php
+++ Classes/Bisemana.php
@@ -164,7 +164,7 @@
 				$select = "SELECT id_bisemana, ds_bisemana, dt_final, dt_inicial
 							FROM tb_bisemana b
 							where dt_final > :hoje and dt_inicial not in (select dt_inicial from rl_alugado a where a.id_ponto=:id_ponto)
-							order by dt_inicial desc";
+							order by dt_inicial";
 				
 				$stmt = $con->prepare($select); 
 				$params = array(':hoje' => $hoje,
