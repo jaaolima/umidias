2f0fd9d3c4089d497d8334d4c59cc73fc948ac6e
jaaolima <victorespucoc@gmail.com>
2021-08-20 17:51:11 -0300

    Semlimites

--- SemLimites/principal.php
+++ SemLimites/principal.php
@@ -48,7 +48,7 @@ License: You must have a valid license purchased only from themeforest(the above
 					<div id="kt_header_mobile" class="header-mobile">
 						<!--begin::Logo-->
 						<a href="index.html">
-							<img alt="Logo" src="assets/media/Logo-Horizontal-02.png" class="max-h-30px" /> 
+							<img alt="Logo" src="assets/media/Logo-Horizontal-01.png" class="max-h-30px" /> 
 						</a>
 						<!--end::Logo-->
 						<!--begin::Toolbar-->
@@ -81,7 +81,7 @@ License: You must have a valid license purchased only from themeforest(the above
 							<div class="d-none d-lg-flex align-items-center mr-3">
 								<!--begin::Logo-->
 								<a href="index.html">
-                                    <img alt="Logo" src="assets/media/Logo-Horizontal-02.png" class="max-h-120px" /> 
+                                    <img alt="Logo" src="assets/media/Logo-Horizontal-01.png" class="max-h-120px" /> 
                                 </a>
 								<!--end::Logo-->
 							</div>
