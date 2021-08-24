1e7f11fe6de6ce1cc20588d48efda1e82315a71d
jaaolima <victorespucoc@gmail.com>
2021-08-24 17:27:50 -0300

    semlimites

--- SemLimites/principal.php
+++ SemLimites/principal.php
@@ -292,22 +292,19 @@ License: You must have a valid license purchased only from themeforest(the above
 													<span class="card-label font-weight-bolder text-dark">Treinos</span>
 												</h3>
 											</div>
-											<div class="card-body">
-												
-												<div class="owl-carousel owl-theme">
-													<div class="item"><h4>1</h4></div>
-													<div class="item"><h4>2</h4></div>
-													<div class="item"><h4>3</h4></div>
-													<div class="item"><h4>4</h4></div>
-													<div class="item"><h4>5</h4></div>
-													<div class="item"><h4>6</h4></div>
-													<div class="item"><h4>7</h4></div>
-													<div class="item"><h4>8</h4></div>
-													<div class="item"><h4>9</h4></div>
-													<div class="item"><h4>10</h4></div>
-													<div class="item"><h4>11</h4></div>
-													<div class="item"><h4>12</h4></div>
-												</div>
+											<div class="owl-carousel owl-theme">
+												<div class="item"><h4>1</h4></div>
+												<div class="item"><h4>2</h4></div>
+												<div class="item"><h4>3</h4></div>
+												<div class="item"><h4>4</h4></div>
+												<div class="item"><h4>5</h4></div>
+												<div class="item"><h4>6</h4></div>
+												<div class="item"><h4>7</h4></div>
+												<div class="item"><h4>8</h4></div>
+												<div class="item"><h4>9</h4></div>
+												<div class="item"><h4>10</h4></div>
+												<div class="item"><h4>11</h4></div>
+												<div class="item"><h4>12</h4></div>
 											</div>
 										</div>
 										<!--end::Tiles Widget 25-->
