98e3ee3b6ce23565db8f4519d58c7069ecc3c1c7
jaaolima <victorespucoc@gmail.com>
2021-08-25 14:51:16 -0300

    semlimites

---
+++ SemLimites/appProfessor/cadastro_treino.php
@@ -0,0 +1,55 @@
+
+
+<div class="card card-custom">
+    <div class="card-header">
+        <h3 class="card-title">
+            Cadastro de Treinos
+        </h3>
+        <div class="card-toolbar">
+            <div class="example-tools justify-content-center">
+                <span class="example-toggle" data-toggle="tooltip" title="View code"></span>
+                <span class="example-copy" data-toggle="tooltip" title="Copy code"></span>
+            </div>
+        </div>
+    </div>
+    <!--begin::Form-->
+    <form id="form_usuario">
+        <div class="card-body">
+            <div class="form-group row">
+                <div class="form-group col-md-3">
+                    <label>Exercício <span class="text-danger">*</span></label>
+                    <input type="text" class="form-control" id="ds_nome" name="ds_nome" />
+                </div>
+                <div class="form-group col-md-3">
+                    <label >Séries <span class="text-danger">*</span></label>
+                    <input type="mail" class="form-control" id="ds_email" name="ds_email" />
+                </div>
+                <div class="form-group col-md-3">
+                    <label >Repetições<span class="text-danger">*</span></label>
+                    <input type="text" class="form-control" id="ds_usuario" name="ds_usuario" />
+                </div> 
+            </div> 
+            <div class="form-group row"> 
+                <div class="form-group col-md-3">
+                    <label >Carga<span class="text-danger">*</span></label>
+                    <input type="text" class="form-control" id="ds_usuario" name="ds_usuario" />
+                </div> 
+                <div class="form-group col-md-3">
+                    <label >Cadência<span class="text-danger">*</span></label>
+                    <input type="text" class="form-control" id="ds_usuario" name="ds_usuario" />
+                </div> 
+                <div class="form-group col-md-3">
+                    <label >Intervalo<span class="text-danger">*</span></label>
+                    <input type="text" class="form-control" id="ds_usuario" name="ds_usuario" />
+                </div> 
+            </div>
+        </div>
+        <div class="card-footer">
+            <button type="button" class="btn btn-primary mr-2" id="salvar">Enviar</button>
+            <button type="reset" class="btn btn-secondary" id="cancelar">Cancelar</button>
+        </div>
+    </form>
+    <!--end::Form-->
+</div>
+<script src="./assets/js/appProfessor/cadastro_treino.js" type="text/javascript"></script>
+
