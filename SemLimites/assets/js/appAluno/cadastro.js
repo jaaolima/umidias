381ae1e21676e15b954fd4fe8bae412b6fd04450
jaaolima <victorespucoc@gmail.com>
2021-08-31 15:30:48 -0300

    semlimites

--- SemLimites/assets/js/appAluno/cadastro.js
+++ SemLimites/assets/js/appAluno/cadastro.js
@@ -7,7 +7,8 @@ $(document).ready(function() {
         }); 
     
         $("#salvar").on("click", function(e){ 
-            if(validar())
+            var cpf = $("#nu_cpf").val();
+            if(validar() && validarCPF(cpf))
             { 
                 $.ajax({
                     url: 'appAluno/gravar_aluno.php'
@@ -30,6 +31,11 @@ $(document).ready(function() {
             }	
         });
     });
+
+    $("#nu_cpf").inputmask({
+		"mask": "999.999.999-99",
+		autoUnmask: true,
+	});
 
     
     function validar()
@@ -140,4 +146,55 @@ $(document).ready(function() {
         }
     
         return true;
+    }
+
+    function validarCPF(cpf) {	
+
+        cpf = cpf.replace(/[^\d]+/g,'');
+        // Elimina CPFs invalidos conhecidos	
+        if (cpf.length != 11 || 
+            cpf == "00000000000" || 
+            cpf == "11111111111" || 
+            cpf == "22222222222" || 
+            cpf == "33333333333" || 
+            cpf == "44444444444" || 
+            cpf == "55555555555" || 
+            cpf == "66666666666" || 
+            cpf == "77777777777" || 
+            cpf == "88888888888" || 
+            cpf == "99999999999")
+        {
+            $("#nu_cpf").focus();
+            swal.fire("Erro", "CPF inválido", "error");
+            $("#nu_cpf").addClass("is-invalid");
+            return false;		
+        }	
+        // Valida 1o digito	
+        add = 0;	
+        for (i=0; i < 9; i ++)		
+            add += parseInt(cpf.charAt(i)) * (10 - i);	
+            rev = 11 - (add % 11);	
+            if (rev == 10 || rev == 11)		
+                rev = 0;	
+            if (rev != parseInt(cpf.charAt(9)))		
+            {
+                $("#nu_cpf").focus();
+                swal.fire("Erro", "CPF inválido", "error");
+                $("#nu_cpf").addClass("is-invalid");
+                return false;		
+            }			
+        // Valida 2o digito	
+        add = 0;	
+        for (i = 0; i < 10; i ++)		
+            add += parseInt(cpf.charAt(i)) * (11 - i);	
+        rev = 11 - (add % 11);	
+        if (rev == 10 || rev == 11)	
+            rev = 0;	
+        if (rev != parseInt(cpf.charAt(10)))
+        {
+            $("#nu_cpf").focus();
+            swal.fire("Erro", "CPF inválido", "error");
+            $("#nu_cpf").addClass("is-invalid");
+            return false;		
+        }			  
     }
