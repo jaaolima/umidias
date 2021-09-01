34268347842054ef5422aa35eb5dd65c1ba5981d
jaaolima <victorespucoc@gmail.com>
2021-09-01 16:04:59 -0300

    semlimites

--- SemLimites/principal.php
+++ SemLimites/principal.php
@@ -276,6 +276,33 @@ License: You must have a valid license purchased only from themeforest(the above
 					<div class="d-flex flex-row flex-column-fluid container">
 						<!--begin::Content Wrapper-->
 						<div class="main d-flex flex-column flex-row-fluid">
+							<!--begin::Subheader-->
+							<div class="subheader py-2 py-lg-6" id="kt_subheader">
+								<div class="w-100 d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
+									<!--begin::Info-->
+									<div class="d-flex align-items-center flex-wrap mr-1">
+										<!--begin::Page Heading-->
+										<div class="d-flex align-items-baseline flex-wrap mr-5">
+											<!--begin::Page Title-->
+											<h5 class="text-dark font-weight-bold my-1 mr-5">Reports</h5>
+											<!--end::Page Title-->
+											<!--begin::Breadcrumb-->
+											<ul class="breadcrumb breadcrumb-transparent breadcrumb-dot font-weight-bold p-0 my-2 font-size-sm">
+												<li class="breadcrumb-item">
+													<a href="" class="text-muted">Dashboard</a>
+												</li>
+												<li class="breadcrumb-item">
+													<a href="" class="text-muted">Application</a>
+												</li>
+											</ul>
+											<!--end::Breadcrumb-->
+										</div>
+										<!--end::Page Heading-->
+									</div>
+									<!--end::Info-->
+								</div>
+							</div>
+							<!--end::Subheader-->
 							<div class="content flex-column-fluid" id="conteudo">
 								<!--begin::Dashboard-->
 								<div class="row">
