417157e197a4ffaaee5aa593a67adbe72c056ca4
jaaolima <victorespucoc@gmail.com>
2021-09-01 15:43:50 -0300

    semlimites

--- SemLimites/principal.php
+++ SemLimites/principal.php
@@ -12,6 +12,10 @@
 	}
    	$id_usuario = $_SESSION['id_usuario'];
 
+	require_once("Classes/Usuario.php");
+	
+	$dadosUsuario = $usuario->buscarDadosUsuario($id_usuario);
+
 ?>
 <!DOCTYPE html>
 <!--
@@ -140,62 +144,6 @@ License: You must have a valid license purchased only from themeforest(the above
 									<!--end::Toggle-->
 									<!--begin::Dropdown-->
 									<div class="dropdown-menu p-0 m-0 dropdown-menu-right dropdown-menu-anim-up dropdown-menu-lg p-0">
-										<!--begin::Header-->
-										<div class="d-flex align-items-center p-8 rounded-top">
-											<!--begin::Symbol-->
-											<div class="symbol symbol-md bg-light-primary mr-3 flex-shrink-0">
-												<img src="assets/media/users/300_21.jpg" alt="" />
-											</div>
-											<!--end::Symbol-->
-											<!--begin::Text-->
-											<div class="text-dark m-0 flex-grow-1 mr-3 font-size-h5">Sean Stone</div>
-											<span class="label label-light-success label-lg font-weight-bold label-inline">3 messages</span>
-											<!--end::Text-->
-										</div>
-										<div class="separator separator-solid"></div>
-										<!--end::Header-->
-										<!--begin::Nav-->
-										<div class="navi navi-spacer-x-0 pt-5">
-											<!--begin::Item-->
-											<a href="custom/apps/user/profile-1/personal-information.html" class="navi-item px-8">
-												<div class="navi-link">
-													<div class="navi-icon mr-2">
-														<i class="flaticon2-calendar-3 text-success"></i>
-													</div>
-													<div class="navi-text">
-														<div class="font-weight-bold">My Profile</div>
-														<div class="text-muted">Account settings and more
-														<span class="label label-light-danger label-inline font-weight-bold">update</span></div>
-													</div>
-												</div>
-											</a>
-											<!--end::Item-->
-											<!--begin::Item-->
-											<a href="custom/apps/user/profile-3.html" class="navi-item px-8">
-												<div class="navi-link">
-													<div class="navi-icon mr-2">
-														<i class="flaticon2-mail text-warning"></i>
-													</div>
-													<div class="navi-text">
-														<div class="font-weight-bold">My Messages</div>
-														<div class="text-muted">Inbox and tasks</div>
-													</div>
-												</div>
-											</a>
-											<!--end::Item-->
-											<!--begin::Item-->
-											<a href="custom/apps/user/profile-2.html" class="navi-item px-8">
-												<div class="navi-link">
-													<div class="navi-icon mr-2">
-														<i class="flaticon2-rocket-1 text-danger"></i>
-													</div>
-													<div class="navi-text">
-														<div class="font-weight-bold">My Activities</div>
-														<div class="text-muted">Logs and notifications</div>
-													</div>
-												</div>
-											</a>
-											<!--end::Item-->
 											<!--begin::Item-->
 											<a href="custom/apps/userprofile-1/overview.html" class="navi-item px-8">
 												<div class="navi-link">
