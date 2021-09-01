922eab2537a571a0a5ce982e48da79e5ee992d6b
jaaolima <victorespucoc@gmail.com>
2021-09-01 16:18:15 -0300

    semlimites

--- SemLimites/principal.php
+++ SemLimites/principal.php
@@ -174,7 +174,7 @@ License: You must have a valid license purchased only from themeforest(the above
 							<div id="kt_header_menu" class="header-menu header-menu-left header-menu-mobile header-menu-layout-default header-menu-root-arrow">
 								<!--begin::Header Nav-->
 								<ul class="menu-nav">
-									<li class="menu-item menu-item-submenu menu-item-rel menu-item-open" data-menu-toggle="click" aria-haspopup="true">
+									<li class="menu-item menu-item-submenu menu-item-rel menu-item-open menu-item-here" data-menu-toggle="click" aria-haspopup="true">
 										<a href="#" class="menu-link menu-toggle">
 											<span class="menu-text">Dashboard</span>
 										</a>
