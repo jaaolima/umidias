a3ca44651f98262e75a193986ea95a92e7aeb2a1
jaaolima <victorespucoc@gmail.com>
2021-09-02 16:12:57 -0300

    login

--- appUsuario/validar_usuario.php
+++ appUsuario/validar_usuario.php
@@ -8,7 +8,7 @@ $email = $_REQUEST['ds_email'];
 $nome = $_REQUEST['ds_nome'];
 
 // Inclui o arquivo class.phpmailer.php localizado na mesma pasta do arquivo php 
-include "./assets\media\PHPMailer-master\PHPMailerAutoload.php"; 
+include "/var/www/app.unimidias.com.br/assets\media\PHPMailer-master\PHPMailerAutoload.php"; 
 
 // Inicia a classe PHPMailer 
 $mail = new PHPMailer(); 
