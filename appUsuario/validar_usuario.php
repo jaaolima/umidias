77a90847f0cd9333d8ed4a6f2d88328e1ac8d6d8
jaaolima <victorespucoc@gmail.com>
2021-09-02 17:57:33 -0300

    login

--- appUsuario/validar_usuario.php
+++ appUsuario/validar_usuario.php
@@ -23,9 +23,14 @@ $mail->Password = '85664147';
 $mail->Port = 587;
 $mail->IsHTML(true);
 $mail->CharSet = 'UTF-8'; 
-$mail->setFrom($email, $nome);
 
-// Charset (opcional) 
+$mail->From = "smtp-relay.sendinblue.com"; 
+
+// Seu nome 
+$mail->FromName = "Unimídias"; 
+
+// Define o(s) destinatário(s) 
+$mail->AddAddress($email, $nome); 
 
 
 // Assunto da mensagem 
