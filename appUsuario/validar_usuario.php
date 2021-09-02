b4341cd8412cf8245cb2a1e83ab3d0f690156df4
jaaolima <victorespucoc@gmail.com>
2021-09-02 17:53:24 -0300

    login

--- appUsuario/validar_usuario.php
+++ appUsuario/validar_usuario.php
@@ -17,48 +17,17 @@ $mail->IsSMTP();
 
 // // Enviar por SMTP 
 $mail->Host = "smtp-relay.sendinblue.com"; 
-
-// Você pode alterar este parametro para o endereço de SMTP do seu provedor 
-$mail->Port = 587; 
-
-
-// // Usar autenticação SMTP (obrigatório) 
 $mail->SMTPAuth = true; 
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-// Usuário do servidor SMTP (endereço de email) 
-// obs: Use a mesma senha da sua conta de email 
 $mail->Username = 'victorespucoc@gmail.com'; 
-$mail->Password = '85664147'; 
-
-// Configurações de compatibilidade para autenticação em TLS 
-$mail->SMTPOptions = array( 'ssl' => array( 'verify_peer' => false, 'verify_peer_name' => false, 'allow_self_signed' => true ) ); 
-
-// Você pode habilitar esta opção caso tenha problemas. Assim pode identificar mensagens de erro. 
-$mail->SMTPDebug = 2; 
-
-//Define o remetente 
-// Seu e-mail 
-$mail->From = "victorespucoc@gmail.com"; 
-
-// Seu nome 
-$mail->FromName = "Unimídias"; 
-
-// Define o(s) destinatário(s) 
-$mail->AddAddress($email, $nome); 
-
-// Opcional: mais de um destinatário
-// $mail->AddAddress('fernando@email.com'); 
-
-// Opcionais: CC e BCC
-// $mail->AddCC('joana@provedor.com', 'Joana'); 
-// $mail->AddBCC('roberto@gmail.com', 'Roberto'); 
-
-// Definir se o e-mail é em formato HTML ou texto plano 
-// Formato HTML . Use "false" para enviar em formato texto simples ou "true" para HTML.
-$mail->IsHTML(true); 
+$mail->Password = '85664147';
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+$mail->IsHTML(true);
+$mail->CharSet = 'UTF-8'; 
+$mail->setFrom($email, $nome);
 
 // Charset (opcional) 
-$mail->CharSet = 'UTF-8'; 
+
 
 // Assunto da mensagem 
 $mail->Subject = "Verificação do Login"; 
