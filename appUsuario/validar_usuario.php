5ae2b3d7d04315fcecf81b8d670b9fa2496fcde3
jaaolima <victorespucoc@gmail.com>
2021-09-03 14:22:39 -0300

    login

--- appUsuario/validar_usuario.php
+++ appUsuario/validar_usuario.php
@@ -17,6 +17,8 @@ $mail->IsSMTP();
 
 // // Enviar por SMTP 
 $mail->Host = "smtp-relay.sendinblue.com"; 
+$mail->SMTPAuth = false ;
+$mail->SMTPAutoTLS = false ; 
 $mail->SMTPAuth = true; 
 $mail->Username = 'victorespucoc@gmail.com'; 
 $mail->Password = '85664147';
