f1ddd947df8da67a95cda9b0546997062ae18867
jaaolima <victorespucoc@gmail.com>
2021-09-03 17:39:19 -0300

    login

--- appUsuario/_email_cadastro.php
+++ appUsuario/_email_cadastro.php
@@ -34,7 +34,27 @@ Olá, <?php echo $_REQUEST['ds_nome']; ?> <br>
 <?php 
 $senha = hash("SHA512",$_REQUEST['nu_senha']);
 ?>
-<a style="color: #ffffff;
+<a style="
+    display: inline-block;
+    font-weight: normal;
+    color: #3F4254;
+    text-align: center;
+    vertical-align: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    background-color: transparent;
+    border: 1px solid transparent;
+    padding: 0.65rem 1rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    border-radius: 16px !important;
+    -webkit-transition: color 0.3s ease-in-out, background-color 0.3s ease-in-out, border-color 0.3s ease-in-out, -webkit-box-shadow 0.3s ease-in-out;
+    transition: color 0.3s ease-in-out, background-color 0.3s ease-in-out, border-color 0.3s ease-in-out, -webkit-box-shadow 0.3s ease-in-out;
+    transition: color 0.3s ease-in-out, background-color 0.3s ease-in-out, border-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+    transition: color 0.3s ease-in-out, background-color 0.3s ease-in-out, border-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out, -webkit-box-shadow 0.3s ease-in-out;
+color: #ffffff;
   background-color: #B721FF;
   border-color: #B721FF;
   -webkit-box-shadow: none;
