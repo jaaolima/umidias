a466777cd53e57b68e70b258ba3b1d70d19cc12f
jaaolima <victorespucoc@gmail.com>
2021-09-03 14:54:17 -0300

    login

--- appUsuario/validar_usuario.php
+++ appUsuario/validar_usuario.php
@@ -38,12 +38,34 @@ $mail->Subject = "Verificação do Login";
 // Corpo do email 
 $mail->Body = '
 <link href="https://app.unimidias.com.br/assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+<style>
+.btn {
+    display: inline-block;
+    font-weight: normal;
+    color: #3F4254;
+    text-align: center;
+    vertical-align: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    background-color: transparent;
+    border: 1px solid transparent;
+    padding: 0.65rem 1rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    border-radius: 16px !important;
+    -webkit-transition: color 0.3s ease-in-out, background-color 0.3s ease-in-out, border-color 0.3s ease-in-out, -webkit-box-shadow 0.3s ease-in-out;
+    transition: color 0.3s ease-in-out, background-color 0.3s ease-in-out, border-color 0.3s ease-in-out, -webkit-box-shadow 0.3s ease-in-out;
+    transition: color 0.3s ease-in-out, background-color 0.3s ease-in-out, border-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+    transition: color 0.3s ease-in-out, background-color 0.3s ease-in-out, border-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out, -webkit-box-shadow 0.3s ease-in-out; }
+</style>
 <html>
     <div id="validar>
         <h2>Olá "'.$nome.'", </h2><br>
         <p>Valide seu Login</p><br>
         <div>
-            <button id="validar" class="btn btn-primary" onClick="Validar();">Validar</button>
+            <a href="https://app.unimidias.com.br/appUsuario/gravar_usuario.php?ds_nome="'.$nome.'"&ds_email="'.$email.'"&ds_usuario="'.$email.'"&id_perfil="1" id="validar" class="btn btn-primary">Validar</a>
         </div>
         <input type="hidden" id="ds_nome" value="'.$nome.'" />
         <input type="hidden" id="ds_email" value="'.$email.'" />
@@ -51,22 +73,6 @@ $mail->Body = '
     <div id="efetuado" style="display:none;">
     </div>
 </html>
-<script>
-ds_nome = document.getElementsByName("ds_nome").value();
-ds_email = document.getElementsByName("ds_email").value();
-
-ds_usuario = ds_email;
-id_perfil = 1;
-
-
-function Validar(){
-    redirectTo(`https://app.unimidias.com.br/appUsuario/gravar_usuario.php?ds_nome="`+ds_nome+`"&ds_email="`+ds_email+`"&ds_usuario="`+ds_usuario+`"&id_perfil="`+id_perfil);
-    var element = document.getElementById("efetuado");
-    element.style.display("flex");
-    var validar = document.getElementById("validar");
-    validar.style.display("none");
-}
-</script>
 '; 
 
 // Opcional: Anexos 
