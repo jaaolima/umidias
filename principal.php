24c00ee23468e643137d12819d401ff4f155f8a5
jaaolima <victorespucoc@gmail.com>
2021-09-09 16:04:39 -0300

    alterar senha

--- principal.php
+++ principal.php
@@ -483,6 +483,8 @@
 																	<h3  class="titulo-div">Mídias próximas à Você</h3>
 																</div>
 																<div class="row col-12 mt-4 owl-carousel owl-theme" >
+																	<button type="button" role="presentation" class="owl-prev"><span aria-label="Previous">‹</span></button>
+																	<button type="button" role="presentation" class="owl-next"><span aria-label="Next">›</span></button>
 																	<?php
 																		 while($dados = $retornoMidias->fetch()){
 																			echo "<div class='item' >
@@ -777,7 +779,6 @@
 			$(document).ready(function(){
 				$(".owl-carousel").owlCarousel({
 					items:4,
-					nav:true,
 					center:true,
 					loop:true,
 					dots:false,
