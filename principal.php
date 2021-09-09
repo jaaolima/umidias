69183722e6d5660109735cb7f6fce0b1e4ba278e
jaaolima <victorespucoc@gmail.com>
2021-09-09 16:01:25 -0300

    alterar senha

--- principal.php
+++ principal.php
@@ -486,7 +486,7 @@
 																	<?php
 																		 while($dados = $retornoMidias->fetch()){
 																			echo "<div class='item' >
-																					<div class='card card-custom card-2' style='height: 498;'>
+																					<div class='card card-custom card-2'>
 																						<!--begin::Body-->
 																						<div class='card-body text-center' style='padding: 0px !important'>
 																							<!--begin::User-->
