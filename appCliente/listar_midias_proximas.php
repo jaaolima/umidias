b68e6fd0db0ac621c003d909273bb924e486314c
jaaolima <victorespucoc@gmail.com>
2021-09-10 14:04:04 -0300

    recuperar senha

--- appCliente/listar_midias_proximas.php
+++ appCliente/listar_midias_proximas.php
@@ -3,7 +3,7 @@ ini_set('display_errors',1);
 ini_set('display_startup_erros',1);
 error_reporting(E_ALL);
 
-require_once("Classes/Ponto.php");
+require_once("../Classes/Ponto.php");
 
 $ponto = new Ponto();
 
