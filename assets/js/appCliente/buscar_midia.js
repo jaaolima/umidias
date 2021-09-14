72367c24d63b8c705ca54b34289a142a9bc54fa2
jaaolima <victorespucoc@gmail.com>
2021-09-14 18:01:43 -0300

    alterar senha

--- assets/js/appCliente/buscar_midia.js
+++ assets/js/appCliente/buscar_midia.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
             month: ('2-digit'),
             year: ('numeric'),
         } 
-        dataFinal = date.toLocaleDateString('en-US').format('YYYY-MM-DD');
+        dataUS = date.toLocaleDateString('en-US');
+        dataFinal = dataUS.format('YYYY-MM-DD');
         var id_midia = $("#id_midia").val();
         $.ajax({
             url: 'appCliente/listar_midia.php' 
