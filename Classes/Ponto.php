327d88d2f71db2b67d7937e369349c0a4d810a02
jaaolima <victorespucoc@gmail.com>
2021-09-14 16:22:25 -0300

    alteracoes

--- Classes/Ponto.php
+++ Classes/Ponto.php
@@ -232,7 +232,7 @@
 							$datasBisemana .= "(dt_inicial between ".$dados["dt_inicial"]." and ".$dados["dt_final"].")";
 						}
 						else{
-							$datasBisemana .= " or (dt_inicial between ".$dados["dt_inicial"]." and ".$dados["dt_final"].")";
+							$datasBisemana .= " and (dt_inicial between ".$dados["dt_inicial"]." and ".$dados["dt_final"].")";
 						}
 					}
 					catch(exception $e)
