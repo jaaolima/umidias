370f0ad3785ae1f272e907c3bf0ba6cc13a5be4c
jaaolima <victorespucoc@gmail.com>
2021-09-16 16:52:21 -0300

    cadastro ponto

--- assets/js/appCliente/fale_conosco.js
+++ assets/js/appCliente/fale_conosco.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     $("#enviar").on("click", function(e){ 
 		var cpf = $("#nu_cpf").val();
-		if(validar() && validarCPF(cpf))
+		if(validar())
 		{ 
 			$.ajax({
 		        url: 'appCliente/email_fale_conosco.php'
@@ -56,119 +56,20 @@ function validar()
 		$("#ds_email").addClass("is-valid");
 	}
 
-	if($("#ds_usuario").val() == "")
+    if($("#ds_mensagem").val() == "")
 	{
-		$("#ds_usuario").focus();
-		swal.fire("Erro", "Preencha o usuario", "error");
-		$("#ds_usuario").addClass("is-invalid");
+		$("#ds_mensagem").focus();
+		swal.fire("Erro", "Coloque uma mensagem", "error");
+		$("#ds_mensagem").addClass("is-invalid");
 		return false;	
 	}
 	else
 	{
-		$("#ds_usuario").removeClass("is-invalid");	
-		$("#ds_usuario").addClass("is-valid");
+		$("#ds_mensagem").removeClass("is-invalid");	
+		$("#ds_mensagem").addClass("is-valid");
 	}
-
-	if($("#ds_senha").val() == "")
-	{
-		$("#ds_senha").focus();
-		swal.fire("Erro", "Preencha a senha", "error");
-		$("#ds_senha").addClass("is-invalid");
-		return false;		
-	}
-	else
-	{
-		$("#ds_senha").removeClass("is-invalid");	
-		$("#ds_senha").addClass("is-valid");
-	}
-
-	if($("#ds_senha").val() !== "********")
-	{
-		var Tamanhosenha = $("#ds_senha").val().toString();
-		if (Tamanhosenha.length < 8)
-		{
-			$("#ds_senha").focus();
-			swal.fire("Erro", "A senha precisa ter 8 dígitos ou mais", "error");
-			return false;	
-		}
-
-		if($("#ds_senha_confirmada").val() == "")
-		{
-			$("#ds_senha_confirmada").focus();
-			swal.fire("Erro", "Confirme a senha", "error");
-			$("#ds_senha_confirmada").addClass("is-invalid");
-			return false;		
-		}
-		else
-		{
-			$("#ds_senha_confirmada").removeClass("is-invalid");	
-			$("#ds_senha_confirmada").addClass("is-valid");
-		}	
-		
-		
-		if($("#nu_senhaconfirmada").val() !== $("#nu_senha").val()){
-			$("#nu_senhaconfirmada").focus();
-			swal.fire("Erro", "A senha e sua confirmação não são iguais", "error");
-			return false;
-		}
-	
-	}
-
 
 	
 
 	return true;
-}
-
-function validarCPF(cpf) {	
-
-	cpf = cpf.replace(/[^\d]+/g,'');
-	// Elimina CPFs invalidos conhecidos	
-	if (cpf.length != 11 || 
-		cpf == "00000000000" || 
-		cpf == "11111111111" || 
-		cpf == "22222222222" || 
-		cpf == "33333333333" || 
-		cpf == "44444444444" || 
-		cpf == "55555555555" || 
-		cpf == "66666666666" || 
-		cpf == "77777777777" || 
-		cpf == "88888888888" || 
-		cpf == "99999999999")
-	{
-		$("#nu_cpf").focus();
-		swal.fire("Erro", "CPF inválido", "error");
-		$("#nu_cpf").addClass("is-invalid");
-		return false;		
-	}	
-	// Valida 1o digito	
-	add = 0;	
-	for (i=0; i < 9; i ++)		
-		add += parseInt(cpf.charAt(i)) * (10 - i);	
-		rev = 11 - (add % 11);	
-		if (rev == 10 || rev == 11)		
-			rev = 0;	
-		if (rev != parseInt(cpf.charAt(9)))		
-		{
-			$("#nu_cpf").focus();
-			swal.fire("Erro", "CPF inválido", "error");
-			$("#nu_cpf").addClass("is-invalid");
-			return false;		
-		}			
-	// Valida 2o digito	
-	add = 0;	
-	for (i = 0; i < 10; i ++)		
-		add += parseInt(cpf.charAt(i)) * (11 - i);	
-	rev = 11 - (add % 11);	
-	if (rev == 10 || rev == 11)	
-		rev = 0;	
-	if (rev != parseInt(cpf.charAt(10)))
-	{
-		$("#nu_cpf").focus();
-		swal.fire("Erro", "CPF inválido", "error");
-		$("#nu_cpf").addClass("is-invalid");
-		return false;		
-	}			
-	return true;   
-
 }
