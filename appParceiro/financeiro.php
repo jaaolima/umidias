59ab67435c62713eebc1fc4cfa6b5fa2b63e0294
jaaolima <victorespucoc@gmail.com>
2021-09-21 14:31:08 -0300

    financeiro

--- appParceiro/financeiro.php
+++ appParceiro/financeiro.php
@@ -15,7 +15,6 @@
 
 	$dadosUsuario = $usuario->buscarDadosUsuario($id_usuario);
 	$retorno = $midia->listarTipoMidia($_POST);
-	$minhasMidias = $ponto->listarminhasMidiasPendentes($id_usuario);
 
 ?>
 <!DOCTYPE html>
@@ -76,7 +75,7 @@ License: You must have a valid license purchased only from themeforest(the above
 				</div>
 			</div>
 		</div>
-		<div class="row">
+		<!-- <div class="row">
 			<div class="col-12">
 				<div class="card card-custom bgi-no-repeat bgi-size-cover gutter-b bg-white p-10"  >
 					<div class="my-6">
@@ -146,7 +145,7 @@ License: You must have a valid license purchased only from themeforest(the above
 					</div>
 				</div>
 			</div>
-		</div>
+		</div> -->
 		<script>var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script>
 		<!--begin::Global Config(global config for global JS scripts)-->
 		<script>var KTAppSettings = { "breakpoints": { "sm": 576, "md": 768, "lg": 992, "xl": 1200, "xxl": 1200 }, "colors": { "theme": { "base": { "white": "#ffffff", "primary": "#8950FC", "secondary": "#E5EAEE", "success": "#1BC5BD", "info": "#6993FF", "warning": "#FFA800", "danger": "#F64E60", "light": "#F3F6F9", "dark": "#212121" }, "light": { "white": "#ffffff", "primary": "#EEE5FF", "secondary": "#ECF0F3", "success": "#C9F7F5", "info": "#E1E9FF", "warning": "#FFF4DE", "danger": "#FFE2E5", "light": "#F3F6F9", "dark": "#D6D6E0" }, "inverse": { "white": "#ffffff", "primary": "#ffffff", "secondary": "#212121", "success": "#ffffff", "info": "#ffffff", "warning": "#ffffff", "danger": "#ffffff", "light": "#464E5F", "dark": "#ffffff" } }, "gray": { "gray-100": "#F3F6F9", "gray-200": "#ECF0F3", "gray-300": "#E5EAEE", "gray-400": "#D6D6E0", "gray-500": "#B5B5C3", "gray-600": "#80808F", "gray-700": "#464E5F", "gray-800": "#1B283F", "gray-900": "#212121" } }, "font-family": "Poppins" };</script>
