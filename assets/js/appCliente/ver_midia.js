d8f0ef19222c0669f5e7f46f6e18beb27dff2ae5
jaaolima <victorespucoc@gmail.com>
2021-09-22 17:02:05 -0300

    data

--- assets/js/appCliente/ver_midia.js
+++ assets/js/appCliente/ver_midia.js
@@ -108,7 +108,7 @@ $(document).ready(function() {
 				rightArrow: '<i class="la la-angle-right"></i>'
 			}
 		}
-		dt_inicial = $("#dt_inicial").val();
+		dt_inicial = new Date($("#dt_inicial").val());
 		console.log(dt_inicial);
 		$('#dt_final').datepicker({
 			format: 'dd/mm/yyyy',
