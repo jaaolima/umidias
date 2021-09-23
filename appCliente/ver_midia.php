a2e2272d84485415ac6085a012afd83a298a5088
jaaolima <victorespucoc@gmail.com>
2021-09-23 18:15:36 -0300

    data

--- appCliente/ver_midia.php
+++ appCliente/ver_midia.php
@@ -476,7 +476,7 @@ License: You must have a valid license purchased only from themeforest(the above
 			$('#datefilter').daterangepicker({
 				autoUpdateInput: false,
 				isInvalidDate: function(date){ 
-					// if(chaves.indexOf(date.format('YYYY-MM-DD')) > -1){
+					// if(chaves.indexOf(date.format('YYYY-MM-DD')) > -1){ 
 					// 	return true;
 					// }
 				},
