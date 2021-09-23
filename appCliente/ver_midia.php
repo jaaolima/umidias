1a57c5cbe0bb3c8f3830e6f35f1c9a3e3cd1c2bd
jaaolima <victorespucoc@gmail.com>
2021-09-23 18:15:12 -0300

    foto perfil

--- appCliente/ver_midia.php
+++ appCliente/ver_midia.php
@@ -481,7 +481,7 @@ License: You must have a valid license purchased only from themeforest(the above
 					// }
 				},
 				
-				minDate: new Date(),
+				minDate: new Date(), 
 				// ranges: {
 				// 	'1 mês': [moment(), moment()],
 				// 	'2 meses': [moment().add(), moment()],
