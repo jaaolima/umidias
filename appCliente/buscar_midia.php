3aa243e66cd28a3b6b3106665fb1a77578a03353
jaaolima <victorespucoc@gmail.com>
2021-09-24 16:05:34 -0300

    mapa

--- appCliente/buscar_midia.php
+++ appCliente/buscar_midia.php
@@ -250,7 +250,7 @@ License: You must have a valid license purchased only from themeforest(the above
 							"</div>"+
 						"</div>"+
 						"<div class='separator separator-solid'></div>"+
-						"<div class='my-8 mx-4 text-right'>"+
+						"<div class='mt-2 text-right'>"+
 							"<a href='appCliente/ver_midia.php?id_ponto='<?php echo $dados['id_ponto']; ?>'&id_midia='<?php echo $id_midia; ?>' class='text-primary'>Ver detalhes"+
 								"<svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>"+
 									"<path d='M5 12H19' stroke='#B721FF' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/>"+
