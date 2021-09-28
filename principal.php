0f1d9b2547c3a3b20115cfc75296f1d565e3f0ae
jaaolima <victorespucoc@gmail.com>
2021-09-28 14:19:38 -0300

    mapa

--- principal.php
+++ principal.php
@@ -488,7 +488,7 @@
 																<div class="row col-12 mt-4 " id="midias_proximas"></div>
 																<div class="row col-12 mt-4 justify-content-center text-center d-none" id="aceitar_localizacao">
 																	<h3>Precisamos da sua localização para procurar</h3><br>	
-																	<button class="btn btn-primary" type="button" id="button_aceitar_localizacao" >Aceitar</button>
+																	<button class="btn btn-primary" type="button" id="button_aceitar_localizacao" onclick="(geolocalizacao());" >Aceitar</button>
 																</div>
 															</div>
 														</div>
@@ -917,7 +917,6 @@
 				KTApexChartsDemo.init();
 				geolocalizacao();
 
-				$("#button_aceitar_localizacao").on("click", geolocalizacao());
 			});
 
 		</script>
