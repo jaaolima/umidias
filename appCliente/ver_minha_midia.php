ee5f72125d4d48bea2f615a79e55802d0f2c9440
jaaolima <victorespucoc@gmail.com>
2021-09-30 15:07:34 -0300

    desativar

--- appCliente/ver_minha_midia.php
+++ appCliente/ver_minha_midia.php
@@ -131,7 +131,7 @@ License: You must have a valid license purchased only from themeforest(the above
 									<?php endif; ?>
 									<?php if($dados["id_status_midia"] == 2) : ?>
 									<div class="text-center" style="font-size:20px;">
-										<span class="font-weight-bolder">><?php echo $dados["ds_status"]; ?></span>
+										<span class="font-weight-bolder"><?php echo $dados["ds_status"]; ?></span>
 										<div class='progress progress-xs mt-2 mb-2 flex-shrink-0 w-150px w-xl-250px'>
 											<div class='progress-bar bg-danger' role='Progresso' style='width: 25%;' aria-valuenow='50' aria-valuemin='0' aria-valuemax='100'></div>
 										</div> 
@@ -147,7 +147,7 @@ License: You must have a valid license purchased only from themeforest(the above
 									<?php endif; ?>
 									<?php if($dados["id_status_midia"] == 4) : ?>
 									<div class="text-center" style="font-size:20px;">
-										<span class="font-weight-bolder">><?php echo $dados["ds_status"]; ?></span>
+										<span class="font-weight-bolder"><?php echo $dados["ds_status"]; ?></span>
 										<div class='progress progress-xs mt-2 mb-2 flex-shrink-0 w-150px w-xl-250px'>
 											<div class='progress-bar bg-danger' role='Progresso' style='width: 75%;' aria-valuenow='50' aria-valuemin='0' aria-valuemax='100'></div>
 										</div>
@@ -155,7 +155,7 @@ License: You must have a valid license purchased only from themeforest(the above
 									<?php endif; ?>
 									<?php if($dados["id_status_midia"] == 5) : ?>
 									<div class="text-center" style="font-size:20px;">
-										<span class="font-weight-bolder">><?php echo $dados["ds_status"]; ?></span>
+										<span class="font-weight-bolder"><?php echo $dados["ds_status"]; ?></span>
 										<div class='progress progress-xs mt-2 mb-2 flex-shrink-0 w-150px w-xl-250px'>
 											<div class='progress-bar bg-success' role='Progresso' style='width: 100%;' aria-valuenow='50' aria-valuemin='0' aria-valuemax='100'></div>
 										</div>
