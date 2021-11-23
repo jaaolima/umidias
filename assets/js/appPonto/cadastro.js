5e0baf44fdf75138f14b4c5d1c3dae8341e28861
jaaolima <victorespucoc@gmail.com>
2021-11-23 17:57:20 -0300

    Bismena

--- assets/js/appPonto/cadastro.js
+++ assets/js/appPonto/cadastro.js
@@ -101,7 +101,7 @@ $(document).ready(function() {
 		"mask": "9,99 x 9,99",
 		numericInput: true,
 	});
-	$("#nu_valor").inputmask({ 
+	$("#nu_valor").inputmask({  
 		'alias': 'numeric',
 		'groupSeparator': '.',
 		'autoGroup': true,
@@ -336,6 +336,74 @@ function validar()
 	return true;
 }
 
+var DatatablesBasicBasic = function() {
+
+	var initTable1 = function() {
+		var table = $('#table_bisemana');
+
+		// begin first table
+		table.DataTable({
+			responsive: true,
+			retrieve: true, 
+			
+
+			//== DOM Layout settings
+			dom: `f<'row'<'col-sm-12'tr>>
+			<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>`,
+
+			lengthMenu: [5, 10, 25, 50],
+
+			pageLength: 10,
+
+			language: {
+				'lengthMenu': 'Mostrar _MENU_',
+			},
+			"oLanguage": {
+			    "sSearch": "<span>Pesquisar:</span> _INPUT_",
+			    "sLengthMenu": "<span>Mostrar:</span> _MENU_",
+			    "sInfo": "<span>Mostrando </span>_START_ até _END_ de _TOTAL_",
+			    "sZeroRecords": "Não existem dados cadastrados",
+			    "sInfoEmpty": "<span>Mostrando </span>0 até 0 de 0",
+			    "oPaginate": { "sFirst": "Primeira", "sLast": "Última", "sNext": ">", "sPrevious": "<" }
+		    },
+
+			//== Order settings
+			order: [[1, 'asc']],
+
+
+			columnDefs: [
+
+				{
+					targets: -1,
+					title: 'Ações',
+					orderable: false,
+				},
+				{
+					targets: 0,
+					visible: false
+				},
+				
+				
+			],
+		});
+
+		
+	};
+
+	return {
+
+		//main function to initiate the module
+		init: function() {
+			initTable1();
+		},
+
+	};
+
+}();
+
+jQuery(document).ready(function() {
+	DatatablesBasicBasic.init();
+});
 
 /*
 var KTDropzoneDemo = function () {
