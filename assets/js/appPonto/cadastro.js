84362a4a86aa9b26d525d21a39648ee79df72768
jaaolima <victorespucoc@gmail.com>
2022-01-26 17:07:29 -0300

    Status midia alugada

--- assets/js/appPonto/cadastro.js
+++ assets/js/appPonto/cadastro.js
@@ -47,7 +47,7 @@ $(document).ready(function() {
 	        , data: {id_estado : id_estado}
 	        , success: function(html) {
 	        	$("#id_cidade").empty();
-	        	$("#id_cidade").append(html);     
+	        	$("#id_cidade").append(html);      
 	        }
 	    }); 
 	}); 
@@ -402,11 +402,64 @@ var DatatablesBasicBasic = function() {
 		
 	};
 
+	var initTable2 = function() {
+		var table = $('#table_mes');
+
+		// begin first table
+		table.DataTable({
+			responsive: true,
+			retrieve: true, 
+			
+
+			//== DOM Layout settings
+			dom: `f<'row'<'col-sm-12'tr>>
+			<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>`,
+
+			lengthMenu: [5, 10, 25, 50],
+
+			pageLength: 10,
+
+			language: {
+				'lengthMenu': 'Mostrar _MENU_',
+			},
+			"oLanguage": {
+			    "sSearch": "<span>Pesquisar:</span> _INPUT_",
+			    "sLengthMenu": "<span>Mostrar:</span> _MENU_",
+			    "sInfo": "<span>Mostrando </span>_START_ até _END_ de _TOTAL_",
+			    "sZeroRecords": "Não existem dados cadastrados",
+			    "sInfoEmpty": "<span>Mostrando </span>0 até 0 de 0",
+			    "oPaginate": { "sFirst": "Primeira", "sLast": "Última", "sNext": ">", "sPrevious": "<" }
+		    },
+
+			//== Order settings
+			order: [[1, 'asc']],
+
+
+			columnDefs: [
+
+				{
+					targets: -1,
+					title: 'Ações',
+					orderable: false,
+				},
+				{
+					targets: 0,
+					visible: false
+				},
+				
+				
+			],
+		});
+
+		
+	};
+
 	return {
 
 		//main function to initiate the module
 		init: function() {
 			initTable1();
+			initTable2();
 		},
 
 	};
