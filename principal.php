5efb9d6a77f06249c1cab52df4ca6dee479f453f
João Victor <victorespucoc@gmail.com>
2022-01-28 17:10:13 -0300

    Todos bairros

--- principal.php
+++ principal.php
@@ -23,13 +23,12 @@
 	if(!isset($_SESSION['id_usuario'])) {
 		$_SESSION["id_usuario"] = 0;
 		$_SESSION["id_perfil"] = 1;
-		$_SESSION["id_parceiro"] = 0;
+		$_SESSION["id_parceiro"] = null;
 	}
 	$id_usuario = $_SESSION["id_usuario"];
 
-	$id_parceiro = $_SESSION['id_parceiro'];
 
-	if($id_parceiro == "" || $id_parceiro == 0){
+	if($_SESSION['id_parceiro'] == ""){
 		$id_parceiro = NULL;
 	}
 
