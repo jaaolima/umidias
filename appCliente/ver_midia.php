2fd79d53f3bb683fc1a64e34719adf2821f04dc9
João Victor <victorespucoc@gmail.com>
2022-02-01 15:03:45 -0300

    Mudanças Unimidias

--- appCliente/ver_midia.php
+++ appCliente/ver_midia.php
@@ -590,9 +590,8 @@ License: You must have a valid license purchased only from themeforest(the above
 
 
 			//calculo material
-			var material = document.getElementById("id_material");
 			var Totalmaterial = document.getElementById("valor_material");
-			material.on("change", function(){
+			$("#id_material").on("change", function(){
 				//adicionar na div material
 				let valorMaterial = $(this).find(':selected').attr('valor')
 				let valorTotalMaterial =  parseInt(valorMaterial, 10);
