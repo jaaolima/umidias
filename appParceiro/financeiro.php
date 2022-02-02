aa0b77147ee6f33b45c759c3a28b25a42b3ba1aa
João Victor <victorespucoc@gmail.com>
2022-02-02 15:13:53 -0300

    Mudanças Unimidias

--- appParceiro/financeiro.php
+++ appParceiro/financeiro.php
@@ -84,74 +84,42 @@ License: You must have a valid license purchased only from themeforest(the above
 			jQuery(document).ready(function () {
 				google.charts.load('current', {'packages':['corechart']});
 				google.charts.setOnLoadCallback(drawChart);
-				function drawChart() {
-
+				function drawMaterial() {
 					var data = new google.visualization.DataTable();
-					data.addColumn('number', 'Mês anterior');
-					data.addColumn('number', 'Esse Mês');
+					data.addColumn('timeofday', 'Time of Day');
+					data.addColumn('number', 'Motivation Level');
+					data.addColumn('number', 'Energy Level');
 
 					data.addRows([
-						[{
-							v: [8, 0, 0],
-							f: '8 am'
-						}, 1, .25],
-						[{
-							v: [9, 0, 0],
-							f: '9 am'
-						}, 2, .5],
-						[{
-							v: [10, 0, 0],
-							f: '10 am'
-						}, 3, 1],
-						[{
-							v: [11, 0, 0],
-							f: '11 am'
-						}, 4, 2.25],
-						[{
-							v: [12, 0, 0],
-							f: '12 pm'
-						}, 5, 2.25],
-						[{
-							v: [13, 0, 0],
-							f: '1 pm'
-						}, 6, 3],
-						[{
-							v: [14, 0, 0],
-							f: '2 pm'
-						}, 7, 4],
-						[{
-							v: [15, 0, 0],
-							f: '3 pm'
-						}, 8, 5.25],
-						[{
-							v: [16, 0, 0],
-							f: '4 pm'
-						}, 9, 7.5],
-						[{
-							v: [17, 0, 0],
-							f: '5 pm'
-						}, 10, 10],
+						[{v: [8, 0, 0], f: '8 am'}, 1, .25],
+						[{v: [9, 0, 0], f: '9 am'}, 2, .5],
+						[{v: [10, 0, 0], f:'10 am'}, 3, 1],
+						[{v: [11, 0, 0], f: '11 am'}, 4, 2.25],
+						[{v: [12, 0, 0], f: '12 pm'}, 5, 2.25],
+						[{v: [13, 0, 0], f: '1 pm'}, 6, 3],
+						[{v: [14, 0, 0], f: '2 pm'}, 7, 4],
+						[{v: [15, 0, 0], f: '3 pm'}, 8, 5.25],
+						[{v: [16, 0, 0], f: '4 pm'}, 9, 7.5],
+						[{v: [17, 0, 0], f: '5 pm'}, 10, 10],
 					]);
 
 					var options = {
-						title: 'Aluguel de',
-						focusTarget: 'category',
+						title: 'Motivation and Energy Level Throughout the Day',
 						hAxis: {
-							title: 'Time of Day',
-							format: 'h:mm a',
-							viewWindow: {
-								min: [7, 30, 0],
-								max: [17, 30, 0]
-							},
+						title: 'Time of Day',
+						format: 'h:mm a',
+						viewWindow: {
+							min: [7, 30, 0],
+							max: [17, 30, 0]
+						}
 						},
 						vAxis: {
-							title: 'Rating (scale of 1-10)'
-						},
-						colors: ['#6e4ff5', '#fe3995']
+						title: 'Rating (scale of 1-10)'
+						}
 					};
 
-					var chart = new google.visualization.ColumnChart(document.getElementById('chart'));
-					chart.draw(data, options);
+					var materialChart = new google.charts.Bar(document.getElementById('charts'));
+					materialChart.draw(data, options);
 				}
 			});
 			
