6720555d9ec2918a3204c393a70da6b6eb52c73b
João Victor <victorespucoc@gmail.com>
2022-02-02 14:27:00 -0300

    Mudanças Unimidias

--- principal.php
+++ principal.php
@@ -120,13 +120,9 @@
                                                                 <h3  id="titulo_midias_proximas" class="titulo-div">Mídias próximas à Você</h3>
                                                             </div>
                                                             <div class="row col-12 mt-4 " id="midias_proximas">
-                                                                <div class="w-100 my-4 text-center">
-                                                                    <div class="spinner spinner-primary mr-15"></div>
+                                                                <div class="row col-12 my-4 text-center">
+                                                                    <div class="spinner spinner-primary"></div>
                                                                 </div>
-                                                            </div>
-                                                            <div class="row col-12 mt-4 justify-content-center text-center d-none" id="aceitar_localizacao">
-                                                                <h3>Precisamos da sua localização para procurar as mídias próximas à você!</h3><br>	
-                                                                <!-- <button class="btn btn-primary" type="button" id="button_aceitar_localizacao">Aceitar</button> -->
                                                             </div>
                                                         </div>
                                                     </div>
