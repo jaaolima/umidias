8df42efac3fa4a0cf659df14fbd1c37c0c8975ce
João Victor <victorespucoc@gmail.com>
2022-02-02 15:21:49 -0300

    Mudanças Unimidias

--- appParceiro/financeiro.php
+++ appParceiro/financeiro.php
@@ -79,44 +79,27 @@ License: You must have a valid license purchased only from themeforest(the above
 
 			
 			jQuery(document).ready(function () {
-				google.charts.load('current', {packages: ['corechart', 'bar']});
+				google.charts.load('current', {'packages':['bar']})
 				google.charts.setOnLoadCallback(drawMaterial);
 				function drawMaterial() {
-					var data = new google.visualization.DataTable();
-					data.addColumn('timeofday', 'Time of Day');
-					data.addColumn('number', 'Motivation Level');
-					data.addColumn('number', 'Energy Level');
-
-					data.addRows([
-						[{v: [8, 0, 0], f: '8 am'}, 1, .25],
-						[{v: [9, 0, 0], f: '9 am'}, 2, .5],
-						[{v: [10, 0, 0], f:'10 am'}, 3, 1],
-						[{v: [11, 0, 0], f: '11 am'}, 4, 2.25],
-						[{v: [12, 0, 0], f: '12 pm'}, 5, 2.25],
-						[{v: [13, 0, 0], f: '1 pm'}, 6, 3],
-						[{v: [14, 0, 0], f: '2 pm'}, 7, 4],
-						[{v: [15, 0, 0], f: '3 pm'}, 8, 5.25],
-						[{v: [16, 0, 0], f: '4 pm'}, 9, 7.5],
-						[{v: [17, 0, 0], f: '5 pm'}, 10, 10],
+					var data = google.visualization.arrayToDataTable([
+					['Year', 'Sales', 'Expenses', 'Profit'],
+					['2014', 1000, 400, 200],
+					['2015', 1170, 460, 250],
+					['2016', 660, 1120, 300],
+					['2017', 1030, 540, 350]
 					]);
 
 					var options = {
-						title: 'Motivation and Energy Level Throughout the Day',
-						hAxis: {
-						title: 'Time of Day',
-						format: 'h:mm a',
-						viewWindow: {
-							min: [7, 30, 0],
-							max: [17, 30, 0]
-						}
-						},
-						vAxis: {
-						title: 'Rating (scale of 1-10)'
-						}
+					chart: {
+						title: 'Company Performance',
+						subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+					}
 					};
 
-					var materialChart = new google.charts.Bar(document.getElementById('chart'));
-					materialChart.draw(data, options);
+					var chart = new google.charts.Bar(document.getElementById('chart'));
+
+					chart.draw(data, google.charts.Bar.convertOptions(options));
 				}
 			});
 			
