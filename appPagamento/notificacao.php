ec44db17a154d71cc700f387fe40ef0391bb3bd9
João Victor <victorespucoc@gmail.com>
2022-02-18 17:25:04 -0300

    sdk mercado pago

--- appPagamento/notificacao.php
+++ appPagamento/notificacao.php
@@ -16,7 +16,7 @@ curl_setopt_array($curl, array(
     CURL_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => "GET",
     CURLOPT_HTTPHEADER => array(
-        "Authorization: Bearer ". $access_tokker
+        "Authorization: Bearer ". $access_token
     ),
 ));
 
