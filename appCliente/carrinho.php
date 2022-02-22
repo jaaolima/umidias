3e63f9d016620af3185e05968a41cff5d89d3f5c
João Victor <victorespucoc@gmail.com>
2022-02-22 14:04:35 -0300

    sdk mercado pago

--- appCliente/carrinho.php
+++ appCliente/carrinho.php
@@ -117,7 +117,7 @@ License: You must have a valid license purchased only from themeforest(the above
                                                         <div class='my-4 mx-15 text-left'>
                                                             <div class='d-flex ml-n8'>
                                                                 <div class='mt-1'>
-                                                                    <img  src='assets/media/tape.png'> 
+                                                                    <i class='flaticon-calendar-1'></i>
                                                                 </div>
                                                                 <div class='ml-2'>
                                                                     <span class='text-dark font-weight-bold text-dark font-size-h4'>Data</span><br>
