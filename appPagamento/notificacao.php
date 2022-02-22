c422c1d87291b9d3836b72449080017b13acc98d
João Victor <victorespucoc@gmail.com>
2022-02-22 14:56:15 -0300

    sdk mercado pago

--- appPagamento/notificacao.php
+++ appPagamento/notificacao.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 
 $colletion_id = $_REQUEST["collection_id"];
 
-$access_token = TEST-7295345192603478-021818-84abf249ade799c52fe9448499b4d47f-220722926;
+$access_token = "TEST-7295345192603478-021818-84abf249ade799c52fe9448499b4d47f-220722926";
 
 $curl = curl_init();
 
@@ -20,7 +20,7 @@ curl_setopt_array($curl, array(
     CURLOPT_HTTP_VERSION => CURLOPT_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => "GET",
     CURLOPT_HTTPHEADER => array(
-        "Authorization: Bearer ". $access_token
+        "Authorization: Bearer TEST-7295345192603478-021818-84abf249ade799c52fe9448499b4d47f-220722926"
     ),
 ));
 
