74f6c54cc4f374e602afb25e58c464e4177a3abc
João Victor <victorespucoc@gmail.com>
2022-03-08 16:19:45 -0300

    contrato

--- appCliente/contrato_pdf.php
+++ appCliente/contrato_pdf.php
@@ -2,6 +2,7 @@
 ini_set('display_errors',1);
 ini_set('display_startup_erros',1);
 error_reporting(E_ALL);
+session_start();
 
 require_once("../Classes/Ponto.php");
 require_once("../Classes/Usuario.php");
