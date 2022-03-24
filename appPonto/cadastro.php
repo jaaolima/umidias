9b594dbf9d17f2d1c3c6aa7e93a530e43344cd4b
João Victor <victorespucoc@gmail.com>
2022-03-24 14:53:25 -0300

    cadastro ponto

--- appPonto/cadastro.php
+++ appPonto/cadastro.php
@@ -414,64 +414,64 @@ $optionsMes = $Bisemana->listarTodosMesPonto();
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB0sGOoifQgDLzR_xYQbaGiiqXRHaJN2tM&callback=initMap&v=beta" async></script>
 <script>
 
-function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: {
-      lat: -15.7750656,
-      lng: -48.0773014,
-    },
-    zoom: 16,
-    heading: 320,
-    tilt: 47.5,
-    mapId: "90f87356969d889c",
-  });
-  const buttons = [
-    ["Rotate Left", "rotate", 20, google.maps.ControlPosition.LEFT_CENTER],
-    ["Rotate Right", "rotate", -20, google.maps.ControlPosition.RIGHT_CENTER],
-    ["Tilt Down", "tilt", 20, google.maps.ControlPosition.TOP_CENTER],
-    ["Tilt Up", "tilt", -20, google.maps.ControlPosition.BOTTOM_CENTER],
-  ];
+    function initMap() {
+        const map = new google.maps.Map(document.getElementById("map"), {
+            center: {
+            lat: -15.7750656,
+            lng: -48.0773014,
+            },
+            zoom: 16,
+            heading: 320,
+            tilt: 47.5,
+            mapId: "90f87356969d889c",
+        });
+        const buttons = [
+            ["Rotate Left", "rotate", 20, google.maps.ControlPosition.LEFT_CENTER],
+            ["Rotate Right", "rotate", -20, google.maps.ControlPosition.RIGHT_CENTER],
+            ["Tilt Down", "tilt", 20, google.maps.ControlPosition.TOP_CENTER],
+            ["Tilt Up", "tilt", -20, google.maps.ControlPosition.BOTTOM_CENTER],
+        ];
 
-  buttons.forEach(([text, mode, amount, position]) => {
-    const controlDiv = document.createElement("div");
-    const controlUI = document.createElement("button");
+        buttons.forEach(([text, mode, amount, position]) => {
+            const controlDiv = document.createElement("div");
+            const controlUI = document.createElement("button");
 
-    controlUI.classList.add("ui-button");
-    controlUI.innerText = `${text}`;
-    controlUI.addEventListener("click", () => {
-      adjustMap(mode, amount);
-    });
-    controlDiv.appendChild(controlUI);
-    map.controls[position].push(controlDiv);
-  });
+            controlUI.classList.add("ui-button");
+            controlUI.innerText = `${text}`;
+            controlUI.addEventListener("click", () => {
+            adjustMap(mode, amount);
+            });
+            controlDiv.appendChild(controlUI);
+            map.controls[position].push(controlDiv);
+        });
 
-  const adjustMap = function (mode, amount) {
-    switch (mode) {
-      case "tilt":
-        map.setTilt(map.getTilt() + amount);
-        break;
-      case "rotate":
-        map.setHeading(map.getHeading() + amount);
-        break;
-      default:
-        break;
+        const adjustMap = function (mode, amount) {
+            switch (mode) {
+            case "tilt":
+                map.setTilt(map.getTilt() + amount);
+                break;
+            case "rotate":
+                map.setHeading(map.getHeading() + amount);
+                break;
+            default:
+                break;
+            }
+        };
+        google.maps.event.addListener(map, "click", (event) => {
+            addMarker(event.latLng, map);
+        });
+
+        // Adds a marker to the map.
+        function addMarker(location, map) {
+            // Add the marker at the clicked location, and add the next-available label
+            // from the array of alphabetical characters.
+            new google.maps.Marker({
+                position: location,
+                label: labels[labelIndex++ % labels.length],
+                map: map,
+            });
+        }
     }
-  };
-  google.maps.event.addListener(map, "click", (event) => {
-    addMarker(event.latLng, map);
-  });
-
-    // Adds a marker to the map.
-    function addMarker(location: google.maps.LatLngLiteral, map: google.maps.Map) {
-        // Add the marker at the clicked location, and add the next-available label
-        // from the array of alphabetical characters.
-        new google.maps.Marker({
-            position: location,
-            label: labels[labelIndex++ % labels.length],
-            map: map,
-        })
-    }
-}
 
 // jQuery(document).ready(function() {
 //     demo3(-15.7750656, -48.0773014);
