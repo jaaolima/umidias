ac90819aa9f01d559416ce0fba236a5c79f58454
João Victor <victorespucoc@gmail.com>
2022-03-24 14:31:15 -0300

    cadastro ponto

--- appPonto/cadastro.php
+++ appPonto/cadastro.php
@@ -413,12 +413,54 @@ $optionsMes = $Bisemana->listarTodosMesPonto();
 <script src="./assets/js/appPonto/cadastro.js" type="text/javascript"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB0sGOoifQgDLzR_xYQbaGiiqXRHaJN2tM&v=beta&callback=initMap"></script>
 <script>
-map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: -15.7750656, lng: -48.0773014},
-  zoom: 16,
-  heading: 320,
-  tilt: 47.5,
-  mapId: 'MAP_ID'
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: -15.7750656,
+      lng: -48.0773014,
+    },
+    zoom: 16,
+    heading: 320,
+    tilt: 47.5,
+    mapId: "90f87356969d889c",
+  });
+  const buttons = [
+    ["Rotate Left", "rotate", 20, google.maps.ControlPosition.LEFT_CENTER],
+    ["Rotate Right", "rotate", -20, google.maps.ControlPosition.RIGHT_CENTER],
+    ["Tilt Down", "tilt", 20, google.maps.ControlPosition.TOP_CENTER],
+    ["Tilt Up", "tilt", -20, google.maps.ControlPosition.BOTTOM_CENTER],
+  ];
+
+  buttons.forEach(([text, mode, amount, position]) => {
+    const controlDiv = document.createElement("div");
+    const controlUI = document.createElement("button");
+
+    controlUI.classList.add("ui-button");
+    controlUI.innerText = `${text}`;
+    controlUI.addEventListener("click", () => {
+      adjustMap(mode, amount);
+    });
+    controlDiv.appendChild(controlUI);
+    map.controls[position].push(controlDiv);
+  });
+
+  const adjustMap = function (mode, amount) {
+    switch (mode) {
+      case "tilt":
+        map.setTilt(map.getTilt() + amount);
+        break;
+      case "rotate":
+        map.setHeading(map.getHeading() + amount);
+        break;
+      default:
+        break;
+    }
+  };
+}
+
+$(document).ready(function(){
+    initMap();
 });
 // jQuery(document).ready(function() {
 //     demo3(-15.7750656, -48.0773014);
