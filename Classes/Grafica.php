23aa4d8da40a70746027a104c313283008e7cf70
João Victor <victorespucoc@gmail.com>
2022-06-05 11:17:12 -0300

    grafica

--- Classes/Grafica.php
+++ Classes/Grafica.php
@@ -210,7 +210,7 @@
 		
 					$dados = $stmt->fetch();
 					$arquivo =  "../" . $dados["ds_arte"];
-					echo $arquivo;
+					var_dump($arquivo);
 					// $mail->AddAttachment($arquivo, "material".$id.".pdf"); 
 				}
 				catch(exception $e)
@@ -221,17 +221,17 @@
 				
 			}
 			
-			// Envia o e-mail 
-			$enviado = $mail->Send(); 
+			// // Envia o e-mail 
+			// $enviado = $mail->Send(); 
 
-			// Exibe uma mensagem de resultado 
-			if ($enviado) 
-			{ 
-				echo "Seu email foi enviado com sucesso!"; 
-			} else { 
-				echo "Houve um erro enviando o email: ".$mail->ErrorInfo; 
-				return false;
-			} 
+			// // Exibe uma mensagem de resultado 
+			// if ($enviado) 
+			// { 
+			// 	echo "Seu email foi enviado com sucesso!"; 
+			// } else { 
+			// 	echo "Houve um erro enviando o email: ".$mail->ErrorInfo; 
+			// 	return false;
+			// } 
 
 		}
 
