e3dc262f2292191149b4319d110be74c45a10970
João Victor <victorespucoc@gmail.com>
2022-10-05 15:48:15 -0300

    cadastro pessoal

--- assets/js/appCliente/cadastro_pessoal.js
+++ assets/js/appCliente/cadastro_pessoal.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 	    
 	$("#adicionar").on("click", function(e){ 
-		if(validar())
+        var cpf = $("#nu_cpf").val();
+		if(validar() && validarCPF(cpf))
 		{ 
 			$.ajax({
 		        url: 'appCliente/gravar_dados_pessoais_cliente.php'
@@ -62,4 +63,57 @@ function validar()
 	
 
 	return true;
+}
+function validarCPF(cpf) {	
+
+	cpf = cpf.replace(/[^\d]+/g,'');
+
+    // Elimina CPFs invalidos conhecidos	
+    if (cpf.length != 11 || 
+        cpf == "00000000000" || 
+        cpf == "11111111111" || 
+        cpf == "22222222222" || 
+        cpf == "33333333333" || 
+        cpf == "44444444444" || 
+        cpf == "55555555555" || 
+        cpf == "66666666666" || 
+        cpf == "77777777777" || 
+        cpf == "88888888888" || 
+        cpf == "99999999999")
+    {
+        $("#nu_cpf").focus();
+        swal.fire("Erro", "CPF inválido", "error");
+        $("#nu_cpf").addClass("is-invalid");
+        return false;		
+    }	
+    // Valida 1o digito	
+    add = 0;	
+    for (i=0; i < 9; i ++)		
+        add += parseInt(cpf.charAt(i)) * (10 - i);	
+        rev = 11 - (add % 11);	
+        if (rev == 10 || rev == 11)		
+            rev = 0;	
+        if (rev != parseInt(cpf.charAt(9)))		
+        {
+            $("#nu_cpf").focus();
+            swal.fire("Erro", "CPF inválido", "error");
+            $("#nu_cpf").addClass("is-invalid");
+            return false;		
+        }			
+    // Valida 2o digito	
+    add = 0;	
+    for (i = 0; i < 10; i ++)		
+        add += parseInt(cpf.charAt(i)) * (11 - i);	
+    rev = 11 - (add % 11);	
+    if (rev == 10 || rev == 11)	
+        rev = 0;	
+    if (rev != parseInt(cpf.charAt(10)))
+    {
+        $("#nu_cpf").focus();
+        swal.fire("Erro", "CPF inválido", "error");
+        $("#nu_cpf").addClass("is-invalid");
+        return false;		
+    }			
+    return true;   
+
 }
